6c1bdbdcda56e58922546bb3e5826be780b1eac2
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-08 21:52:23 -0700

    wip

--- test_liqpool/src/lib.rs
+++ test_liqpool/src/lib.rs
@@ -1,9 +1,12 @@
 #![no_std]
-use sdk::Val;
+use sdk::{Symbol, Val};
 use stellar_contract_sdk as sdk;
+
+const dataPoolAssetCirculatingKey: Val = Symbol::from_str("hello").into();
 
 #[no_mangle]
 pub fn init(acc_id: Val, pool_asset: Val, asset_a: Val, asset_b: Val) -> Val {
+    sdk::ledger::put_contract_data(dataPoolAssetCirculatingKey, Val::from_u63(0));
     todo!()
 }
 
@@ -18,11 +21,23 @@ pub fn withdraw(src_acc_id: Val, pool_amount: Val) -> Val {
 }
 
 #[no_mangle]
-pub fn trade_fixed_in(src_acc_id: Val, asset_in: Val, amount_in: Val, asset_out: Val, min_amount_out: Val) -> Val {
+pub fn trade_fixed_in(
+    src_acc_id: Val,
+    asset_in: Val,
+    amount_in: Val,
+    asset_out: Val,
+    min_amount_out: Val,
+) -> Val {
     todo!()
 }
 
 #[no_mangle]
-pub fn trade_fixed_out(src_acc_id: Val, asset_in: Val, max_amount_in: Val, asset_out: Val, amount_out: Val) -> Val {
+pub fn trade_fixed_out(
+    src_acc_id: Val,
+    asset_in: Val,
+    max_amount_in: Val,
+    asset_out: Val,
+    amount_out: Val,
+) -> Val {
     todo!()
 }
