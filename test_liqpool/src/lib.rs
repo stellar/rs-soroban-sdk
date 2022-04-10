df11165c59aed8e70bea1fe3e067cc443cebd7b8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-10 12:40:15 -0700

    update attr

--- test_liqpool/src/lib.rs
+++ test_liqpool/src/lib.rs
@@ -1,26 +1,27 @@
 #![no_std]
-use sdk::{Symbol, Val};
+use sdk::{OrAbort, Symbol, Val};
 use stellar_contract_sdk as sdk;
+use stellar_contract_sdk_macros as sdkmacros;
 
 const DATA_POOL_ASSET_CIRCULATING: Val = Val::from_symbol(Symbol::from_str("hello"));
 
-#[no_mangle]
+#[sdkmacros::contractfn]
 pub fn init(acc_id: Val, pool_asset: Val, asset_a: Val, asset_b: Val) -> Val {
     sdk::ledger::put_contract_data(DATA_POOL_ASSET_CIRCULATING, Val::from_u63(0));
     todo!()
 }
 
-#[no_mangle]
+#[sdkmacros::contractfn]
 pub fn deposit(src_acc_id: Val, amount_a: Val, amount_b: Val) -> Val {
     todo!()
 }
 
-#[no_mangle]
+#[sdkmacros::contractfn]
 pub fn withdraw(src_acc_id: Val, pool_amount: Val) -> Val {
     todo!()
 }
 
-#[no_mangle]
+#[sdkmacros::contractfn]
 pub fn trade_fixed_in(
     src_acc_id: Val,
     asset_in: Val,
@@ -31,7 +32,7 @@ pub fn trade_fixed_in(
     todo!()
 }
 
-#[no_mangle]
+#[sdkmacros::contractfn]
 pub fn trade_fixed_out(
     src_acc_id: Val,
     asset_in: Val,
