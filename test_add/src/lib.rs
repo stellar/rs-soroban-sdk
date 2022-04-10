e0c5902e7e77acb764b63eb291ad5b92e73ad7cb
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-10 01:04:51 -0700

    Add a macro for unwrapping and wrapping Vals

--- test_add/src/lib.rs
+++ test_add/src/lib.rs
@@ -1,9 +1,11 @@
 #![no_std]
 use sdk::{OrAbort, Val};
 use stellar_contract_sdk as sdk;
+use stellar_contract_sdk_macros as sdkmacros;
 
 #[no_mangle]
-pub fn add(a: Val, b: Val) -> Val {
+#[sdkmacros::contractfn]
+pub fn add(a: i64, b: i64) -> i64 {
     let a: i64 = a.try_into().or_abort();
     let b: i64 = b.try_into().or_abort();
 
@@ -14,16 +16,17 @@ pub fn add(a: Val, b: Val) -> Val {
 
 #[cfg(test)]
 mod test {
+    use super::__cf_add;
     use super::add;
     use sdk::Val;
     use stellar_contract_sdk as sdk;
 
     #[test]
     fn test_add() {
+        assert_eq!(add(10, 12), 22);
         let x: Val = Val::from_u63(10);
         let y: Val = Val::from_u63(12);
-        let z: Val = add(x, y);
-        let z: i64 = z.try_into().unwrap();
-        assert!(z == 22);
+        let r: Val = Val::from_u63(22);
+        assert_eq!(__cf_add(x, y), r);
     }
 }
