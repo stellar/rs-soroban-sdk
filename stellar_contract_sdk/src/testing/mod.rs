6797b39c71e36045c9a1cf3ec67e717b9ab0c300
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-12 17:02:25 -0700

    Move Status debug formatter into testing too

--- stellar_contract_sdk/src/testing/mod.rs
+++ stellar_contract_sdk/src/testing/mod.rs
@@ -1,2 +1,8 @@
 #[cfg(not(target_family = "wasm"))]
 pub mod host;
+
+#[cfg(not(target_family = "wasm"))]
+mod val;
+
+#[cfg(not(target_family = "wasm"))]
+mod status;
--- stellar_contract_sdk/src/val.rs
+++ stellar_contract_sdk/src/val.rs
@@ -160,7 +160,7 @@ impl Val {
     }
 
     #[inline(always)]
-    pub(crate) fn get_body(&self) -> u64 {
+    pub(crate) const fn get_body(&self) -> u64 {
         self.0 >> 4
     }
 
@@ -300,18 +300,5 @@ impl Val {
     #[inline(always)]
     pub fn from_object(obj: Object) -> Val {
         obj.into()
-    }
-}
-
-#[cfg(not(target_family = "wasm"))]
-use core::fmt::Debug;
-
-#[cfg(not(target_family = "wasm"))]
-impl Debug for Val {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Val")
-            .field("body", &self.get_body())
-            .field("tag", &self.get_tag())
-            .finish()
     }
 }
