11517b85665a349a019fdef2c3dd52b02191dfc6
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-04-15 15:28:17 -0700

    Add an implicit example

--- test_i64/src/lib.rs
+++ test_i64/src/lib.rs
@@ -24,7 +24,16 @@ mod test {
     use std::boxed::Box;
 
     #[test]
-    fn test_add_with_i64_vals() {
+    fn test_add_implicitly_using_objs() {
+        let x: Val = Val::from_u63(10);
+        let y: Val = Val::from(-7 as i64);
+        let z: Val = add(x, y);
+        let z: i64 = z.try_into().unwrap();
+        assert!(z == 3);
+    }
+
+    #[test]
+    fn test_add_explicitly_using_objs() {
         let mut host = Box::new(MemHost::new());
         let seven = host.put_obj(MemObj::I64(-7));
         swap_mock_host(host);
