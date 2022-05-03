5bf45f78c6cb9d4ee8e0374180f93c9e6360fc2a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-03 11:17:51 -0700

    wip

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -30,3 +30,42 @@ mod vec;
 pub use bignum::BigNum;
 pub use map::Map;
 pub use vec::Vec;
+
+#[inline(always)]
+pub const fn require(b: bool) {
+    if !b {
+        panic!();
+    }
+}
+
+#[inline(always)]
+pub fn log_value(v: Val) {
+    unsafe {
+        host::context::log_value(v);
+    }
+}
+
+#[inline(always)]
+pub fn call0(contract: Val, func: Symbol) -> Val {
+    unsafe { host::call::call0(contract, func.into()) }
+}
+
+#[inline(always)]
+pub fn call1(contract: Val, func: Symbol, a: Val) -> Val {
+    unsafe { host::call::call1(contract, func.into(), a) }
+}
+
+#[inline(always)]
+pub fn call2(contract: Val, func: Symbol, a: Val, b: Val) -> Val {
+    unsafe { host::call::call2(contract, func.into(), a, b) }
+}
+
+#[inline(always)]
+pub fn call3(contract: Val, func: Symbol, a: Val, b: Val, c: Val) -> Val {
+    unsafe { host::call::call3(contract, func.into(), a, b, c) }
+}
+
+#[inline(always)]
+pub fn call4(contract: Val, func: Symbol, a: Val, b: Val, c: Val, d: Val) -> Val {
+    unsafe { host::call::call4(contract, func.into(), a, b, c, d) }
+}
