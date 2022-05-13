2c281b193c2438bda3d239db452ae2a2eebe0d8f
Graydon Hoare <graydon@pobox.com>
2022-05-13 12:51:01 -0700

    Add a "tiny" target to Makefile that does build-std and panic_immediate_abort (#91)

---
+++ examples/add_i32/src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+use stellar_contract_sdk::{Env, EnvValConvertible, OrAbort, RawVal};
+
+#[no_mangle]
+pub fn add(e: &Env, a: RawVal, b: RawVal) -> RawVal {
+    let a: i32 = i32::try_from_val(e, a).or_abort();
+    let b: i32 = i32::try_from_val(e, b).or_abort();
+
+    let c = a + b;
+
+    return c.into_val(e);
+}
+
+#[cfg(test)]
+mod test {
+    use super::add;
+    use stellar_contract_sdk::{Env, EnvValConvertible, OrAbort};
+
+    #[test]
+    fn test_add() {
+        let e = &Env::default();
+        let x = 10i32.into_val(e);
+        let y = 12i32.into_val(e);
+        let z = add(e, x, y);
+        let z = i32::try_from_val(e, z).or_abort();
+        assert!(z == 22);
+    }
+}
