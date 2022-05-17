b76a51fa68dfbf3d19b32f40d8205b9d6ffd5ca3
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-17 16:58:39 -0700

    Add use of proptest to i32 example (#99)

--- examples/add_i32/src/lib.rs
+++ examples/add_i32/src/lib.rs
@@ -13,6 +13,9 @@ pub fn add(e: Env, a: RawVal, b: RawVal) -> RawVal {
 
 #[cfg(test)]
 mod test {
+    extern crate std;
+    use std::panic::{catch_unwind, AssertUnwindSafe};
+
     use super::add;
     use stellar_contract_sdk::{Env, EnvValConvertible, OrAbort};
 
@@ -24,5 +27,51 @@ mod test {
         let z = add(e.clone(), x, y);
         let z = i32::try_from_val(&e, z).or_abort();
         assert!(z == 22);
+    }
+
+    #[test]
+    fn test_add_overflow() {
+        let e = Env::default();
+        let x = (-241823608i32).into_val(&e);
+        let y = (-1905660041i32).into_val(&e);
+        let res = catch_unwind(AssertUnwindSafe(|| {
+            add(e, x, y);
+        }));
+        assert!(res.is_err());
+    }
+}
+
+#[cfg(test)]
+mod proptest {
+    extern crate std;
+    use core::panic::AssertUnwindSafe;
+    use proptest::prelude::*;
+    use std::{format, panic};
+
+    use super::add;
+    use stellar_contract_sdk::{Env, EnvValConvertible, OrAbort};
+
+    proptest! {
+        #[test]
+        fn test_add(a in any::<i32>(), b in any::<i32>()) {
+            let e = Env::default();
+            match a.checked_add(b) {
+                // If a + b would result in overflow, assert that the add fn
+                // will panic.
+                None => {
+                    let res = panic::catch_unwind(AssertUnwindSafe(move || {
+                        add(e.clone(), a.into_val(&e), b.into_val(&e));
+                    }));
+                    prop_assert!(res.is_err());
+                },
+                // If a + b would not result in overflow, assert that the add fn
+                // returns the sum of a and b.
+                Some(expected_sum) => {
+                    let vsum = add(e.clone(), a.into_val(&e), b.into_val(&e));
+                    let sum = i32::try_from_val(&e, vsum).or_abort();
+                    prop_assert_eq!(sum, expected_sum);
+                },
+            }
+        }
     }
 }
