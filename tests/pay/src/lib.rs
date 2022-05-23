b682f6a3530f16898319ba3962d907e231bdec4a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-23 15:56:19 -0700

    Remove out-dated tests and examples (#106)

--- tests/pay/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-use sdk::{Object, Val};
-use stellar_contract_sdk as sdk;
-
-#[no_mangle]
-pub fn pay(src: Object, dst: Object, asset: Object, amount: Val) -> Val {
-    sdk::ledger::pay(src, dst, asset, amount)
-}
