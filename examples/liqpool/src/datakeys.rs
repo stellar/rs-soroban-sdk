4645ea56c71b5100e48a44c24501ff0247a238fe
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-23 15:51:17 -0700

    Remove out-dated tests and examples

--- examples/liqpool/src/datakeys.rs
+++
@@ -1,7 +0,0 @@
-use stellar_contract_sdk::Val;
-
-pub const DATA_KEY_ACCOUNT: Val = Val::from_i32(0);
-pub const DATA_KEY_ASSET_POOL: Val = Val::from_i32(1);
-pub const DATA_KEY_ASSET_POOL_CIRCULATING: Val = Val::from_i32(2);
-pub const DATA_KEY_ASSET_A: Val = Val::from_i32(3);
-pub const DATA_KEY_ASSET_B: Val = Val::from_i32(4);
