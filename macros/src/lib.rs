2592eedf2bdfab79112e2d9415cb4510df607df2
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-08 09:09:30 -0700

    reorg

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -10,12 +10,33 @@ use syn::{
     ItemImpl, Pat, PatType, ReturnType, Type,
 };
 
+// TODO: Investigate how to make the multiple spec statics be joined into a
+// variable length XDR array instead of being a stream of XDR values.
+
+#[proc_macro_attribute]
+#[allow(clippy::missing_panics_doc)]
+pub fn contractfn(_metadata: TokenStream, input: TokenStream) -> TokenStream {
+    let func = parse_macro_input!(input as ItemFn);
+    let ident = &func.sig.ident;
+    let wrap_and_spec = wrap_and_spec(
+        &quote! { #ident },
+        ident,
+        &func.sig.inputs,
+        &func.sig.output,
+    );
+    quote! {
+        #func
+        #wrap_and_spec
+    }
+    .into()
+}
+
 #[proc_macro_attribute]
 #[allow(clippy::missing_panics_doc)]
 pub fn contractimpl(_metadata: TokenStream, input: TokenStream) -> TokenStream {
     let imp = parse_macro_input!(input as ItemImpl);
     let self_ty = &imp.self_ty;
-    let wass = imp
+    let wrap_and_specs = imp
         .items
         .iter()
         .filter_map(|i| match i {
@@ -33,25 +54,7 @@ pub fn contractimpl(_metadata: TokenStream, input: TokenStream) -> TokenStream {
         });
     quote! {
         #imp
-        #(#wass)*
-    }
-    .into()
-}
-
-#[proc_macro_attribute]
-#[allow(clippy::missing_panics_doc)]
-pub fn contractfn(_metadata: TokenStream, input: TokenStream) -> TokenStream {
-    let func = parse_macro_input!(input as ItemFn);
-    let ident = &func.sig.ident;
-    let was = wrap_and_spec(
-        &quote! { #ident },
-        ident,
-        &func.sig.inputs,
-        &func.sig.output,
-    );
-    quote! {
-        #func
-        #was
+        #(#wrap_and_specs)*
     }
     .into()
 }
