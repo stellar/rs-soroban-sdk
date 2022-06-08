821e4cf4b26385b6e3c0d69a5476fd3799d12d1c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-07 21:32:03 -0700

    reduce

--- examples/add_i64/src/lib.rs
+++ examples/add_i64/src/lib.rs
@@ -7,23 +7,18 @@ pub fn add(_e: Env, a: i64, b: i64) -> i64 {
     a + b
 }
 
-#[contractfn]
-pub fn add2(_e: Env, a: i64, b: i64, c: i64) -> i64 {
-    a + b + c
-}
-
 pub struct Add;
 
 #[contractimpl]
 impl Add {
-    pub fn add3(_e: Env, a: i64, b: i64, c: i64) -> i64 {
-        a + b + c
+    fn add2(_e: Env, a: i64, b: i64) -> i64 {
+        a + b
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{_add, _add2, _add3};
+    use super::{_add, _add2};
     use stellar_contract_sdk::{Env, IntoVal, TryFromVal};
 
     #[test]
@@ -41,20 +36,8 @@ mod test {
         let e = Env::default();
         let x = 10i64.into_val(&e);
         let y = 12i64.into_val(&e);
-        let y2 = 12i64.into_val(&e);
-        let z = _add2(e.clone(), x, y, y2);
+        let z = _add2(e.clone(), x, y);
         let z = i64::try_from_val(&e, z).unwrap();
-        assert!(z == 34);
-    }
-
-    #[test]
-    fn test_add3() {
-        let e = Env::default();
-        let x = 10i64.into_val(&e);
-        let y = 12i64.into_val(&e);
-        let y2 = 12i64.into_val(&e);
-        let z = _add3(e.clone(), x, y, y2);
-        let z = i64::try_from_val(&e, z).unwrap();
-        assert!(z == 34);
+        assert!(z == 22);
     }
 }
