aa4cd1fd64af2079fa52488be75f962f071fd8f9
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-08 23:19:31 -0700

    Better error handling and reduced dupe logic

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,13 +1,12 @@
 extern crate proc_macro;
 
-use core::panic;
-
+use itertools::MultiUnzip;
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote, ToTokens};
 use syn::{
     parse_macro_input, punctuated::Punctuated, spanned::Spanned, token::Comma, Error, FnArg, Ident,
-    ImplItem, ItemFn, ItemImpl, PatType, Type, TypePath, Visibility,
+    ImplItem, ItemFn, ItemImpl, PatType, ReturnType, Type, TypePath, Visibility,
 };
 
 // TODO: Investigate how to make the multiple spec statics be joined into a
@@ -19,7 +18,7 @@ pub fn contractfn(_metadata: TokenStream, input: TokenStream) -> TokenStream {
     let func = parse_macro_input!(input as ItemFn);
     let ident = &func.sig.ident;
     let call = quote! { #ident };
-    let wrap_and_spec = wrap_and_spec(&call, ident, &func.sig.inputs);
+    let wrap_and_spec = wrap_and_spec(&call, ident, &func.sig.inputs, &func.sig.output);
     quote! {
         #func
         #wrap_and_spec
@@ -44,7 +43,7 @@ pub fn contractimpl(_metadata: TokenStream, input: TokenStream) -> TokenStream {
         .map(|m| {
             let ident = &m.sig.ident;
             let call = quote! { <#ty>::#ident };
-            wrap_and_spec(&call, ident, &m.sig.inputs)
+            wrap_and_spec(&call, ident, &m.sig.inputs, &m.sig.output)
         });
     quote! {
         #imp
@@ -58,35 +57,14 @@ fn wrap_and_spec(
     call: &TokenStream2,
     ident: &Ident,
     inputs: &Punctuated<FnArg, Comma>,
+    output: &ReturnType,
 ) -> TokenStream2 {
-    // Collect errors as we go and emit them at the end.
+    // Collect errors as they are encountered and emit them at the end.
     let mut errors = Vec::<Error>::new();
 
-    // Prepare the spec parameters.
-    let spec_ident = format_ident!("__SPEC_{}", ident.to_string().to_uppercase());
-    let spec_inputs = format!(
-        "{:?}",
-        inputs
-            .iter()
-            .skip(1)
-            .map(|f| {
-                if let &FnArg::Typed(pat_type) = &f {
-                    return match &*pat_type.ty {
-                        Type::Path(p) => p.into_token_stream(),
-                        _ => todo!(),
-                    };
-                }
-                panic!("only accepts functions without a self argument")
-            })
-            .reduce(|a, b| quote! { #a #b })
-    );
-    let spec_inputs_literal = proc_macro2::Literal::byte_string(spec_inputs.as_bytes());
-    let spec_inputs_literal_size = spec_inputs.len();
-
     // Prepare the env input.
-    let env_input = inputs.first();
-    if let Some(env_input) = env_input {
-        match &env_input {
+    let env_input = inputs.first().map(|a| {
+        match a {
             FnArg::Typed(pat_type) => {
                 let ty = &*pat_type.ty;
                 if let Type::Path(TypePath {
@@ -94,17 +72,13 @@ fn wrap_and_spec(
                     ..
                 }) = ty
                 {
-                    if !segments.last().map_or(false, |s| s.ident == "Env") {
-                        errors.push(syn::Error::new(
-                            segments.span(),
-                            "first argument must be of type Env",
-                        ));
+                    if segments.last().map_or(false, |s| s.ident == "Env") {
+                        Some(a)
+                    } else {
+                        None
                     }
                 } else {
-                    errors.push(syn::Error::new(
-                        ty.span(),
-                        "first argument must be of type Env",
-                    ));
+                    None
                 }
             }
             FnArg::Receiver(r) => {
@@ -112,39 +86,43 @@ fn wrap_and_spec(
                     r.span(),
                     "self argument not supported first argument must be of type Env",
                 ));
+                None
             }
         };
-    }
+    });
 
     // Prepare the argument inputs.
-    let (wrap_args, wrap_calls): (Vec<_>, Vec<_>) = inputs
+    let (spec_args, wrap_args, wrap_calls): (Vec<_>, Vec<_>, Vec<_>) = inputs
         .iter()
         .skip(if env_input.is_some() { 1 } else { 0 })
         .map(|a| {
-            if let &FnArg::Typed(pat_type) = &a {
-                let pat = pat_type.pat.clone();
-                let arg = FnArg::Typed(PatType {
-                    attrs: Vec::new(),
-                    pat: pat_type.pat.clone(),
-                    colon_token: pat_type.colon_token,
-                    ty: Box::new(Type::Verbatim(quote! { stellar_contract_sdk::RawVal })),
-                });
-                let call = quote! {
-                    <_ as stellar_contract_sdk::TryFromVal<stellar_contract_sdk::Env, stellar_contract_sdk::RawVal>>::try_from_val(
-                        &__e,
-                        #pat
-                    ).unwrap()
-                };
-                (arg, call)
-            } else {
-                errors.push(syn::Error::new(
-                    a.span(),
-                    "argument not supported",
-                ));
-                (a.clone(), quote! { })
+            match a {
+                FnArg::Typed(pat_type) => {
+                    let pat = pat_type.pat.clone();
+                    let spec = pat_type.ty.to_token_stream().to_string(); // TODO: Map types to SCType for spec.
+                    let arg = FnArg::Typed(PatType {
+                        attrs: Vec::new(),
+                        pat: pat_type.pat.clone(),
+                        colon_token: pat_type.colon_token,
+                        ty: Box::new(Type::Verbatim(quote! { stellar_contract_sdk::RawVal })),
+                    });
+                    let call = quote! {
+                        <_ as stellar_contract_sdk::TryFromVal<stellar_contract_sdk::Env, stellar_contract_sdk::RawVal>>::try_from_val(
+                            &__e,
+                            #pat
+                        ).unwrap()
+                    };
+                    (spec, arg, call)
+                }
+                FnArg::Receiver(_) => {
+                    errors.push(syn::Error::new(
+                        a.span(),
+                        "self argument not supported",
+                    ));
+                    ("".to_string(), a.clone(), quote! { })
+                }
             }
-        })
-        .unzip();
+        }).multiunzip();
 
     // If errors have occurred, render them instead.
     if !errors.is_empty() {
@@ -153,9 +131,26 @@ fn wrap_and_spec(
     }
 
     // Output.
+    let spec_ident = format_ident!("__SPEC_{}", ident.to_string().to_uppercase());
+    let spec_args_str = format!(
+        // TODO: Produce XDR instead.
+        "[{}({}):{}]",
+        ident,
+        spec_args.join(","),
+        match output {
+            ReturnType::Default => "()".to_string(),
+            ReturnType::Type(_, ty) => ty.to_token_stream().to_string(),
+        }
+    );
+    let spec_args_bytes = spec_args_str.as_bytes();
+    let spec_args_literal = proc_macro2::Literal::byte_string(spec_args_bytes);
+    let spec_args_literal_size = spec_args_bytes.len();
     let wrap_export_name = format!("{}", ident);
     let wrap_ident = format_ident!("__{}", ident);
     quote! {
+        #[cfg_attr(target_family = "wasm", link_section = "contractspecv0")]
+        pub static #spec_ident: [u8; #spec_args_literal_size] = *#spec_args_literal;
+
         #[export_name = #wrap_export_name]
         fn #wrap_ident(__e: stellar_contract_sdk::Env, #(#wrap_args),*) -> stellar_contract_sdk::RawVal {
             <_ as stellar_contract_sdk::IntoVal<stellar_contract_sdk::Env, stellar_contract_sdk::RawVal>>::into_val(
@@ -166,7 +161,5 @@ fn wrap_and_spec(
                 &__e
             )
         }
-        #[cfg_attr(target_family = "wasm", link_section = "contractspecv0")]
-        pub static #spec_ident: [u8; #spec_inputs_literal_size] = *#spec_inputs_literal;
     }
 }
