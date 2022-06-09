b073f1810bb1a4299a619bbd968b09ebbf477280
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-08 21:49:29 -0700

    wip

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -6,8 +6,8 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote, ToTokens};
 use syn::{
-    parse_macro_input, punctuated::Punctuated, spanned::Spanned, token::Comma, FnArg, Ident,
-    ImplItem, ItemFn, ItemImpl, Pat, PatType, ReturnType, Type, TypePath, Visibility,
+    parse_macro_input, punctuated::Punctuated, spanned::Spanned, token::Comma, Error, FnArg, Ident,
+    ImplItem, ItemFn, ItemImpl, Pat, PatType, Type, TypePath, Visibility,
 };
 
 // TODO: Investigate how to make the multiple spec statics be joined into a
@@ -18,12 +18,8 @@ use syn::{
 pub fn contractfn(_metadata: TokenStream, input: TokenStream) -> TokenStream {
     let func = parse_macro_input!(input as ItemFn);
     let ident = &func.sig.ident;
-    let wrap_and_spec = wrap_and_spec(
-        &quote! { #ident },
-        ident,
-        &func.sig.inputs,
-        &func.sig.output,
-    );
+    let call = quote! { #ident };
+    let wrap_and_spec = wrap_and_spec(&call, ident, &func.sig.inputs);
     quote! {
         #func
         #wrap_and_spec
@@ -36,7 +32,7 @@ pub fn contractfn(_metadata: TokenStream, input: TokenStream) -> TokenStream {
 pub fn contractimpl(_metadata: TokenStream, input: TokenStream) -> TokenStream {
     let imp = parse_macro_input!(input as ItemImpl);
     let is_trait = imp.trait_.is_some();
-    let self_ty = &imp.self_ty;
+    let ty = &imp.self_ty;
     let wrap_and_specs = imp
         .items
         .iter()
@@ -47,12 +43,8 @@ pub fn contractimpl(_metadata: TokenStream, input: TokenStream) -> TokenStream {
         .filter(|m| is_trait || matches!(m.vis, Visibility::Public(_)))
         .map(|m| {
             let ident = &m.sig.ident;
-            wrap_and_spec(
-                &quote! { <#self_ty>::#ident },
-                ident,
-                &m.sig.inputs,
-                &m.sig.output,
-            )
+            let call = quote! { <#ty>::#ident };
+            wrap_and_spec(&call, ident, &m.sig.inputs)
         });
     quote! {
         #imp
@@ -66,50 +58,72 @@ fn wrap_and_spec(
     call: &TokenStream2,
     ident: &Ident,
     inputs: &Punctuated<FnArg, Comma>,
-    output: &ReturnType,
 ) -> TokenStream2 {
     // Collect errors as we go and emit them at the end.
-    let mut errors = Vec::<TokenStream2>::new();
-    errors.extend(inputs.first().and_then(|f| {
-        match &f {
-            FnArg::Typed(pat_type) => match &*pat_type.ty {
-                Type::Path(TypePath {
+    let mut errors = Vec::<Error>::new();
+
+    // Prepare the inputs.
+    let env_input = inputs.first();
+    if let Some(env_input) = env_input {
+        match &env_input {
+            FnArg::Typed(pat_type) => {
+                let ty = &*pat_type.ty;
+                if let Type::Path(TypePath {
                     path: syn::Path { segments, .. },
                     ..
-                }) => {
-                    if segments.last().map_or(false, |s| s.ident == "Env") {
-                        None
-                    } else {
-                        Some(
-                            syn::Error::new(segments.span(), "first argument must be of type Env")
-                                .to_compile_error(),
-                        )
+                }) = ty
+                {
+                    if !segments.last().map_or(false, |s| s.ident == "Env") {
+                        errors.push(syn::Error::new(
+                            segments.span(),
+                            "first argument must be of type Env",
+                        ));
                     }
+                } else {
+                    errors.push(syn::Error::new(
+                        ty.span(),
+                        "first argument must be of type Env",
+                    ));
                 }
-                _ => Some(
-                    syn::Error::new(pat_type.ty.span(), "first argument must be of type Env")
-                        .to_compile_error(),
-                ),
-            },
-            FnArg::Receiver(r) => Some(
-                syn::Error::new(r.span(), "first argument must be of type Env").to_compile_error(),
-            ),
-        }
-    }));
-    errors.extend(inputs.iter().skip(1).filter_map(|f| match &f {
-        FnArg::Typed(pat_type) => match &*pat_type.ty {
-            Type::Path(_) => None,
-            _ => Some(
-                syn::Error::new(pat_type.ty.span(), "argument type unsupported").to_compile_error(),
-            ),
-        },
-        FnArg::Receiver(r) => {
-            Some(syn::Error::new(r.span(), "argument type unsupported").to_compile_error())
-        }
-    }));
-    if !errors.is_empty() {
-        return quote! { #(#errors)* };
+            }
+            FnArg::Receiver(r) => {
+                errors.push(syn::Error::new(
+                    r.span(),
+                    "self argument not supported first argument must be of type Env",
+                ));
+            }
+        };
     }
+    // inputs.iter().skip(1).map(|a| {
+    //     if let &FnArg::Typed(pat_type) = &a {
+    //         let ty = &*pat_type.ty;
+    //         match ty {
+    //             Type::Path(_) => {}
+    //             Type::Array(_)
+    //             | Type::BareFn(_)
+    //             | Type::Group(_)
+    //             | Type::ImplTrait(_)
+    //             | Type::Infer(_)
+    //             | Type::Macro(_)
+    //             | Type::Never(_)
+    //             | Type::Paren(_)
+    //             | Type::Ptr(_)
+    //             | Type::Reference(_)
+    //             | Type::Slice(_)
+    //             | Type::TraitObject(_)
+    //             | Type::Tuple(_)
+    //             | Type::Verbatim(_)
+    //             | _ => {
+    //                 errors.push(syn::Error::new(
+    //                     a.span(),
+    //                     "self argument not supported first argument must be of type Env",
+    //                 ));
+    //             }
+    //         };
+    //     } else {
+    //         errors.push(syn::Error::new(a.span(), "self argument not supported"));
+    //     }
+    // });
 
     // Prepare the spec parameters.
     let spec_ident = format_ident!("__SPEC_{}", ident.to_string().to_uppercase());
@@ -135,8 +149,7 @@ fn wrap_and_spec(
     // Prepare the wrap parameters.
     let wrap_export_name = format!("{}", ident);
     let wrap_ident = format_ident!("__{}", ident);
-    let wrap_inputs_env_ident = inputs
-        .first()
+    let wrap_inputs_env_ident = env_input
         .and_then(|f| {
             if let &FnArg::Typed(pat_type) = &f {
                 if let Pat::Ident(pat_ident) = &*pat_type.pat {
@@ -145,12 +158,9 @@ fn wrap_and_spec(
             }
             None
         })
-        .expect("only accepts functions with first parameter as the Env type");
-    let wrap_inputs = inputs.iter().enumerate().map(|(i, f)| {
+        .unwrap();
+    let wrap_arg_inputs = inputs.iter().skip(1).map(|f| {
         if let &FnArg::Typed(pat_type) = &f {
-            if i == 0 {
-                return f.clone();
-            }
             return FnArg::Typed(PatType {
                 attrs: pat_type.attrs.clone(),
                 pat: pat_type.pat.clone(),
@@ -178,31 +188,25 @@ fn wrap_and_spec(
         panic!("only accepts functions without a self argument")
     });
 
+    // If errors have occurred, render them instead.
+    if !errors.is_empty() {
+        let compile_errors = errors.iter().map(syn::Error::to_compile_error);
+        return quote! { #(#compile_errors)* };
+    }
+
     // Output.
-    match output {
-        ReturnType::Default => quote! {
-            #[export_name = #wrap_export_name]
-            fn #wrap_ident(#(#wrap_inputs),*) -> stellar_contract_sdk::RawVal {
-                #call(#(#wrap_call_inputs),*);
-                stellar_contract_sdk::RawVal::from_void()
-            }
-            #[cfg(target_family = "wasm")]
-            #[cfg_attr(target_family = "wasm", link_section = "contractspecv0")]
-            pub static #spec_ident: [u8; #spec_inputs_literal_size] = *#spec_inputs_literal;
-        },
-        ReturnType::Type(_, _) => quote! {
-            #[export_name = #wrap_export_name]
-            fn #wrap_ident(#(#wrap_inputs),*) -> stellar_contract_sdk::RawVal {
-                <_ as stellar_contract_sdk::IntoVal<stellar_contract_sdk::Env, stellar_contract_sdk::RawVal>>::into_val(
-                    #call(
-                        #wrap_inputs_env_ident.clone(),
-                        #(#wrap_call_inputs),*
-                    ),
-                    &#wrap_inputs_env_ident
-                )
-            }
-            #[cfg_attr(target_family = "wasm", link_section = "contractspecv0")]
-            pub static #spec_ident: [u8; #spec_inputs_literal_size] = *#spec_inputs_literal;
-        },
+    quote! {
+        #[export_name = #wrap_export_name]
+        fn #wrap_ident(#wrap_inputs_env_ident: stellar_contract_sdk::Env, #(#wrap_arg_inputs),*) -> stellar_contract_sdk::RawVal {
+            <_ as stellar_contract_sdk::IntoVal<stellar_contract_sdk::Env, stellar_contract_sdk::RawVal>>::into_val(
+                #call(
+                    #wrap_inputs_env_ident.clone(),
+                    #(#wrap_call_inputs),*
+                ),
+                &#wrap_inputs_env_ident
+            )
+        }
+        #[cfg_attr(target_family = "wasm", link_section = "contractspecv0")]
+        pub static #spec_ident: [u8; #spec_inputs_literal_size] = *#spec_inputs_literal;
     }
 }
