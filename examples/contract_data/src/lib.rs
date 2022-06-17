df016ee1739a17cafd9d90e2c744ed6988538d79
Jonathan Jove <jon@stellar.org>
2022-06-17 15:25:37 -0500

    Change from no_mangle to contractfn

--- examples/contract_data/src/lib.rs
+++ examples/contract_data/src/lib.rs
@@ -1,12 +1,12 @@
 #![no_std]
-use stellar_contract_sdk::{Env, Symbol};
+use stellar_contract_sdk::{contractfn, Env, Symbol};
 
-#[no_mangle]
+#[contractfn]
 pub fn put(e: Env, key: Symbol, val: Symbol) {
     e.put_contract_data(key, val)
 }
 
-#[no_mangle]
+#[contractfn]
 pub fn del(e: Env, key: Symbol) {
     e.del_contract_data(key)
 }
