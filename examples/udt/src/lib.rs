67b9e734c34a9c7ad0bfd120c0880906301505a1
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-16 18:00:12 -0700

    Add an example of manually encoding a user-defined type (#135)

---
+++ examples/udt/src/lib.rs
@@ -0,0 +1,50 @@
+#![no_std]
+use stellar_contract_sdk::{Env, EnvVal, IntoEnvVal, IntoVal, RawVal, TryFromVal};
+
+pub struct Udt {
+    pub a: i64,
+    pub b: i64,
+}
+
+// TODO: These trait implementations will be hidden behind a macro, and probably
+// be implemented using a Map rather than a tuple to provide for better data
+// migration.
+
+impl TryFrom<EnvVal> for Udt {
+    type Error = ();
+
+    fn try_from(ev: EnvVal) -> Result<Self, Self::Error> {
+        let (a, b): (i64, i64) = ev.try_into()?;
+        Ok(Udt { a, b })
+    }
+}
+
+impl IntoEnvVal<Env, RawVal> for Udt {
+    fn into_env_val(self, env: &Env) -> EnvVal {
+        (self.a, self.b).into_env_val(env)
+    }
+}
+
+#[no_mangle]
+pub fn add(e: Env, udt: RawVal) -> RawVal {
+    let udt: Udt = Udt::try_from_val(&e, udt).unwrap();
+
+    let c = udt.a + udt.b;
+
+    return c.into_val(&e);
+}
+
+#[cfg(test)]
+mod test {
+    use super::{add, Udt};
+    use stellar_contract_sdk::{Env, IntoVal, TryFromVal};
+
+    #[test]
+    fn test_add() {
+        let e = Env::default();
+        let udt = Udt { a: 10, b: 12 }.into_val(&e);
+        let z = add(e.clone(), udt);
+        let z = i64::try_from_val(&e, z).unwrap();
+        assert!(z == 22);
+    }
+}
