a59719058014852f60aa88b2337e57a8ba449f8b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-22 17:17:23 -0700

    wip

--- sdk/tests/macros_spec_udt.rs
+++ sdk/tests/macros_spec_udt.rs
@@ -2,10 +2,11 @@ use std::io::Cursor;
 
 use stellar_contract_sdk::{contractfn, Env, EnvVal, IntoEnvVal, IntoVal, RawVal, TryFromVal};
 use stellar_xdr::{
-    ReadXdr, SpecEntry, SpecEntryFunction, SpecEntryFunctionV0, SpecTypeDef, SpecTypeUdt,
+    ReadXdr, SpecEntry, SpecEntryFunction, SpecEntryFunctionV0, SpecTypeDef, SpecTypeTuple,
+    SpecTypeUdt,
 };
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Udt {
     a: i32,
     b: i32,
@@ -27,21 +28,18 @@ impl IntoEnvVal<Env, RawVal> for Udt {
 }
 
 #[contractfn]
-pub fn add(a: Udt, b: Udt) -> Udt {
-    Udt {
-        a: a.a + b.a,
-        b: a.b + b.b,
-    }
+pub fn add(a: Udt, b: Udt) -> (Udt, Udt) {
+    (a, b)
 }
 
 #[test]
 fn test_functional() {
     let e = Env::default();
-    let a = Udt { a: 5, b: 7 }.into_val(&e);
-    let b = Udt { a: 10, b: 14 }.into_val(&e);
-    let c = __add(e.clone(), a, b);
-    let c = Udt::try_from_val(&e, c).unwrap();
-    assert_eq!(c, Udt { a: 15, b: 21 });
+    let a = Udt { a: 5, b: 7 };
+    let b = Udt { a: 10, b: 14 };
+    let c = __add(e.clone(), a.into_val(&e), b.into_val(&e));
+    let c = <(Udt, Udt)>::try_from_val(&e, c).unwrap();
+    assert_eq!(c, (a, b));
 }
 
 #[test]
@@ -59,9 +57,18 @@ fn test_spec() {
         ]
         .try_into()
         .unwrap(),
-        output_types: vec![SpecTypeDef::Udt(SpecTypeUdt {
-            name: "Udt".try_into().unwrap(),
-        })]
+        output_types: vec![SpecTypeDef::Tuple(Box::new(SpecTypeTuple {
+            value_types: vec![
+                SpecTypeDef::Udt(SpecTypeUdt {
+                    name: "Udt".try_into().unwrap(),
+                }),
+                SpecTypeDef::Udt(SpecTypeUdt {
+                    name: "Udt".try_into().unwrap(),
+                }),
+            ]
+            .try_into()
+            .unwrap(),
+        }))]
         .try_into()
         .unwrap(),
     }));
