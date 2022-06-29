097075b0c05bcf3de8aa7bdf49f603b30f5b57fb
Siddharth Suresh <siddharth@stellar.org>
2022-06-29 15:23:52 -0700

    use Binary in contract

--- examples/create_contract/src/lib.rs
+++ examples/create_contract/src/lib.rs
@@ -1,12 +1,9 @@
 #![no_std]
-use stellar_contract_sdk::{contractfn, Env, Object};
+use stellar_contract_sdk::{contractfn, Binary, Env};
 
 // Note that anyone can create a contract here with any salt, so a users call to
 // this could be frontrun and the same salt taken.
 #[contractfn]
-pub fn create(e: Env, c: Object, s: Object) {
-    e.create_contract_using_parent_id(
-        c.in_env(&e).try_into().unwrap(),
-        s.in_env(&e).try_into().unwrap(),
-    )
+pub fn create(e: Env, c: Binary, s: Binary) {
+    e.create_contract_using_parent_id(c, s)
 }
