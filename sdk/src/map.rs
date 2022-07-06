b1c9a8a4f598bb09a2f6227b1f88ab06084dd920
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-05 18:00:16 -0700

    Replace some .ok() usage with including a constraint on Debug (#169)

--- sdk/src/map.rs
+++ sdk/src/map.rs
@@ -1,4 +1,4 @@
-use core::{cmp::Ordering, marker::PhantomData};
+use core::{cmp::Ordering, fmt::Debug, marker::PhantomData};
 
 use super::{
     env::internal::Env as _, xdr::ScObjectType, ConversionError, Env, EnvObj, EnvVal,
@@ -101,10 +101,13 @@ impl<K: IntoTryFromVal, V: IntoTryFromVal> Map<K, V> {
     }
 
     #[inline(always)]
-    pub fn get(&self, k: K) -> V {
+    pub fn get(&self, k: K) -> V
+    where
+        V::Error: Debug,
+    {
         let env = self.env();
         let v = env.map_get(self.0.to_tagged(), k.into_val(env));
-        V::try_from_val(env, v).ok().unwrap()
+        V::try_from_val(env, v).unwrap()
     }
 
     #[inline(always)]
