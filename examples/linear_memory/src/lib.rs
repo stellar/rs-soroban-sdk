fd111880929b5716d207c162f517674e1cd3f05c
Jay Geng <jay@stellar.org>
2022-07-08 15:08:05 -0400

    Fix formatting

--- examples/linear_memory/src/lib.rs
+++ examples/linear_memory/src/lib.rs
@@ -1,25 +1,25 @@
 #![no_std]
-use stellar_contract_sdk::{contractfn, Env, Binary, FixedLengthBinary};
+use stellar_contract_sdk::{contractfn, Binary, Env, FixedLengthBinary};
 
 #[contractfn]
 pub fn bin_new(e: Env, len: u32) -> Binary {
-    let buf: [u8; 4] = [0,1,2,3];
+    let buf: [u8; 4] = [0, 1, 2, 3];
     e.binary_new_from_linear_memory(buf.as_ptr() as u32, len)
 }
 
 #[contractfn]
 pub fn from_guest(e: Env, b: Binary, b_pos: u32, lm_pos: u32, len: u32) -> Binary {
-    let buf: [u8; 4] = [0,1,2,3];
+    let buf: [u8; 4] = [0, 1, 2, 3];
     assert!(lm_pos + len > buf.len() as u32);
-    let lm_pos: u32 = unsafe { buf.as_ptr().add(lm_pos as usize) as u32};
+    let lm_pos: u32 = unsafe { buf.as_ptr().add(lm_pos as usize) as u32 };
     e.binary_copy_from_linear_memory(b, b_pos, lm_pos, len)
 }
 
 #[contractfn]
-pub fn to_guest(e: Env, b: Binary, b_pos: u32, lm_pos: u32, len: u32){
-    let buf: [u8; 4] = [0; 4];    
-    assert!(lm_pos + len > buf.len() as u32); 
-    let lm_pos: u32 = unsafe { buf.as_ptr().add(lm_pos as usize) as u32};
+pub fn to_guest(e: Env, b: Binary, b_pos: u32, lm_pos: u32, len: u32) {
+    let buf: [u8; 4] = [0; 4];
+    assert!(lm_pos + len > buf.len() as u32);
+    let lm_pos: u32 = unsafe { buf.as_ptr().add(lm_pos as usize) as u32 };
     e.binary_copy_to_linear_memory(b.clone(), b_pos, lm_pos, len);
     for idx in lm_pos..buf.len() as u32 {
         assert_eq!(buf[idx as usize], b.get(b_pos + idx));
