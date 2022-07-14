0f7771fbc61734d9f51bd9d30f75b5e9710a4442
Siddharth Suresh <siddharth@stellar.org>
2022-07-14 13:38:25 -0700

    Stop using parent (#235)
    Co-authored-by: Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>

--- examples/create_contract/src/lib.rs
+++ examples/create_contract/src/lib.rs
@@ -8,6 +8,6 @@ impl Contract {
     // Note that anyone can create a contract here with any salt, so a users call to
     // this could be frontrun and the same salt taken.
     pub fn create(e: Env, c: Binary, s: Binary) {
-        e.create_contract_using_parent_id(c, s);
+        e.create_contract_from_contract(c, s);
     }
 }
