ab390bfff98145e3daee87ad02a2b24e88e637df
Graydon Hoare <graydon@pobox.com>
2022-07-15 22:21:46 -0700

    Fix backwards requirements and confusing names in linear-memory example.

--- examples/linear_memory/src/lib.rs
+++ examples/linear_memory/src/lib.rs
@@ -12,17 +12,17 @@ impl Contract {
         e.binary_new_from_linear_memory(buf.as_ptr() as u32, len)
     }
 
-    pub fn from_guest(e: Env, b: Binary, b_pos: u32, lm_pos: u32, len: u32) -> Binary {
+    pub fn from_guest(e: Env, b: Binary, b_pos: u32, buf_off: u32, len: u32) -> Binary {
         let buf: [u8; 4] = [0, 1, 2, 3];
-        assert!(lm_pos + len > buf.len() as u32);
-        let lm_pos: u32 = unsafe { buf.as_ptr().add(lm_pos as usize) as u32 };
+        assert!(buf_off + len <= buf.len() as u32);
+        let lm_pos: u32 = unsafe { buf.as_ptr().add(buf_off as usize) as u32 };
         e.binary_copy_from_linear_memory(b, b_pos, lm_pos, len)
     }
 
-    pub fn to_guest(e: Env, b: Binary, b_pos: u32, lm_pos: u32, len: u32) {
+    pub fn to_guest(e: Env, b: Binary, b_pos: u32, buf_off: u32, len: u32) {
         let buf: [u8; 4] = [0; 4];
-        assert!(lm_pos + len > buf.len() as u32);
-        let lm_pos: u32 = unsafe { buf.as_ptr().add(lm_pos as usize) as u32 };
+        assert!(buf_off + len <= buf.len() as u32);
+        let lm_pos: u32 = unsafe { buf.as_ptr().add(buf_off as usize) as u32 };
         e.binary_copy_to_linear_memory(b.clone(), b_pos, lm_pos, len);
         for idx in lm_pos..buf.len() as u32 {
             assert!(buf[idx as usize] == b.get(b_pos + idx));
