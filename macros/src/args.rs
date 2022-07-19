a629470f95756c14dbfc39f01f5c5b641ffa232f
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-19 10:50:24 -0700

    add use

--- macros/src/args.rs
+++ macros/src/args.rs
@@ -1,23 +1,23 @@
 use quote::format_ident;
-use syn::{LitStr, NestedMeta};
+use syn::{Lit, LitStr, Meta, NestedMeta};
 
 // TODO: Consider replacing this with crates.io/crates/darling.
 
 pub fn get_str(args: &Vec<NestedMeta>, name: &str) -> Option<LitStr> {
     args.iter().find_map(|m| match m {
-        syn::NestedMeta::Meta(m) => match m {
-            syn::Meta::NameValue(nv) => {
+        NestedMeta::Meta(m) => match m {
+            Meta::NameValue(nv) => {
                 if nv.path.is_ident(&format_ident!("{}", name)) {
                     match &nv.lit {
-                        syn::Lit::Str(s) => Some(s.clone()),
+                        Lit::Str(s) => Some(s.clone()),
                         _ => None,
                     }
                 } else {
                     None
                 }
             }
-            syn::Meta::Path(_) | syn::Meta::List(_) => None,
+            Meta::Path(_) | Meta::List(_) => None,
         },
-        syn::NestedMeta::Lit(_) => None,
+        NestedMeta::Lit(_) => None,
     })
 }
