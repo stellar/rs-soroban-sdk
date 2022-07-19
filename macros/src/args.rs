c870b02e2a3ef8cc5a2340d4a50e0a40c62ec2aa
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-19 10:51:27 -0700

    use typedef

--- macros/src/args.rs
+++ macros/src/args.rs
@@ -1,9 +1,9 @@
 use quote::format_ident;
-use syn::{Lit, LitStr, Meta, NestedMeta};
+use syn::{AttributeArgs, Lit, LitStr, Meta, NestedMeta};
 
 // TODO: Consider replacing this with crates.io/crates/darling.
 
-pub fn get_str(args: &Vec<NestedMeta>, name: &str) -> Option<LitStr> {
+pub fn get_str(args: &AttributeArgs, name: &str) -> Option<LitStr> {
     args.iter().find_map(|m| match m {
         NestedMeta::Meta(m) => match m {
             Meta::NameValue(nv) => {
