79046e2e6f84ec488300bfacb61d85d3c2e0e24f
Jay Geng <jay@stellar.org>
2022-07-20 09:49:16 -0400

    Resolve differences with upstream

--- examples/call/src/lib.rs
+++ examples/call/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use stellar_contract_sdk::{contract, contractimpl, Env, Symbol, Vec};
+use stellar_contract_sdk::{contract, contractimpl, Env, Symbol, Vec, IntoEnvVal};
 
 contract!();
 
@@ -10,8 +10,8 @@ impl Contract {
     pub fn delegate(e: Env, val: u32) {
         let buff = [1u8; 32];
         let cid = e.binary_new_from_linear_memory(buff.as_ptr() as u32, 32);
-        let fun = Symbol::from_str("vec_err");
-        let args = Vec::from_array(&e, [val; 1]);
-        e.call(cid, fun.into(), args.into());
+        let fun = Symbol::from_str("vec_err");        
+        let args = Vec::from_array(&e, [val.into_env_val(&e); 1]);
+        e.call::<Vec<u32>>(cid, fun.into(), args);
     }
 }
--- sdk/src/env.rs
+++ sdk/src/env.rs
@@ -199,11 +199,6 @@ impl Env {
         new_obj.in_env(self).try_into().unwrap()
     }
 
-    pub fn call(&self, contract: Binary, func: Symbol, args: EnvObj) -> RawVal {
-        let contract_bin: Object = RawVal::from(contract).try_into().unwrap();
-        internal::Env::call(self, contract_bin, func, args.into())
-    }
-
     pub fn log_value<V: IntoVal<Env, RawVal>>(&self, v: V) {
         internal::Env::log_value(self, v.into_val(self));
     }
