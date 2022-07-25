fd505ce5a811b589f3c05d6bf1ffcde7910ed562
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-25 15:01:22 -0700

    Make Env default empty recording storage

--- sdk/tests/assert_panic.rs
+++ sdk/tests/assert_panic.rs
@@ -6,34 +6,34 @@ use stellar_xdr::ScHostStorageErrorCode;
 
 #[test]
 fn test_assert_panic_with_status() {
-    let e = Env::with_empty_recording_storage();
+    let e = Env::default();
     let status: Status = ScHostStorageErrorCode::AccessToUnknownEntry.into();
     e.assert_panic_with_status(status, |_env| panic_any(status));
 }
 
 #[test]
 fn test_assert_panic_with_string() {
-    let e = Env::with_empty_recording_storage();
+    let e = Env::default();
     e.assert_panic_with_string("oh no", |_env| panic!("oh no"));
 }
 
 #[test]
 fn test_assert_panic_with_fmt_string() {
-    let e = Env::with_empty_recording_storage();
+    let e = Env::default();
     e.assert_panic_with_string("oh no 123", |_env| panic!("oh no {}", 123));
 }
 
 #[test]
 #[should_panic]
 fn test_assert_panic_wrong_string() {
-    let e = Env::with_empty_recording_storage();
+    let e = Env::default();
     e.assert_panic_with_string("oh no", |_env| panic!("and yet"));
 }
 
 #[test]
 #[should_panic]
 fn test_assert_panic_with_wrong_status() {
-    let e = Env::with_empty_recording_storage();
+    let e = Env::default();
     let expected: Status = ScHostStorageErrorCode::AccessToUnknownEntry.into();
     let got: Status = ScHostStorageErrorCode::GetOnDeletedKey.into();
     e.assert_panic_with_status(expected, |_env| panic_any(got));
@@ -42,7 +42,7 @@ fn test_assert_panic_with_wrong_status() {
 #[test]
 #[should_panic]
 fn test_assert_panic_with_wrong_type() {
-    let e = Env::with_empty_recording_storage();
+    let e = Env::default();
     let expected: Status = ScHostStorageErrorCode::AccessToUnknownEntry.into();
     e.assert_panic_with_status(expected, |_env| panic_any(10));
 }
