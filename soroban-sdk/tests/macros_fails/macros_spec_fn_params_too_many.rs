047c3565be604aede74ab6427622009cf4fc50ef
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-08 16:40:38 -0700

    Add compiler error when fn name too long and param count too many (#402)

---
+++ soroban-sdk/tests/macros_fails/macros_spec_fn_params_too_many.rs
@@ -0,0 +1,24 @@
+use soroban_sdk::contractimpl;
+
+pub struct Contract;
+
+#[contractimpl]
+impl Contract {
+    pub fn pay(
+        p1: u32,
+        p2: u32,
+        p3: u32,
+        p4: u32,
+        p5: u32,
+        p6: u32,
+        p7: u32,
+        p8: u32,
+        p9: u32,
+        p10: u32,
+        p11: u32,
+    ) {
+        unimplemented!()
+    }
+}
+
+pub fn main() {}
