3218009f34c486849e3ee6562208dfe867e28b22
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-08 11:45:30 -0700

    add test for existing behavior

---
+++ soroban-sdk/tests/macros_fails/macros_spec_toolong.rs
@@ -0,0 +1,12 @@
+use soroban_sdk::contractimpl;
+
+pub struct Contract;
+
+#[contractimpl]
+impl Contract {
+    pub fn pay_to_destionation() {
+        unimplemented!()
+    }
+}
+
+pub fn main() {}
