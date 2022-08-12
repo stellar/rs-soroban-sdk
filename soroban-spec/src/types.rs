4622b5c6081599bfee8731601d416340ff3d7cc0
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-11 18:21:03 -0700

    wip

---
+++ soroban-spec/src/types.rs
@@ -0,0 +1,119 @@
+use std::borrow::Cow;
+
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+use stellar_xdr::{
+    ScSpecEntry, ScSpecFunctionV0, ScSpecTypeDef, ScSpecUdtStructV0, ScSpecUdtUnionV0,
+};
+
+pub fn generate_types(specs: &[ScSpecEntry], wasm: Option<&Cow<'_, str>>) -> TokenStream {
+    let mut spec_fns = Vec::new();
+    let mut spec_structs = Vec::new();
+    let mut spec_unions = Vec::new();
+    for s in specs {
+        match s {
+            ScSpecEntry::FunctionV0(f) => spec_fns.push(f),
+            ScSpecEntry::UdtStructV0(s) => spec_structs.push(s),
+            ScSpecEntry::UdtUnionV0(u) => spec_unions.push(u),
+        }
+    }
+    let trait_ = generate_trait("Contract", &spec_fns);
+    let structs = spec_structs.iter().map(|s| generate_struct(s));
+    let unions = spec_unions.iter().map(|s| generate_union(s));
+    quote! {
+        #[contractclient]
+        #[contractwasm(wasm = #wasm)]
+        #trait_
+        #(#structs)*
+        #(#unions)*
+    }
+}
+
+/// Constructs a token stream containing a single trait that has a function for
+/// every function spec.
+pub fn generate_trait(name: &str, specs: &[&ScSpecFunctionV0]) -> TokenStream {
+    let trait_ident = format_ident!("{}", name);
+    let fns: Vec<_> = specs
+        .iter()
+        .map(|s| {
+            let fn_ident = format_ident!("{}", s.name.to_string().unwrap());
+            let fn_inputs = s.input_types.iter().enumerate().map(|(i, t)| {
+                let name = format_ident!("a{}", i);
+                let type_ident = generate_type_ident(t);
+                quote! { #name: #type_ident }
+            });
+            let fn_outputs = s
+                .output_types
+                .iter()
+                .enumerate()
+                .map(|(i, t)| generate_type_ident(t));
+            quote! {
+                fn #fn_ident(#(#fn_inputs),*) -> (#(#fn_outputs),*);
+            }
+        })
+        .collect();
+    quote! {
+        pub trait #trait_ident {
+            #(
+                #fns
+            )*
+        }
+    }
+}
+
+/// Constructs a token stream containing a single struct that mirrors the struct
+/// spec.
+pub fn generate_struct(spec: &ScSpecUdtStructV0) -> TokenStream {
+    quote! {}
+}
+
+/// Constructs a token stream containing a single enum that mirrors the union
+/// spec.
+pub fn generate_union(spec: &ScSpecUdtUnionV0) -> TokenStream {
+    quote! {}
+}
+
+pub fn generate_type_ident(spec: &ScSpecTypeDef) -> TokenStream {
+    match spec {
+        ScSpecTypeDef::U64 => quote! { u64 },
+        ScSpecTypeDef::I64 => quote! { i64 },
+        ScSpecTypeDef::U32 => quote! { u32 },
+        ScSpecTypeDef::I32 => quote! { i32 },
+        ScSpecTypeDef::Bool => quote! { bool },
+        ScSpecTypeDef::Symbol => quote! { ::soroban_sdk::Symbol },
+        ScSpecTypeDef::Bitset => quote! { ::soroban_sdk::Bitset },
+        ScSpecTypeDef::Status => quote! { ::soroban_sdk::Status },
+        ScSpecTypeDef::Binary => quote! { ::soroban_sdk::Bytes },
+        ScSpecTypeDef::BigInt => quote! { ::soroban_sdk::BigInt },
+        ScSpecTypeDef::Option(o) => {
+            let value_ident = generate_type_ident(&o.value_type);
+            quote! { Option<#value_ident> }
+        }
+        ScSpecTypeDef::Result(r) => {
+            let ok_ident = generate_type_ident(&r.ok_type);
+            let error_ident = generate_type_ident(&r.error_type);
+            quote! { Result<#ok_ident, #error_ident> }
+        }
+        ScSpecTypeDef::Vec(v) => {
+            let element_ident = generate_type_ident(&v.element_type);
+            quote! { ::soroban_sdk::Vec<#element_ident> }
+        }
+        ScSpecTypeDef::Map(m) => {
+            let key_ident = generate_type_ident(&m.key_type);
+            let value_ident = generate_type_ident(&m.value_type);
+            quote! { ::soroban_sdk::Map<#key_ident, #value_ident> }
+        }
+        ScSpecTypeDef::Set(s) => {
+            let element_ident = generate_type_ident(&s.element_type);
+            quote! { ::soroban_sdk::Set<#element_ident> }
+        }
+        ScSpecTypeDef::Tuple(t) => {
+            let type_idents = t.value_types.iter().map(|vt| generate_type_ident(&vt));
+            quote! { (#(#type_idents,)*) }
+        }
+        ScSpecTypeDef::Udt(u) => {
+            let ident = format_ident!("{}", u.name.to_string().unwrap());
+            quote! { #ident }
+        }
+    }
+}
