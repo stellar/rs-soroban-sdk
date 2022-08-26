f03e22a50197daffcc9ee326c4371127b79cc1ca
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-26 10:38:42 -0700

    Remove "Spec" from names

--- soroban-spec/src/gen/json.rs
+++ soroban-spec/src/gen/json.rs
@@ -8,7 +8,7 @@ use soroban_env_host::xdr::{
 #[serde(rename_all = "camelCase")]
 pub struct StructField {
     name: String,
-    value: SpecType,
+    value: Type,
 }
 
 impl TryFrom<&ScSpecUdtStructFieldV0> for StructField {
@@ -17,7 +17,7 @@ impl TryFrom<&ScSpecUdtStructFieldV0> for StructField {
     fn try_from(f: &ScSpecUdtStructFieldV0) -> Result<Self, Self::Error> {
         Ok(StructField {
             name: f.name.to_string()?,
-            value: SpecType::try_from(&f.type_)?,
+            value: Type::try_from(&f.type_)?,
         })
     }
 }
@@ -26,7 +26,7 @@ impl TryFrom<&ScSpecUdtStructFieldV0> for StructField {
 #[serde(rename_all = "camelCase")]
 pub struct FunctionInput {
     name: String,
-    value: SpecType,
+    value: Type,
 }
 
 impl TryFrom<&ScSpecFunctionInputV0> for FunctionInput {
@@ -35,7 +35,7 @@ impl TryFrom<&ScSpecFunctionInputV0> for FunctionInput {
     fn try_from(f: &ScSpecFunctionInputV0) -> Result<Self, Self::Error> {
         Ok(FunctionInput {
             name: f.name.to_string()?,
-            value: SpecType::try_from(&f.type_)?,
+            value: Type::try_from(&f.type_)?,
         })
     }
 }
@@ -44,7 +44,7 @@ impl TryFrom<&ScSpecFunctionInputV0> for FunctionInput {
 #[serde(rename_all = "camelCase")]
 pub struct UnionCase {
     name: String,
-    values: Vec<SpecType>,
+    values: Vec<Type>,
 }
 
 impl TryFrom<&ScSpecUdtUnionCaseV0> for UnionCase {
@@ -56,7 +56,7 @@ impl TryFrom<&ScSpecUdtUnionCaseV0> for UnionCase {
             values: c
                 .type_
                 .as_ref()
-                .map(SpecType::try_from)
+                .map(Type::try_from)
                 .transpose()?
                 .into_iter()
                 .collect(),
@@ -67,7 +67,7 @@ impl TryFrom<&ScSpecUdtUnionCaseV0> for UnionCase {
 #[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "camelCase")]
-pub enum SpecType {
+pub enum Type {
     U64,
     I64,
     U32,
@@ -78,39 +78,23 @@ pub enum SpecType {
     Status,
     Bytes,
     BigInt,
-    Map {
-        key: Box<SpecType>,
-        value: Box<SpecType>,
-    },
-    Option {
-        value: Box<SpecType>,
-    },
-    Result {
-        value: Box<SpecType>,
-        error: Box<SpecType>,
-    },
-    Set {
-        element: Box<SpecType>,
-    },
-    Vec {
-        element: Box<SpecType>,
-    },
-    Tuple {
-        elements: Vec<SpecType>,
-    },
-    Custom {
-        name: String,
-    },
+    Map { key: Box<Type>, value: Box<Type> },
+    Option { value: Box<Type> },
+    Result { value: Box<Type>, error: Box<Type> },
+    Set { element: Box<Type> },
+    Vec { element: Box<Type> },
+    Tuple { elements: Vec<Type> },
+    Custom { name: String },
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "camelCase")]
-pub enum EntryType {
+pub enum Entry {
     Function {
         name: String,
         inputs: Vec<FunctionInput>,
-        outputs: Vec<SpecType>,
+        outputs: Vec<Type>,
     },
     Struct {
         name: String,
@@ -122,58 +106,58 @@ pub enum EntryType {
     },
 }
 
-impl TryFrom<&ScSpecTypeDef> for SpecType {
+impl TryFrom<&ScSpecTypeDef> for Type {
     type Error = Error;
 
     fn try_from(spec: &ScSpecTypeDef) -> Result<Self, Self::Error> {
         match spec {
-            ScSpecTypeDef::Map(map) => Ok(SpecType::Map {
-                key: Box::new(SpecType::try_from(map.key_type.as_ref())?),
-                value: Box::new(SpecType::try_from(map.value_type.as_ref())?),
+            ScSpecTypeDef::Map(map) => Ok(Type::Map {
+                key: Box::new(Type::try_from(map.key_type.as_ref())?),
+                value: Box::new(Type::try_from(map.value_type.as_ref())?),
             }),
-            ScSpecTypeDef::Option(opt) => Ok(SpecType::Option {
-                value: Box::new(SpecType::try_from(opt.value_type.as_ref())?),
+            ScSpecTypeDef::Option(opt) => Ok(Type::Option {
+                value: Box::new(Type::try_from(opt.value_type.as_ref())?),
             }),
-            ScSpecTypeDef::Result(res) => Ok(SpecType::Result {
-                value: Box::new(SpecType::try_from(res.ok_type.as_ref())?),
-                error: Box::new(SpecType::try_from(res.error_type.as_ref())?),
+            ScSpecTypeDef::Result(res) => Ok(Type::Result {
+                value: Box::new(Type::try_from(res.ok_type.as_ref())?),
+                error: Box::new(Type::try_from(res.error_type.as_ref())?),
             }),
-            ScSpecTypeDef::Set(set) => Ok(SpecType::Set {
-                element: Box::new(SpecType::try_from(set.element_type.as_ref())?),
+            ScSpecTypeDef::Set(set) => Ok(Type::Set {
+                element: Box::new(Type::try_from(set.element_type.as_ref())?),
             }),
-            ScSpecTypeDef::Tuple(tuple) => Ok(SpecType::Tuple {
+            ScSpecTypeDef::Tuple(tuple) => Ok(Type::Tuple {
                 elements: tuple
                     .value_types
                     .iter()
-                    .map(SpecType::try_from)
+                    .map(Type::try_from)
                     .collect::<Result<Vec<_>, Error>>()?,
             }),
-            ScSpecTypeDef::Vec(vec) => Ok(SpecType::Vec {
-                element: Box::new(SpecType::try_from(vec.element_type.as_ref())?),
+            ScSpecTypeDef::Vec(vec) => Ok(Type::Vec {
+                element: Box::new(Type::try_from(vec.element_type.as_ref())?),
             }),
-            ScSpecTypeDef::Udt(udt) => Ok(SpecType::Custom {
+            ScSpecTypeDef::Udt(udt) => Ok(Type::Custom {
                 name: udt.name.to_string()?,
             }),
-            ScSpecTypeDef::U64 => Ok(SpecType::U64),
-            ScSpecTypeDef::I64 => Ok(SpecType::I64),
-            ScSpecTypeDef::U32 => Ok(SpecType::U32),
-            ScSpecTypeDef::I32 => Ok(SpecType::I32),
-            ScSpecTypeDef::Bool => Ok(SpecType::Bool),
-            ScSpecTypeDef::Symbol => Ok(SpecType::Symbol),
-            ScSpecTypeDef::Bitset => Ok(SpecType::Bitset),
-            ScSpecTypeDef::Status => Ok(SpecType::Status),
-            ScSpecTypeDef::Bytes => Ok(SpecType::Bytes),
-            ScSpecTypeDef::BigInt => Ok(SpecType::BigInt),
+            ScSpecTypeDef::U64 => Ok(Type::U64),
+            ScSpecTypeDef::I64 => Ok(Type::I64),
+            ScSpecTypeDef::U32 => Ok(Type::U32),
+            ScSpecTypeDef::I32 => Ok(Type::I32),
+            ScSpecTypeDef::Bool => Ok(Type::Bool),
+            ScSpecTypeDef::Symbol => Ok(Type::Symbol),
+            ScSpecTypeDef::Bitset => Ok(Type::Bitset),
+            ScSpecTypeDef::Status => Ok(Type::Status),
+            ScSpecTypeDef::Bytes => Ok(Type::Bytes),
+            ScSpecTypeDef::BigInt => Ok(Type::BigInt),
         }
     }
 }
 
-impl TryFrom<&ScSpecEntry> for EntryType {
+impl TryFrom<&ScSpecEntry> for Entry {
     type Error = Error;
 
     fn try_from(spec: &ScSpecEntry) -> Result<Self, Self::Error> {
         match spec {
-            ScSpecEntry::FunctionV0(f) => Ok(EntryType::Function {
+            ScSpecEntry::FunctionV0(f) => Ok(Entry::Function {
                 name: f.name.to_string()?,
                 inputs: f
                     .inputs
@@ -183,10 +167,10 @@ impl TryFrom<&ScSpecEntry> for EntryType {
                 outputs: f
                     .outputs
                     .iter()
-                    .map(SpecType::try_from)
+                    .map(Type::try_from)
                     .collect::<Result<Vec<_>, Error>>()?,
             }),
-            ScSpecEntry::UdtStructV0(s) => Ok(EntryType::Struct {
+            ScSpecEntry::UdtStructV0(s) => Ok(Entry::Struct {
                 name: s.name.to_string()?,
                 fields: s
                     .fields
@@ -194,7 +178,7 @@ impl TryFrom<&ScSpecEntry> for EntryType {
                     .map(StructField::try_from)
                     .collect::<Result<Vec<_>, Error>>()?,
             }),
-            ScSpecEntry::UdtUnionV0(u) => Ok(EntryType::Union {
+            ScSpecEntry::UdtUnionV0(u) => Ok(Entry::Union {
                 name: u.name.to_string()?,
                 cases: u
                     .cases
@@ -210,7 +194,7 @@ impl TryFrom<&ScSpecEntry> for EntryType {
 mod test {
     use pretty_assertions::assert_eq;
 
-    use super::EntryType;
+    use super::Entry;
 
     const EXAMPLE_WASM: &[u8] =
         include_bytes!("../../../target/wasm32-unknown-unknown/release/example_udt.wasm");
@@ -220,7 +204,7 @@ mod test {
         let entries = crate::read::from_wasm(EXAMPLE_WASM).unwrap();
         let mut json = "".to_string();
         for entry in &entries {
-            let json_entry: EntryType = entry.try_into().unwrap();
+            let json_entry: Entry = entry.try_into().unwrap();
             json.push_str(&serde_json::to_string_pretty(&json_entry).unwrap());
         }
         assert_eq!(
