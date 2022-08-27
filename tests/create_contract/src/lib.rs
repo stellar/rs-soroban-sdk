a4e48056c334e9c56d46ec28d0ca04a057542e0e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-27 12:45:27 -0700

    add ed25519 example

--- tests/create_contract/src/lib.rs
+++ tests/create_contract/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contractimpl, Bytes, BytesN, CurrentNamespace, Env};
+use soroban_sdk::{contractimpl, Bytes, BytesN, CurrentNamespace, Ed25519Namespace, Env};
 
 pub struct Contract;
 
@@ -7,8 +7,14 @@ pub struct Contract;
 impl Contract {
     // Note that anyone can create a contract here with any salt, so a users call to
     // this could be frontrun and the same salt taken.
-    pub fn create(e: Env, c: Bytes, s: BytesN<32>) {
-        let _contract_id = e.contract_id(CurrentNamespace, &s);
-        let _contract_id = e.deployer(CurrentNamespace).deploy_wasm(&s, c);
+    pub fn create(e: Env, wasm: Bytes, salt: BytesN<32>) {
+        let _contract_id = e.contract_id(CurrentNamespace, &salt);
+        let _contract_id = e.deployer(CurrentNamespace).deploy_wasm(&salt, &wasm);
+
+        let public_key = BytesN::from_array(&e, &[0; 32]);
+        let signature = BytesN::from_array(&e, &[0; 64]);
+        let _contract_id = e
+            .deployer(Ed25519Namespace { public_key })
+            .deploy_wasm(&salt, &wasm, signature);
     }
 }
