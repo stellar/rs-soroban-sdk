959f428ed973217c75f4056b88efad1864f2343b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-28 21:27:07 -0700

    fix

--- tests/create_contract/src/lib.rs
+++ tests/create_contract/src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
-use soroban_sdk::{contractimpl, Bytes, BytesN, CurrentNamespace, Ed25519Namespace, Env};
+use soroban_sdk::{
+    contractimpl,
+    deploy::{CurrentNamespace, Ed25519Namespace},
+    Bytes, BytesN, Env,
+};
 
 pub struct Contract;
 
@@ -9,12 +13,12 @@ impl Contract {
     // this could be frontrun and the same salt taken.
     pub fn create(e: Env, wasm: Bytes, salt: BytesN<32>) {
         let _contract_id = e.contract_id(CurrentNamespace, &salt);
-        let _contract_id = e.deployer(CurrentNamespace).deploy_wasm(&salt, &wasm);
+        let _contract_id = e.deployer(CurrentNamespace).deploy(&salt, &wasm);
 
         let public_key = BytesN::from_array(&e, &[0; 32]);
         let signature = BytesN::from_array(&e, &[0; 64]);
         let namespace = Ed25519Namespace { public_key };
         let _contract_id = e.contract_id(&namespace, &salt);
-        let _contract_id = e.deployer(&namespace).deploy_wasm(&salt, &wasm, signature);
+        let _contract_id = e.deployer(&namespace).deploy(&salt, &wasm, signature);
     }
 }
