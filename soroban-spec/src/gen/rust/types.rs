2c93d3cfd13c4a47fa40541ef3cdf29d5e412f09
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-29 20:19:00 -0700

    Fix BytesN<N> generated N type as usize (#523)
    * Fix BytesN<N> generated N type as usize
    
    * Fix it by removing the suffix
    
    * keep it as u32

--- soroban-spec/src/gen/rust/types.rs
+++ soroban-spec/src/gen/rust/types.rs
@@ -1,4 +1,4 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Literal, TokenStream};
 use quote::{format_ident, quote};
 use stellar_xdr::{ScSpecTypeDef, ScSpecUdtStructV0, ScSpecUdtUnionV0};
 
@@ -76,7 +76,7 @@ pub fn generate_type_ident(spec: &ScSpecTypeDef) -> TokenStream {
             quote! { (#(#type_idents,)*) }
         }
         ScSpecTypeDef::BytesN(b) => {
-            let n = b.n;
+            let n = Literal::u32_unsuffixed(b.n);
             quote! { ::soroban_sdk::BytesN<#n> }
         }
         ScSpecTypeDef::Udt(u) => {
