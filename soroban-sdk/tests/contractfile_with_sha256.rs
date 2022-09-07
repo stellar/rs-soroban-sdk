e10f13cbe3c5c2dc3d7a3d0bc3616467352181b0
Siddharth Suresh <siddharth@stellar.org>
2022-09-06 18:25:00 -0700

    Update env (#566)
    * update env
    
    * update tests

--- soroban-sdk/tests/contractfile_with_sha256.rs
+++ soroban-sdk/tests/contractfile_with_sha256.rs
@@ -1,6 +1,6 @@
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/example_add_i32.wasm",
-    sha256 = "d4096b608d95d17aca620e294a3840b683b19badb6e9c0ce110ce8f1ce421b78",
+    sha256 = "3593fc3d411499d3a755d0ce08b32084047e9a61861aed923aa72252c2cef770",
 );
 
 #[test]
