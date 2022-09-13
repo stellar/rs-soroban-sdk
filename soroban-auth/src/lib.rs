98f5eaefe218c1c831a2e01209c544de8a411428
Siddharth Suresh <siddharth@stellar.org>
2022-09-13 13:41:12 -0700

    Remove replay protection from the auth crate

--- soroban-auth/src/lib.rs
+++ soroban-auth/src/lib.rs
@@ -1,60 +1,12 @@
 #![no_std]
 
-use soroban_sdk::{serde::Serialize, Account, BigInt, BytesN, Env, RawVal, Symbol, Vec};
+use soroban_sdk::{serde::Serialize, Account, BytesN, Env, RawVal, Symbol, Vec};
 
 mod public_types;
 pub use crate::public_types::{
     AccountSignatures, Ed25519Signature, Identifier, Signature, SignaturePayload,
     SignaturePayloadV0,
 };
-
-/// Users of this module will need to pass a struct to check_auth that
-/// implements NonceAuth. NonceAuth should manage nonces in the contracts
-/// storage, and wrap the Signature being verified.
-///
-/// ### Examples
-/// ```
-/// use soroban_sdk::{BigInt, Env, contracttype};
-/// use soroban_auth::{check_auth, Identifier, Signature, NonceAuth};
-///
-/// #[contracttype]
-/// pub enum DataKey {
-///     Acc(Identifier),
-///     Nonce(Identifier)
-/// }
-/// struct WrappedAuth(Signature);
-///
-/// impl NonceAuth for WrappedAuth {
-///     fn read_nonce(e: &Env, id: Identifier) -> BigInt {
-///        let key = DataKey::Nonce(id);
-///        if let Some(nonce) = e.contract_data().get(key) {
-///             nonce.unwrap()
-///        } else {
-///             BigInt::zero(e)
-///        }
-///     }
-///    
-///     fn read_and_increment_nonce(&self, e: &Env, id: Identifier) -> BigInt {
-///         let key = DataKey::Nonce(id.clone());
-///         let nonce = Self::read_nonce(e, id);
-///         e.contract_data()
-///             .set(key, nonce.clone() + BigInt::from_u32(e, 1));
-///         nonce
-///     }
-///    
-///     fn signature(&self) -> &Signature {
-///         &self.0
-///     }
-/// }
-/// ```
-pub trait NonceAuth {
-    /// Return the nonce stored in the contract.
-    fn read_nonce(e: &Env, id: Identifier) -> BigInt;
-    /// Return the nonce stored in the contract, and then increment it.
-    fn read_and_increment_nonce(&self, e: &Env, id: Identifier) -> BigInt;
-    /// Return the Signature used for authorization.
-    fn signature(&self) -> &Signature;
-}
 
 fn check_ed25519_auth(env: &Env, auth: &Ed25519Signature, function: Symbol, args: Vec<RawVal>) {
     let msg = SignaturePayloadV0 {
@@ -109,34 +61,15 @@ fn check_account_auth(env: &Env, auth: &AccountSignatures, function: Symbol, arg
     }
 }
 
-/// Checks a Signature that should be wrapped in a struct that implements NonceAuth.
-/// Note that the nonce is expected to be 0 if the signature is of type Signature::Contract.
-pub fn check_auth<T>(env: &Env, auth: &T, nonce: BigInt, function: Symbol, args: Vec<RawVal>)
-where
-    T: NonceAuth,
-{
-    match auth.signature() {
+/// Verifies a Signature. It's important to note that this module does
+/// not provide replay protection. That will need to be implemented by
+/// the user.
+pub fn check_auth(env: &Env, sig: &Signature, function: Symbol, args: Vec<RawVal>) {
+    match sig {
         Signature::Contract => {
-            if nonce != BigInt::from_i32(env, 0) {
-                panic!("nonce should be zero for Contract")
-            }
             env.get_invoking_contract();
         }
-        Signature::Ed25519(kea) => {
-            let stored_nonce =
-                auth.read_and_increment_nonce(env, Identifier::Ed25519(kea.public_key.clone()));
-            if nonce != stored_nonce {
-                panic!("incorrect nonce")
-            }
-            check_ed25519_auth(env, kea, function, args)
-        }
-        Signature::Account(kaa) => {
-            let stored_nonce =
-                auth.read_and_increment_nonce(env, Identifier::Account(kaa.account_id.clone()));
-            if nonce != stored_nonce {
-                panic!("incorrect nonce")
-            }
-            check_account_auth(env, kaa, function, args)
-        }
+        Signature::Ed25519(kea) => check_ed25519_auth(env, &kea, function, args),
+        Signature::Account(kaa) => check_account_auth(env, &kaa, function, args),
     }
 }
