bf6b383ef4cc817223e7bd07725df468d880bb27
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-14 15:58:12 -0700

    Fix bytes_lit reexport refs (#609)

--- soroban-sdk/src/bytes.rs
+++ soroban-sdk/src/bytes.rs
@@ -66,7 +66,7 @@ macro_rules! bytes {
         $crate::Bytes::from_array($env, &[$($x),+])
     };
     ($env:expr, $x:tt $(,)?) => {
-        $crate::Bytes::from_array($env, &::bytes_lit::bytes!($x))
+        $crate::Bytes::from_array($env, &$crate::__bytes_lit_bytes!($x))
     };
 }
 
@@ -100,7 +100,7 @@ macro_rules! bytesn {
         $crate::BytesN::from_array($env, &[$($x),+])
     };
     ($env:expr, $x:tt $(,)?) => {
-        $crate::BytesN::from_array($env, &::bytes_lit::bytes!($x))
+        $crate::BytesN::from_array($env, &$crate::__bytes_lit_bytes!($x))
     };
 }
 
