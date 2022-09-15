41d631873dd40e277f9e8bd67c09f2a6a5c8d4f8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-15 08:46:42 -0700

    E -> e

--- soroban-sdk/src/vec.rs
+++ soroban-sdk/src/vec.rs
@@ -106,7 +106,7 @@ impl_into_vec_for_tuple! { T0 0 T1 1 T2 2 T3 3 T4 4 T5 5 T6 6 T7 7 T8 8 T9 9 T10
 /// The following information is implementation details that has no consistency
 /// guarantee.
 ///
-/// Values are stored in the Environment backed by a [RRB-vector][rrb] using
+/// Values are stored in the environment backed by a [RRB-vector][rrb] using
 /// [im_rc::Vector]. Most operations are O(log n). Push/pop are O(1)
 /// amortised, and O(log n) in the worst case.
 ///
