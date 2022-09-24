261854bb1e765b13c7ae674d8d47b8db3c3b7b27
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-23 21:55:50 -0700

    invoker

--- soroban-sdk/src/invoker.rs
+++ soroban-sdk/src/invoker.rs
@@ -1,8 +1,9 @@
-use crate::BytesN;
+use crate::{contracttype, BytesN};
 
 /// TODO: Make Invoker storable and convertible to RawVal.
 
 /// Invoker is the invoker of a contract.
+#[contracttype(crate_path = "crate", export = false)]
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub enum Invoker {
     Account(BytesN<32>),
