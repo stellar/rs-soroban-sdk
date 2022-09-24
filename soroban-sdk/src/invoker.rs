ad6325a451b85c8a7e2c17de06ee139881775ac9
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-23 22:07:57 -0700

    remove todo

--- soroban-sdk/src/invoker.rs
+++ soroban-sdk/src/invoker.rs
@@ -1,7 +1,5 @@
 use crate::{contracttype, BytesN};
 
-/// TODO: Make Invoker storable and convertible to RawVal.
-
 /// Invoker is the invoker of a contract.
 #[contracttype(crate_path = "crate", export = false)]
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
