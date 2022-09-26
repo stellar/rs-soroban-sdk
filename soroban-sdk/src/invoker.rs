1d4de80a91cc826607d97f41bbb43c31e6e47c7a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-25 20:15:40 -0700

    wip

--- soroban-sdk/src/invoker.rs
+++ soroban-sdk/src/invoker.rs
@@ -1,7 +1,7 @@
 use crate::{contracttype, BytesN};
 
 /// Invoker is the invoker of a contract.
-#[contracttype(crate_path = "crate", export = false)]
+#[contracttype(crate_path = "crate", lib = "soroban_sdk", export = false)]
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub enum Invoker {
     Account(BytesN<32>),
