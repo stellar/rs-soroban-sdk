81536e101d3f7cdbb250d0743bfbbdd59d6a794a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-26 17:16:18 -0700

    Update soroban-env (#674)
    * Update soroban-env
    
    * upd hash

--- soroban-sdk/tests/contractfile_with_sha256.rs
+++ soroban-sdk/tests/contractfile_with_sha256.rs
@@ -1,6 +1,6 @@
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/example_add_i32.wasm",
-    sha256 = "dd5db60c98c2dc971c9754e306e36122a1d3982d4e0928b36fb6491a95f58420",
+    sha256 = "c0c5db07dd4db1ff9f38d69b6ef9b7a73acaa8af94eba17515874be9b22f2a48",
 );
 
 #[test]
