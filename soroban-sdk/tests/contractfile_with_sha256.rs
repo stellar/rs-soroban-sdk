2b556aedfa665fce0c863bdb0acc0075e2b8aa8b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-27 15:36:58 -0700

    Map Account invoker sigs and Account sigs to same identifier

--- soroban-sdk/tests/contractfile_with_sha256.rs
+++ soroban-sdk/tests/contractfile_with_sha256.rs
@@ -1,6 +1,6 @@
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/example_add_i32.wasm",
-    sha256 = "c0c5db07dd4db1ff9f38d69b6ef9b7a73acaa8af94eba17515874be9b22f2a48",
+    sha256 = "bd54d7acd68edca80653a4cebdf8204fdb678de773911eba73fdd96962efd9b5",
 );
 
 #[test]
