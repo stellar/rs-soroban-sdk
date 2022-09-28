3586389cf596213f1837eb7d6d958eedf7098d2b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-28 10:33:16 -0700

    fix

--- soroban-sdk/tests/contractfile_with_sha256.rs
+++ soroban-sdk/tests/contractfile_with_sha256.rs
@@ -1,6 +1,6 @@
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/example_add_i32.wasm",
-    sha256 = "bd54d7acd68edca80653a4cebdf8204fdb678de773911eba73fdd96962efd9b5",
+    sha256 = "c98df4de5c71eccfcf9c0df3c86b27aeff9fa5142e8a37bb547e66db3995b033",
 );
 
 #[test]
