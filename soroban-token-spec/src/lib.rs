74231b102454bc5d707436f03eecf4bcd59f398a
Alfonso Acosta <alfonso@stellar.org>
2022-10-11 17:19:26 +0200

    soroban-token-spec: Expose full token contract spec

--- soroban-token-spec/src/lib.rs
+++ soroban-token-spec/src/lib.rs
@@ -1,4 +1,7 @@
 #![no_std]
+extern crate alloc;
+
+use alloc::vec::Vec;
 use soroban_auth::{Identifier, Signature};
 use soroban_sdk::{contractimpl, contracttype, BigInt, Bytes, Env};
 
@@ -105,4 +108,37 @@ impl Token {
     pub fn export(env: Env, id: Signature, nonce: BigInt, amount: i64) {
         panic!("calling into interface");
     }
+}
+
+/// Obtains the full spec for the Token contract
+pub fn get_token_contract_spec_xdr() -> Vec<u8> {
+    // TODO: We should probably update the contractimpl macro to automatically generate this as a method
+    [
+        Token::spec_xdr_allowance().as_slice(),
+        Token::spec_xdr_approve().as_slice(),
+        Token::spec_xdr_balance().as_slice(),
+        Token::spec_xdr_burn().as_slice(),
+        Token::spec_xdr_decimals().as_slice(),
+        Token::spec_xdr_export().as_slice(),
+        Token::spec_xdr_freeze().as_slice(),
+        Token::spec_xdr_import().as_slice(),
+        Token::spec_xdr_init().as_slice(),
+        Token::spec_xdr_is_frozen().as_slice(),
+        Token::spec_xdr_mint().as_slice(),
+        Token::spec_xdr_name().as_slice(),
+        Token::spec_xdr_nonce().as_slice(),
+        Token::spec_xdr_set_admin().as_slice(),
+        Token::spec_xdr_symbol().as_slice(),
+        Token::spec_xdr_unfreeze().as_slice(),
+        Token::spec_xdr_xfer().as_slice(),
+        Token::spec_xdr_xfer_from().as_slice(),
+        TokenMetadata::spec_xdr().as_slice(),
+        soroban_auth::Identifier::spec_xdr().as_slice(),
+        soroban_auth::Signature::spec_xdr().as_slice(),
+        soroban_auth::Ed25519Signature::spec_xdr().as_slice(),
+        soroban_auth::AccountSignatures::spec_xdr().as_slice(),
+        soroban_auth::SignaturePayload::spec_xdr().as_slice(),
+        soroban_auth::SignaturePayloadV0::spec_xdr().as_slice(),
+    ]
+    .concat()
 }
