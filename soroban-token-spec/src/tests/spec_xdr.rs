33bdd5ec644a7a7853e596522ca6a869c077c216
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-17 17:30:02 -0700

    fix

--- soroban-token-spec/src/tests/spec_xdr.rs
+++ soroban-token-spec/src/tests/spec_xdr.rs
@@ -8,7 +8,7 @@ extern crate std;
 fn test_spec_xdr() {
     let xdr = spec_xdr();
     let mut cursor = std::io::Cursor::new(xdr);
-    for spec_entry in ScSpecEntry::read_xdr_iter(&mut cursor) {
-        std::println!("{:?}", spec_entry);
+    for (i, spec_entry) in ScSpecEntry::read_xdr_iter(&mut cursor).enumerate() {
+        std::println!("{}: {:?}", i, spec_entry);
     }
 }
