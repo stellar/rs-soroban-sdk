875f8fe037dba758a98101d950873f2072bb9cad
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-17 17:27:33 -0700

    Add test to token::spec_xdr

--- soroban-token-spec/src/tests/spec_xdr.rs
+++ soroban-token-spec/src/tests/spec_xdr.rs
@@ -1,7 +1,14 @@
+use soroban_sdk::xdr::{ReadXdr, ScSpecEntry};
+
 use crate::spec_xdr;
+
+extern crate std;
 
 #[test]
 fn test_spec_xdr() {
-    // it shouldn't panic
-    let _ = spec_xdr();
+    let xdr = spec_xdr();
+    let mut cursor = std::io::Cursor::new(xdr);
+    for spec_entry in ScSpecEntry::read_xdr_iter(&mut cursor) {
+        std::println!("{:?}", spec_entry);
+    }
 }
