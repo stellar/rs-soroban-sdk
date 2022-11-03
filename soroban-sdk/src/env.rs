stellar/rs-soroban-sdk

commit 959b0d050ee5f477b0915c4152beaa1b73f45ce1

Add assert_with_error! macro (#749)
### What
Add `assert_with_error!` macro that works like `assert!` but uses
`panic_error!`.

### Why
I've noticed that reviewing examples written by the folks experimenting
with Soroban that folks like to use `assert!`. We provide a
`panic_error!` function for folks who like to panic on error, so that
they can panic with an error code instead of a string. Given the
patterns I'm seeing with `assert!` I think it's worth also providing
same for error codes, and seeing if folks find error codes preferrable
with the same assertion pattern.

soroban-sdk/src/lib.rs
@@ -129,7 +129,30 @@ pub use soroban_sdk_macros::symbol;
 macro_rules! panic_error {
     ($env:expr, $error:expr) => {{
         $env.panic_error($error);
-        unreachable!()
+        unreachable!();
+    }};
+}
+
+/// Assert a condition and panic with the given error if it is false.
+///
+/// The first argument in the list must be a reference to an [Env].
+///
+/// The second argument is an expression that if resolves to `false` will cause
+/// a panic with the error in the third argument.
+///
+/// The third argument is an error value. The error value will be given to any
+/// calling contract.
+///
+/// Equivalent to `assert!`, but with an error value instead of a string. The
+/// error value will be given to any calling contract.
+///
+/// See [`contracterror`] for how to define an error type.
+#[macro_export]
+macro_rules! assert_with_error {
+    ($env:expr, $cond:expr, $error:expr) => {{
+        if !($cond) {
+            $crate::panic_error!($env, $error);
+        }
     }};
 }
 

soroban-sdk/src/tests.rs
@@ -1,6 +1,7 @@
 #![cfg(test)]
 
 mod contract_add_i32;
+mod contract_assert;
 mod contract_call_stack;
 mod contract_invoke;
 mod contract_invoker_account;

soroban-sdk/src/tests/contract_assert.rs
@@ -1,0 +1,43 @@
+use crate as soroban_sdk;
+use soroban_sdk::{contractimpl, xdr::ScStatusType, Env, Status};
+use soroban_sdk_macros::contracterror;
+
+pub struct Contract;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Error {
+    Zero = 1,
+}
+
+#[contractimpl]
+impl Contract {
+    pub fn assert(env: Env, value: u32) -> u32 {
+        assert_with_error!(&env, value > 0, Error::Zero);
+        value
+    }
+}
+
+#[test]
+#[should_panic(expected = "Status(ContractError(1)")]
+fn test_invoke_expect_status() {
+    let e = Env::default();
+    let contract_id = e.register_contract(None, Contract);
+
+    ContractClient::new(&e, &contract_id).assert(&0);
+}
+
+#[test]
+fn test_try_invoke() {
+    let e = Env::default();
+    let contract_id = e.register_contract(None, Contract);
+
+    let res = ContractClient::new(&e, &contract_id).try_assert(&0);
+    assert_eq!(
+        res,
+        Err(Ok(Status::from_type_and_code(
+            ScStatusType::ContractError,
+            1,
+        )))
+    );
+}

commit 20e9765397beb979ec81dd73b204cb0664e1cb81

Rename panic_error! to panic_with_error! (#752)
### What
Rename `panic_error!` to `panic_with_error!`.

### Why
For consistency with `assert_with_error!` introduced in #749.

soroban-sdk/src/env.rs
@@ -87,7 +87,7 @@ impl Env {
     ///
     /// Equivalent to `panic!`, but with an error value instead of a string.
     #[doc(hidden)]
-    pub fn panic_error(&self, error: impl Into<Status>) {
+    pub fn panic_with_error(&self, error: impl Into<Status>) {
         _ = internal::Env::fail_with_status(self, error.into());
         unreachable!()
     }

soroban-sdk/src/lib.rs
@@ -126,10 +126,19 @@ pub use soroban_sdk_macros::symbol;
 ///
 /// See [`contracterror`] for how to define an error type.
 #[macro_export]
+macro_rules! panic_with_error {
+    ($env:expr, $error:expr) => {{
+        $env.panic_with_error($error);
+        unreachable!();
+    }};
+}
+
+#[doc(hidden)]
+#[deprecated(note = "use panic_with_error!")]
+#[macro_export]
 macro_rules! panic_error {
     ($env:expr, $error:expr) => {{
-        $env.panic_error($error);
-        unreachable!();
+        $crate::panic_with_error!($env, $error);
     }};
 }
 
@@ -151,7 +160,7 @@ macro_rules! panic_error {
 macro_rules! assert_with_error {
     ($env:expr, $cond:expr, $error:expr) => {{
         if !($cond) {
-            $crate::panic_error!($env, $error);
+            $crate::panic_with_error!($env, $error);
         }
     }};
 }

tests/errors/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contracterror, contractimpl, panic_error, symbol, Env, Symbol};
+use soroban_sdk::{contracterror, contractimpl, panic_with_error, symbol, Env, Symbol};
 
 pub struct Contract;
 
@@ -18,7 +18,7 @@ impl Contract {
         } else if flag == 1 {
             Err(Error::AnError)
         } else if flag == 2 {
-            panic_error!(&env, Error::AnError)
+            panic_with_error!(&env, Error::AnError)
         } else if flag == 3 {
             panic!("an error")
         } else {