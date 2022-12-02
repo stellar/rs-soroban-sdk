a0eebba7f8d1e98aceaff5495e00d6998d82ddb3
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-02 12:04:50 -0800

    Minor doc clean up of crypto fns

--- soroban-sdk/src/crypto.rs
+++ soroban-sdk/src/crypto.rs
@@ -15,25 +15,21 @@ impl Crypto {
         &self.env
     }
 
-    /// Computes a SHA-256 hash.
-    pub fn sha256(&self, message: &Bytes) -> BytesN<32> {
+    /// Returns the a SHA-256 hash of the data.
+    pub fn sha256(&self, data: &Bytes) -> BytesN<32> {
         let env = self.env();
-        let bin_obj = internal::Env::compute_hash_sha256(env, message.into());
+        let bin_obj = internal::Env::compute_hash_sha256(env, data.into());
         unsafe { BytesN::unchecked_new(bin_obj.in_env(env)) }
     }
 
     /// Verifies an ed25519 signature.
     ///
-    /// The ed25519 signature (`sig`) is verified as a valid signature of the
-    /// message (`msg`) by the ed25519 public key (`pk`).
+    /// The signature is verified as a valid signature of the message by the
+    /// ed25519 public key.
     ///
     /// ### Panics
     ///
-    /// Will panic if the signature verification fails.
-    ///
-    /// ### TODO
-    ///
-    /// Return a [Result] instead of panicking.
+    /// If the signature verification fails.
     pub fn ed25519_verify(&self, public_key: &BytesN<32>, message: &Bytes, signature: &BytesN<64>) {
         let env = self.env();
         let _ = internal::Env::verify_sig_ed25519(
