50431873bfcb6386cdeacdf2bceb399f25c3c06c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-06 21:52:14 -0800

    fix

--- tests/contract_data/src/lib.rs
+++ tests/contract_data/src/lib.rs
@@ -1,19 +1,19 @@
 #![no_std]
-use soroban_sdk::{contractimpl, Env, Symbol};
+use soroban_sdk::{contractimpl, storage_map::StorageMap, Env, Symbol};
 
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn put(e: Env, key: Symbol, val: Symbol) {
-        e.storage().set(key, val)
+    fn mapping(e: &Env) -> StorageMap<Symbol, u32> {
+        e.storage_map()
     }
 
-    pub fn get(e: Env, key: Symbol) -> Option<Symbol> {
-        e.storage().get(key).map(|v| v.unwrap())
+    pub fn put(e: Env, key: Symbol, val: u32) {
+        Self::mapping(&e).set(key, val)
     }
 
-    pub fn del(e: Env, key: Symbol) {
-        e.storage().remove(key)
+    pub fn get(e: Env, key: Symbol) -> Option<u32> {
+        Self::mapping(&e).get(key).map(|val| val.unwrap())
     }
 }
