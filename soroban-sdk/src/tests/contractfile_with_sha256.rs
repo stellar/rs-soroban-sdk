204ed0e1cfa45fb34ec6ab2191e0d3eac4db1192
Dmytro Kozhevin <dmytro@stellar.org>
2023-02-02 19:36:30 -0500

    Minor test updates.

--- soroban-sdk/src/tests/contractfile_with_sha256.rs
+++ soroban-sdk/src/tests/contractfile_with_sha256.rs
@@ -1,7 +1,7 @@
 use crate as soroban_sdk;
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/test_add_u64.wasm",
-    sha256 = "88d88c54624c85d4d6e4654150cd76aaf34614dfaef67c73aa4a793614687798",
+    sha256 = "d86c76c8dd3d312881af263cd0190b6d6c40344f0788d05d178d275a1caefe66",
 );
 
 #[test]
