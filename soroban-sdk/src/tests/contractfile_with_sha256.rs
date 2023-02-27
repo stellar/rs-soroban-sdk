4fdb7e903f76e547b7699e554f6f536794f69b6c
Dmytro Kozhevin <dmytro@stellar.org>
2023-02-27 16:47:00 -0500

    Add utils for converting from/to contract/account ids. (#876)
    ### What
    
    Add utils for converting from/to contract/account ids.
    
    ### Why
    
    This covers some relatively rare, but seemingly useful Address use
    cases, such as transferring tokens to the newly deployed contracts or
    supporting cross-chain bridges.
    
    ### Known limitations
    
    N/A

--- soroban-sdk/src/tests/contractfile_with_sha256.rs
+++ soroban-sdk/src/tests/contractfile_with_sha256.rs
@@ -1,7 +1,7 @@
 use crate as soroban_sdk;
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/test_add_u64.wasm",
-    sha256 = "8defec8d424eb76db7e1d66a7d19e31ff34afae5dafdf5fca9ec59ed53ab9a63",
+    sha256 = "50ef498f6a4ed5f793f4b0cacc3283eec3d134f88f4e597936efa558c5ba5093",
 );
 
 #[test]
