0bbf9a22ba95ee679eabc79683cdcd442d541f54
Dmytro Kozhevin <dmytro@stellar.org>
2023-03-17 19:53:47 -0400

    Introduce temporary storage support in SDK.
    Also did a passing-by cleanup of some deprecated functions.

--- soroban-sdk/src/tests/contractfile_with_sha256.rs
+++ soroban-sdk/src/tests/contractfile_with_sha256.rs
@@ -1,7 +1,7 @@
 use crate as soroban_sdk;
 pub const WASM: &[u8] = soroban_sdk::contractfile!(
     file = "../target/wasm32-unknown-unknown/release/test_add_u64.wasm",
-    sha256 = "eb756067d9341b7658c8943d69a41ff548da1917f023c6b63085ebece3fd69e2",
+    sha256 = "41797b764142f3cb973ac855ac74647e9e53cc881d47d5806bfae55f6ff76d76",
 );
 
 #[test]
