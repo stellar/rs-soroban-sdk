3b23db32888241bba0ccebf23786875d2eaf1921
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-04-17 15:08:25 -0700

    diff

--- soroban-sdk/src/tests/token_spec.rs
+++ soroban-sdk/src/tests/token_spec.rs
@@ -1,7 +1,7 @@
 use crate as soroban_sdk;
 
 use soroban_sdk::{
-    token,
+    token::{Spec, SPEC_XDR_INPUT, SPEC_XDR_LEN},
     xdr::{Error, ReadXdr, ScSpecEntry},
 };
 
@@ -9,15 +9,13 @@ extern crate std;
 
 #[test]
 fn test_spec_xdr_len() {
-    let len = token::SPEC_XDR_INPUT
-        .iter()
-        .fold(0usize, |sum, x| sum + x.len());
-    assert_eq!(token::SPEC_XDR_LEN, len);
+    let len = SPEC_XDR_INPUT.iter().fold(0usize, |sum, x| sum + x.len());
+    assert_eq!(SPEC_XDR_LEN, len);
 }
 
 #[test]
 fn test_spec_xdr() -> Result<(), Error> {
-    let xdr = token::Spec::spec_xdr();
+    let xdr = Spec::spec_xdr();
     let mut cursor = std::io::Cursor::new(xdr);
     for spec_entry in ScSpecEntry::read_xdr_iter(&mut cursor) {
         spec_entry?;
