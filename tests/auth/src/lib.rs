51e8ce31f5e8a1305ec0afaada1e57fb37e70f25
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-04-20 15:40:21 -0700

    test

--- tests/auth/src/lib.rs
+++ tests/auth/src/lib.rs
@@ -1,19 +1,102 @@
 #![no_std]
-use soroban_sdk::{contracterror, contractimpl, vec, Address, Env, RawVal};
-
-// --- THE CONTRACT
+use soroban_sdk::{contracterror, contractimpl, Address, RawVal};
 
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn add(e: Env, a: Address) -> u64 {
-        a.require_auth_for_args(vec![&e]);
+    pub fn add(a: Address) -> u64 {
+        a.require_auth();
         2
     }
 }
 
-// --- THE AUTHONTATOR
+#[cfg(test)]
+mod test {
+    use soroban_sdk::{
+        testutils::Address as _,
+        xdr::{
+            AddressWithNonce, AuthorizedInvocation, ContractAuth, ScAddress, ScVal, StringM, VecM,
+        },
+        Address, Env,
+    };
+
+    use crate::{Authonator, Contract, ContractClient};
+    extern crate std;
+
+    #[test]
+    fn test_with_mocked_auth() {
+        let e = Env::default();
+        e.record_auth();
+
+        let contract_id = e.register_contract(None, Contract);
+        let client = ContractClient::new(&e, &contract_id);
+
+        let a = Address::random_contract(&e);
+
+        let r = client.add(&a);
+        assert_eq!(r, 2);
+        let auths = e.recorded_top_authorizations();
+        std::println!("auths: {:?}", auths);
+    }
+
+    #[test]
+    fn test_with_real_contract_auth() {
+        let e = Env::default();
+
+        let authonator_id = e.register_contract(None, Authonator);
+        let contract_id = e.register_contract(None, Contract);
+        let client = ContractClient::new(&e, &contract_id);
+
+        let a = Address::from_contract_id(&authonator_id);
+        let a_xdr: ScAddress = (&a).try_into().unwrap();
+
+        e.set_auth(&[ContractAuth {
+            address_with_nonce: Some(AddressWithNonce {
+                address: a_xdr.clone(),
+                nonce: 0,
+            }),
+            root_invocation: AuthorizedInvocation {
+                contract_id: contract_id.to_array().into(),
+                function_name: StringM::try_from("add").unwrap().into(),
+                args: std::vec![ScVal::Address(a_xdr)].try_into().unwrap(),
+                sub_invocations: VecM::default(),
+            },
+            signature_args: std::vec![].try_into().unwrap(),
+        }]);
+
+        let r = client.add(&a);
+        assert_eq!(r, 2);
+    }
+
+    #[test]
+    fn test_with_real_account_auth() {
+        let e = Env::default();
+
+        let contract_id = e.register_contract(None, Contract);
+        let client = ContractClient::new(&e, &contract_id);
+
+        let a = Address::random_account(&e);
+        let a_xdr: ScAddress = (&a).try_into().unwrap();
+
+        e.set_auth(&[ContractAuth {
+            address_with_nonce: Some(AddressWithNonce {
+                address: a_xdr.clone(),
+                nonce: 0,
+            }),
+            root_invocation: AuthorizedInvocation {
+                contract_id: contract_id.to_array().into(),
+                function_name: StringM::try_from("add").unwrap().into(),
+                args: std::vec![ScVal::Address(a_xdr)].try_into().unwrap(),
+                sub_invocations: VecM::default(),
+            },
+            signature_args: std::vec![].try_into().unwrap(),
+        }]);
+
+        let r = client.add(&a);
+        assert_eq!(r, 2);
+    }
+}
 
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
@@ -33,47 +116,5 @@ impl Authonator {
         _auth_context: RawVal,
     ) -> Result<(), AuthonatorError> {
         Ok(())
-    }
-}
-
-// --- THE TESTS
-
-#[cfg(test)]
-mod test {
-    use soroban_sdk::{
-        xdr::{AddressWithNonce, AuthorizedInvocation, ContractAuth, ScVec, StringM, VecM},
-        Address, Env,
-    };
-
-    use crate::{Authonator, Contract, ContractClient};
-    extern crate std;
-
-    #[test]
-    fn test_add() {
-        let e = Env::default();
-        let authonator_id = e.register_contract(None, Authonator);
-        let contract_id = e.register_contract(None, Contract);
-        let client = ContractClient::new(&e, &contract_id);
-
-        let a = Address::from_contract_id(&authonator_id);
-
-        e.set_auth(&[ContractAuth {
-            address_with_nonce: Some(AddressWithNonce {
-                address: (&a).try_into().unwrap(),
-                nonce: 0,
-            }),
-            root_invocation: AuthorizedInvocation {
-                contract_id: contract_id.to_array().into(),
-                function_name: StringM::try_from("add").unwrap().into(),
-                args: ScVec::default(),
-                sub_invocations: VecM::default(),
-            },
-            signature_args: std::vec![].try_into().unwrap(),
-        }]);
-
-        let r = client.add(&a);
-        assert_eq!(r, 2);
-        // let auths = e.recorded_top_authorizations();
-        // std::println!("auths: {:?}", auths);
     }
 }
