a8c393cc581ef6ea55fa82e6d1ad199d9fb56492
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-04-25 17:31:59 -0700

    fiddles

--- tests/auth/src/lib.rs
+++ tests/auth/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contracterror, contractimpl, Address, RawVal};
+use soroban_sdk::{contractimpl, Address};
 
 pub struct Contract;
 
@@ -13,20 +13,21 @@ impl Contract {
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use soroban_sdk::{
+        contracterror,
         testutils::Address as _,
+        vec,
         xdr::{
             AddressWithNonce, AuthorizedInvocation, ContractAuth, ScAddress, ScBytes, ScMap,
             ScMapEntry, ScSymbol, ScVal, StringM, VecM,
         },
-        Address, Env,
+        Address, Env, RawVal, Symbol,
     };
-
-    use crate::{Authonator, Contract, ContractClient};
     extern crate std;
 
     #[test]
-    fn test_with_mocked_auth() {
+    fn test_with_mock_all_auth() {
         let e = Env::default();
         e.mock_all_auths();
 
@@ -37,19 +38,26 @@ mod test {
 
         let r = client.add(&a);
         assert_eq!(r, 2);
-        let auths = e.mocked_auths();
-        std::println!("auths: {:?}", auths);
+        assert_eq!(
+            e.mocked_auths(),
+            [(
+                a.clone(),
+                contract_id,
+                Symbol::short("add"),
+                vec![&e, a.to_raw()]
+            )],
+        );
     }
 
     #[test]
-    fn test_with_real_contract_auth() {
+    fn test_with_real_contract_auth_approve() {
         let e = Env::default();
 
-        let authonator_id = e.register_contract(None, Authonator);
+        let auth_contract_id = e.register_contract(None, auth_approve::Contract);
         let contract_id = e.register_contract(None, Contract);
         let client = ContractClient::new(&e, &contract_id);
 
-        let a = Address::from_contract_id(&authonator_id);
+        let a = Address::from_contract_id(&auth_contract_id);
         let a_xdr: ScAddress = (&a).try_into().unwrap();
 
         e.set_auths(&[ContractAuth {
@@ -68,6 +76,35 @@ mod test {
 
         let r = client.add(&a);
         assert_eq!(r, 2);
+    }
+
+    #[test]
+    #[should_panic = "Status(ContractError(1))"]
+    fn test_with_real_contract_auth_decline() {
+        let e = Env::default();
+
+        let auth_contract_id = e.register_contract(None, auth_decline::Contract);
+        let contract_id = e.register_contract(None, Contract);
+        let client = ContractClient::new(&e, &contract_id);
+
+        let a = Address::from_contract_id(&auth_contract_id);
+        let a_xdr: ScAddress = (&a).try_into().unwrap();
+
+        e.set_auths(&[ContractAuth {
+            address_with_nonce: Some(AddressWithNonce {
+                address: a_xdr.clone(),
+                nonce: 0,
+            }),
+            root_invocation: AuthorizedInvocation {
+                contract_id: contract_id.to_array().into(),
+                function_name: StringM::try_from("add").unwrap().into(),
+                args: std::vec![ScVal::Address(a_xdr)].try_into().unwrap(),
+                sub_invocations: VecM::default(),
+            },
+            signature_args: std::vec![].try_into().unwrap(),
+        }]);
+
+        client.add(&a);
     }
 
     // TODO: This test is broken and doesn't work because the account doesn't exist.
@@ -113,25 +150,46 @@ mod test {
         let r = client.add(&a);
         assert_eq!(r, 2);
     }
-}
 
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
-pub enum AuthonatorError {
-    HellNo = 1,
-}
+    mod auth_approve {
+        use super::*;
 
-pub struct Authonator;
+        pub struct Contract;
 
-#[contractimpl]
-impl Authonator {
-    #[allow(non_snake_case)]
-    pub fn __check_auth(
-        _signature_payload: RawVal,
-        _signatures: RawVal,
-        _auth_context: RawVal,
-    ) -> Result<(), AuthonatorError> {
-        Ok(())
+        #[contractimpl]
+        impl Contract {
+            #[allow(non_snake_case)]
+            pub fn __check_auth(
+                _signature_payload: RawVal,
+                _signatures: RawVal,
+                _auth_context: RawVal,
+            ) {
+            }
+        }
+    }
+
+    mod auth_decline {
+        use super::*;
+
+        pub struct Contract;
+
+        #[contracterror]
+        #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+        #[repr(u32)]
+        pub enum Error {
+            Decline = 1,
+        }
+
+        #[contractimpl]
+        impl Contract {
+            #[allow(non_snake_case)]
+            pub fn __check_auth(
+                _signature_payload: RawVal,
+                _signatures: RawVal,
+                _auth_context: RawVal,
+            ) -> Result<(), Error> {
+                Err(Error::Decline)
+            }
+        }
     }
 }
