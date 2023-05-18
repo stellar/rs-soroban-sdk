0084a356124ef4e84455c5e0f8e2c2630c8a2303
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-05-18 13:40:38 -0700

    Make signature type configurable

--- soroban-sdk/src/auth.rs
+++ soroban-sdk/src/auth.rs
@@ -1,4 +1,4 @@
-use crate::{contracttype, Address, BytesN, Env, RawVal, Status, Symbol, Vec};
+use crate::{contracttype, Address, BytesN, Env, IntoVal, RawVal, Status, Symbol, Vec};
 
 /// Auth context that the environment provides to account contracts. A context
 /// represents a single instance of authorization in the auth call tree.
@@ -21,15 +21,15 @@ impl Context {
 ///
 /// Once a contract implements the interface, the address of the contract can be
 /// used for addresses that have [`Address::require_auth`] called.
-pub trait Interface<E>
-where
-    Status: for<'a> TryFrom<&'a E>,
-{
+pub trait Interface {
+    type Error: Into<Status>;
+    type Signature;
+
     /// Check that the signatures and auth contexts are valid.
     fn __check_auth(
         env: Env,
         signature_payload: BytesN<32>,
-        signatures: Vec<RawVal>,
+        signatures: Vec<Self::Signature>,
         auth_contexts: Vec<Context>,
-    ) -> Result<(), E>;
+    ) -> Result<(), Self::Error>;
 }
