fb6fd91972e66ceef497e494bf6da010ac03356c
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-05-24 12:17:47 -0400

    chore: rm accidental dir

--- soroban-spec/root/src/server.ts
+++
@@ -1,8 +0,0 @@
-import * as SorobanClient from 'soroban-client'
-import { RPC_URL } from './constants'
-
-/**
- * SorobanClient.Server instance, initialized using {@link RPC_URL} used to
- * initialize this library.
- */
-export const Server = new SorobanClient.Server(RPC_URL);
