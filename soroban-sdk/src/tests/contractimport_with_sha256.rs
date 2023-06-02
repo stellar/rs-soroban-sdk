33ede90f18d6d04f6c34253ea8f67bb151df447a
Dmytro Kozhevin <dmytro@stellar.org>
2023-06-02 19:32:46 -0400

    !fixup contract hashes

--- soroban-sdk/src/tests/contractimport_with_sha256.rs
+++ soroban-sdk/src/tests/contractimport_with_sha256.rs
@@ -6,7 +6,7 @@ mod addcontract {
     use crate as soroban_sdk;
     soroban_sdk::contractimport!(
         file = "../target/wasm32-unknown-unknown/release/test_add_u64.wasm",
-        sha256 = "b5dbebd9685208acff793bf22ac64730dad9a68f7cfb244da1522274ceb9be08",
+        sha256 = "c88c56ae1ba2b4d81b7bd8a0ab17c0ccb717db46eac707536059a626108bf5ae",
     );
 }
 
