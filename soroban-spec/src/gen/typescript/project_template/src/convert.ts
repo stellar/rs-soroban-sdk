31e7709052627620c72d02645cf2b16d0399dfbe
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-06-06 13:39:44 -0400

    fix: convert.ts errors/warnings when building projects

--- soroban-spec/src/gen/typescript/project_template/src/convert.ts
+++ soroban-spec/src/gen/typescript/project_template/src/convert.ts
@@ -40,17 +40,18 @@ export function scValStrToJs<T>(base64Xdr: string): T {
 
 export function scValToJs<T>(val: xdr.ScVal): T {
     switch (val?.switch()) {
-        case xdr.ScValType.scvBool():
-            return val.b() as T;
+        case xdr.ScValType.scvBool(): {
+            return val.b() as unknown as T;
+        }
         case xdr.ScValType.scvVoid():
         case undefined: {
-            return 0 as T;
+            return 0 as unknown as T;
         }
         case xdr.ScValType.scvU32(): {
-            return val.u32() as T;
+            return val.u32() as unknown as T;
         }
         case xdr.ScValType.scvI32(): {
-            return val.i32() as T;
+            return val.i32() as unknown as T;
         }
         case xdr.ScValType.scvU64():
         case xdr.ScValType.scvI64():
@@ -58,23 +59,23 @@ export function scValToJs<T>(val: xdr.ScVal): T {
         case xdr.ScValType.scvI128():
         case xdr.ScValType.scvU256():
         case xdr.ScValType.scvI256(): {
-            return scvalToBigInt(val) as T;
+            return scvalToBigInt(val) as unknown as T;
         }
         case xdr.ScValType.scvAddress(): {
-            return Address.fromScVal(val).toString() as T;
+            return Address.fromScVal(val).toString() as unknown as T;
         }
         case xdr.ScValType.scvString(): {
-            return val.str().toString() as T;
+            return val.str().toString() as unknown as T;
         }
         case xdr.ScValType.scvSymbol(): {
-            return val.sym().toString() as T;
+            return val.sym().toString() as unknown as T;
         }
         case xdr.ScValType.scvBytes(): {
-            return val.bytes() as T;
+            return val.bytes() as unknown as T;
         }
         case xdr.ScValType.scvVec(): {
             type Element = ElementType<T>;
-            return val.vec().map(scValToJs<Element>) as T
+            return val.vec().map(v => scValToJs<Element>(v)) as unknown as T;
         }
         case xdr.ScValType.scvMap(): {
             type Key = KeyType<T>;
@@ -86,16 +87,16 @@ export function scValToJs<T>(val: xdr.ScVal): T {
                 res.set(key as Key, value as Value);
             });
 
-            return res as T
+            return res as unknown as T
         }
         case xdr.ScValType.scvContractExecutable():
-            return val.exec() as T;
+            return val.exec() as unknown as T;
         case xdr.ScValType.scvLedgerKeyNonce():
-            return val.nonceKey() as T;
+            return val.nonceKey() as unknown as T;
         case xdr.ScValType.scvTimepoint():
-            return val.timepoint() as T;
+            return val.timepoint() as unknown as T;
         case xdr.ScValType.scvDuration():
-            return val.duration() as T;
+            return val.duration() as unknown as T;
         // TODO: Add this case when merged
         // case xdr.ScValType.scvError():
         default: {
