9bf33c562c2596a7c2c8e8b19543bf63f93afda1
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-06-20 15:02:33 -0700

    fix test clean up example

--- tests/fuzz/fuzz/fuzz_targets/fuzz_target_1.rs
+++ tests/fuzz/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,83 +1,9 @@
 #![no_main]
 
-// TODO: HELP!
-// This contract sees the following build errors and I'm not sure what the right
-// way to fix them is:
-//
-// ❯ cargo +nightly fuzz build
-// Compiling test_fuzz v0.8.4 (/Users/leighmcculloch/Code/rs-soroban-sdk/tests/fuzz)
-// Compiling test_fuzz-fuzz v0.0.0 (/Users/leighmcculloch/Code/rs-soroban-sdk/tests/fuzz/fuzz)
-// error[E0433]: failed to resolve: could not find `size_hint` in `arbitrary`
-// --> fuzz_targets/fuzz_target_1.rs:18:10
-// |
-// 18 | #[derive(Arbitrary, Debug)]
-// |          ^^^^^^^^^ could not find `size_hint` in `arbitrary`
-// |
-// = note: this error originates in the derive macro `Arbitrary` (in Nightly builds, run with -Z macro-backtrace for more info)
-//
-// error[E0412]: cannot find type `Unstructured` in module `arbitrary`
-// --> fuzz_targets/fuzz_target_1.rs:18:10
-// |
-// 18 | #[derive(Arbitrary, Debug)]
-// |          ^^^^^^^^^ not found in `arbitrary`
-// |
-// = note: this error originates in the derive macro `Arbitrary` (in Nightly builds, run with -Z macro-backtrace for more info)
-//
-// error[E0412]: cannot find type `Result` in module `arbitrary`
-// --> fuzz_targets/fuzz_target_1.rs:18:10
-// |
-// 18 | #[derive(Arbitrary, Debug)]
-// |          ^^^^^^^^^ not found in `arbitrary`
-// |
-// = note: this error originates in the derive macro `Arbitrary` (in Nightly builds, run with -Z macro-backtrace for more info)
-// help: consider importing one of these items
-// |
-// 3  + use core::fmt::Result;
-// |
-// 3  + use core::result::Result;
-// |
-// 3  + use std::fmt::Result;
-// |
-// 3  + use std::io::Result;
-// |
-// and 2 other candidates
-// help: if you import `Result`, refer to it directly
-// |
-// 18 | #[derive(Arbitrary, Debug)]
-// |
-//
-// error[E0433]: failed to resolve: could not find `Error` in `arbitrary`
-// --> fuzz_targets/fuzz_target_1.rs:18:10
-// |
-// 18 | #[derive(Arbitrary, Debug)]
-// |          ^^^^^^^^^ could not find `Error` in `arbitrary`
-// |
-// = note: this error originates in the derive macro `Arbitrary` (in Nightly builds, run with -Z macro-backtrace for more info)
-// help: consider importing one of these items
-// |
-// 3  + use core::error::Error;
-// |
-// 3  + use core::fmt::Error;
-// |
-// 3  + use soroban_sdk::Error;
-// |
-// 3  + use soroban_sdk::testutils::ed25519::Error;
-// |
-// and 4 other candidates
-// help: if you import `Error`, refer to it directly
-// |
-// 18 | #[derive(Arbitrary, Debug)]
-// |
-
 use libfuzzer_sys::fuzz_target;
 
 use arbitrary::Arbitrary;
-use soroban_sdk::{
-    arbitrary::SorobanArbitrary,
-    Env,
-    IntoVal,
-    U256,
-};
+use soroban_sdk::{arbitrary::SorobanArbitrary, Env, IntoVal, U256};
 
 use test_fuzz::{Contract, ContractClient};
 
@@ -96,5 +22,5 @@ fuzz_target!(|input: Input| {
     let contract_id = env.register_contract(None, Contract);
     let client = ContractClient::new(&env, &contract_id);
 
-    let _ = client.add(&a, &b);
+    let _ = client.run(&a, &b);
 });
--- tests/fuzz/src/lib.rs
+++ tests/fuzz/src/lib.rs
@@ -5,26 +5,9 @@ pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn add(a: U256, b: U256) -> bool {
-        a < b
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use soroban_sdk::Env;
-
-    use crate::{Contract, ContractClient};
-
-    #[test]
-    fn test_add() {
-        let e = Env::default();
-        let contract_id = e.register_contract(None, Contract);
-        let client = ContractClient::new(&e, &contract_id);
-
-        let x = 10u64;
-        let y = 12u64;
-        let z = client.add(&x, &y);
-        assert!(z == 22);
+    pub fn run(a: U256, b: U256) {
+        if a < b {
+            panic!("unexpected")
+        }
     }
 }
