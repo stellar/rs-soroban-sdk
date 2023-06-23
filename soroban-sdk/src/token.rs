2f8563c860158712bc45b3b284772515569e9dd6
Elliot Voris <elliot@voris.me>
2023-06-23 09:18:37 -0500

    Token doc comments (#967)

--- soroban-sdk/src/token.rs
+++ soroban-sdk/src/token.rs
@@ -26,22 +26,210 @@ pub struct Spec;
 #[contractspecfn(name = "Spec", export = false)]
 #[contractclient(crate_path = "crate", name = "Client")]
 pub trait Interface {
+    /// Returns the allowance for `spender` to transfer from `from`.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The address holding the balance of tokens to be drawn from.
+    /// * `spender` - The address spending the tokens held by `from`.
     fn allowance(env: Env, from: Address, spender: Address) -> i128;
+
+    /// Increase the allowance by `amount` for `spender` to transfer/burn from
+    /// `from`.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The address holding the balance of tokens to be drawn from.
+    /// * `spender` - The address being authorized to spend the tokens held by
+    ///   `from`.
+    /// * `amount` - The additional tokens to be made availabe to `spender`.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["increase_allowance", from: Address,
+    /// spender: Address], data = [amount: i128]`
     fn increase_allowance(env: Env, from: Address, spender: Address, amount: i128);
+
+    /// Decrease the allowance by `amount` for `spender` to transfer/burn from
+    /// `from`.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The address holding the balance of tokens to be drawn from.
+    /// * `spender` - The address being (de-)authorized to spend the tokens held
+    ///   by `from`.
+    /// * `amount` - The tokens which are no longer availabe for use by
+    ///   `spender`. If `amount` is greater than the current allowance, set the
+    ///   allowance to 0.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["decrease_allowance", from: Address,
+    /// spender: Address], data = [amount: i128]`
     fn decrease_allowance(env: Env, from: Address, spender: Address, amount: i128);
+
+    /// Returns the balance of `id`.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The address for which a balance is being queried. If the
+    ///   address has no existing balance, returns 0.
     fn balance(env: Env, id: Address) -> i128;
+
+    /// Returns the spendable balance of `id`.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The address for which a spendable balance is being queried.
+    ///   This will return the same value as `balance()` unless this is called
+    ///   on the Stellar Asset Contract, in which case this can be less due to
+    ///   reserves/liabilities.
     fn spendable_balance(env: Env, id: Address) -> i128;
+
+    /// Returns true if `id` is authorized to use its balance.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The address for which token authorization is being checked.
     fn authorized(env: Env, id: Address) -> bool;
+
+    /// Transfer `amount` from `from` to `to`.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The address holding the balance of tokens which will be
+    ///   withdrawn from.
+    /// * `to` - The address which will receive the transferred tokens.
+    /// * `amount` - The amount of tokens to be transferred.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["transfer", from: Address, to: Address],
+    /// data = [amount: i128]`
     fn transfer(env: Env, from: Address, to: Address, amount: i128);
+
+    /// Transfer `amount` from `from` to `to`, consuming the allowance of
+    /// `spender`. Authorized by spender (`spender.require_auth()`).
+    ///
+    /// # Arguments
+    ///
+    /// * `spender` - The address authorizing the transfer, and having its
+    ///   allowance consumed during the transfer.
+    /// * `from` - The address holding the balance of tokens which will be
+    ///   withdrawn from.
+    /// * `to` - The address which will receive the transferred tokens.
+    /// * `amount` - The amount of tokens to be transferred.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["transfer", from: Address, to: Address],
+    /// data = [amount: i128]`
     fn transfer_from(env: Env, spender: Address, from: Address, to: Address, amount: i128);
+
+    /// Burn `amount` from `from`.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The address holding the balance of tokens which will be
+    ///   burned from.
+    /// * `amount` - The amount of tokens to be burned.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["burn", from: Address], data = [amount:
+    /// i128]`
     fn burn(env: Env, from: Address, amount: i128);
+
+    /// Burn `amount` from `from`, consuming the allowance of `spender`.
+    ///
+    /// # Arguments
+    ///
+    /// * `spender` - The address authorizing the burn, and having its allowance
+    ///   consumed during the burn.
+    /// * `from` - The address holding the balance of tokens which will be
+    ///   burned from.
+    /// * `amount` - The amount of tokens to be burned.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["burn", from: Address], data = [amount:
+    /// i128]`
     fn burn_from(env: Env, spender: Address, from: Address, amount: i128);
+
+    /// Sets whether the account is authorized to use its balance. If
+    /// `authorized` is true, `id` should be able to use its balance.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The address being (de-)authorized.
+    /// * `authorize` - Whether or not `id` can use its balance.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["set_authorized", id: Address], data =
+    /// [authorize: bool]`
     fn set_authorized(env: Env, id: Address, authorize: bool);
+
+    /// Mints `amount` to `to`.
+    ///
+    /// # Arguments
+    ///
+    /// * `to` - The address which will receive the minted tokens.
+    /// * `amount` - The amount of tokens to be minted.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["mint", admin: Address, to: Address], data
+    /// = [amount: i128]`
     fn mint(env: Env, to: Address, amount: i128);
+
+    /// Clawback `amount` from `from` account. `amount` is burned in the
+    /// clawback process.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The address holding the balance from which the clawback will
+    ///   take tokens.
+    /// * `amount` - The amount of tokens to be clawed back.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["clawback", admin: Address, to: Address],
+    /// data = [amount: i128]`
     fn clawback(env: Env, from: Address, amount: i128);
+
+    /// Sets the administrator to the specified address `new_admin`.
+    ///
+    /// # Arguments
+    ///
+    /// * `new_admin` - The address which will henceforth be the administrator
+    ///   of this token contract.
+    ///
+    /// # Events
+    ///
+    /// Emits an event with topics `["set_admin", admin: Address], data =
+    /// [new_admin: Address]`
     fn set_admin(env: Env, new_admin: Address);
+
+    /// Returns the number of decimals used to represent amounts of this token.
+    ///
+    /// # Panics
+    ///
+    /// If the contract has not yet been initialized.
     fn decimals(env: Env) -> u32;
+
+    /// Returns the name for this token.
+    ///
+    /// # Panics
+    ///
+    /// If the contract has not yet been initialized.
     fn name(env: Env) -> String;
+
+    /// Returns the symbol for this token.
+    ///
+    /// # Panics
+    ///
+    /// If the contract has not yet been initialized.
     fn symbol(env: Env) -> String;
 }
 
@@ -65,7 +253,7 @@ pub(crate) const SPEC_XDR_INPUT: &[&[u8]] = &[
     &Spec::spec_xdr_transfer_from(),
 ];
 
-pub(crate) const SPEC_XDR_LEN: usize = 1108;
+pub(crate) const SPEC_XDR_LEN: usize = 5900;
 
 impl Spec {
     /// Returns the XDR spec for the Token contract.
