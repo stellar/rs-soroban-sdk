d98c8aeee9b779898fe3ad903871cbaa38fdc2b2
Dmytro Kozhevin <dmytro@stellar.org>
2023-06-23 20:45:23 -0400

    !fixup Doc updates

--- soroban-sdk/src/storage.rs
+++ soroban-sdk/src/storage.rs
@@ -94,7 +94,7 @@ impl Storage {
     /// Storage for data that may stay in ledger only for a limited amount of
     /// time.
     ///
-    /// This is cheaper than `persistent()` storage.
+    /// Temporary storage is cheaper than Persistent storage.
     ///
     /// Temporary entries will be removed from the ledger after their lifetime
     /// ends. Removed entries can be created again, potentially with different
@@ -111,15 +111,25 @@ impl Storage {
     /// Storage for a **small amount** of persistent data associated with
     /// the current contract's instance.
     ///
-    /// All the `instance` entries are stored in a single `persistent()` ledger
-    /// entry. That makes it cheap to use this, but also limits the total
-    /// size of the stored data.
-    ///
-    /// This has the same lifetime properties as `persistent()` storage (i.e.
+    /// Storing a small amount of frequently used data in instance storage is
+    /// likely cheaper than storing it separately in Persistent storage.
+    /// 
+    /// Instance storage is tightly coupled with the contract instance: it will
+    /// be loaded from the ledger every time the contract instance itself is 
+    /// loaded. It also won't appear in the ledger footprint. *All*
+    /// the data stored in the instance storage is read from ledger every time
+    /// the contract is used and it doesn't matter whether contract uses the
+    /// storage or not.
+    /// 
+    /// This has the same lifetime properties as Persistent storage, i.e.
     /// the data semantically stays in the ledger forever and can be
-    ///  evicted/restored).
-    ///
-    /// This should be used for data directly associated with the current
+    /// evicted/restored.
+    /// 
+    /// The amount of data that can be stored in the instance storage is limited
+    /// by the ledger entry size (a network-defined parameter). It is
+    /// in the order of 100 KB serialized.
+    /// 
+    /// This should be used for small data directly associated with the current
     /// contract, such as its admin, configuration settings, tokens the contract
     /// operates on etc. Do not use this with any data that can scale in
     /// unbounded fashion (such as user balances).
