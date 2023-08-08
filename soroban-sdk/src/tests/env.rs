78eb60aba723be627f663e7e72925df4e2ff653b
Alejandro Criado-Pérez <alejandro@criadoperez.com>
2023-08-08 10:43:59 +0200

    Update env.rs

--- soroban-sdk/src/tests/env.rs
+++ soroban-sdk/src/tests/env.rs
@@ -2,7 +2,7 @@ use crate::Env;
 
 #[test]
 // Env::default is expected to configure the underlying host with a source
-// account in tests so that the Env is configured similarily to how it will be
+// account in tests so that the Env is configured similarly to how it will be
 // configured for real. Some functions in Env have in the past or may now make
 // assumptions about a source account being set. This is something small we do
 // to make sure we don't accidentally introduce Env functionality that will
