aa6c8f335cfbe6455995c83eca4af49d57bda0ab
Graydon Hoare <graydon@pobox.com>
2023-08-16 21:18:17 -0700

    Update dalek, remove non-syn2 exemptions, tighten deps

--- soroban-sdk/src/env.rs
+++ soroban-sdk/src/env.rs
@@ -710,7 +710,7 @@ impl Env {
             .unwrap();
 
         let prev_auth_manager = self.env_impl.snapshot_auth_manager().unwrap();
-        self.env_impl.switch_to_recording_auth().unwrap();
+        self.env_impl.switch_to_recording_auth(true).unwrap();
         self.invoke_contract::<()>(
             &token_id,
             &soroban_sdk_macros::internal_symbol_short!("set_admin"),
@@ -735,7 +735,7 @@ impl Env {
 
     fn register_contract_with_source(&self, executable: xdr::ContractExecutable) -> Address {
         let prev_auth_manager = self.env_impl.snapshot_auth_manager().unwrap();
-        self.env_impl.switch_to_recording_auth().unwrap();
+        self.env_impl.switch_to_recording_auth(true).unwrap();
 
         let contract_id: Address = self
             .env_impl
@@ -884,7 +884,7 @@ impl Env {
     /// }
     /// ```
     pub fn mock_all_auths(&self) {
-        self.env_impl.switch_to_recording_auth().unwrap();
+        self.env_impl.switch_to_recording_auth(true).unwrap();
     }
 
     /// Returns a list of authorization trees that were seen during the last
@@ -1149,7 +1149,7 @@ impl Env {
         let storage = internal::storage::Storage::with_recording_footprint(rs.clone());
         let budget = internal::budget::Budget::default();
         let env_impl = internal::EnvImpl::with_storage_and_budget(storage, budget.clone());
-        env_impl.switch_to_recording_auth().unwrap();
+        env_impl.switch_to_recording_auth(true).unwrap();
 
         let env = Env {
             env_impl,
