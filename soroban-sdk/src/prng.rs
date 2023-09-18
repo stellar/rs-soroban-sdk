4cd4d3d9d6dabcb58d55ae948c45d5e0f01b6d0e
Graydon Hoare <graydon@pobox.com>
2023-09-18 16:47:26 -0700

    Elaborate more in comment on PRNG strengths and weaknesses

--- soroban-sdk/src/prng.rs
+++ soroban-sdk/src/prng.rs
@@ -1,23 +1,57 @@
-//! Prng contains a pseudo-random generator.
+//! Prng contains a pseudo-random number generator.
 //!
-//! # Warning
+//! ## Warning
 //!
-//! **The pseudo-random generator contained in this module is not suitable for
-//! security-sensitive work.**
+//! Do not use the PRNG in this module without a clear understanding of two
+//! major limitations in the way it is deployed in the Stellar network:
 //!
-//! The entropy used to seed the generator is not strong. Every node in the
-//! network executing a contract get exactly the same output.  The value is hard
-//! to predict, but trivial to derive once the network has determined the inputs
-//! into the ledger the invocation occurs in. The value is also controllable by
-//! the node nominating. Therefore, the results of the pseudo-random number
-//! generator are determinable once the inputs to a ledger are known.
+//!   1. The PRNG is seeded with data that is public as soon as each ledger is
+//!      nominated. Therefore it **should never be used to generate secrets**.
 //!
-//! Every contract invocation gets its own, independent seed. If a contract
-//! invocation fails, the seed from the failed invocation is not reused for the
-//! next invocation of the contract.
+//!   2. The PRNG is seeded with data that is under the control of validators.
+//!      Therefore it **should only be used in applications where the risk of
+//!      validator influence is acceptable**.
 //!
-//! In tests, the contract invocation seed is consistently zero, and tests will
-//! receive consistent results from the PRNG.
+//! The PRNG in this module is a strong CSPRNG (ChaCha20) and can be manually
+//! re-seeded by contracts, in order to support commit/reveal schemes, oracles,
+//! or similar advanced types of pseudo-random contract behaviour. Any PRNG is
+//! however only as strong as its seed.
+//!
+//! The network runs in strict consensus, so every node in the network seeds its
+//! PRNG with a consensus value, **not a random entropy source**. It uses data
+//! that is generally difficult to predict in advance, and generally difficult
+//! for network **users** to bias to a specific value: the seed is derived from
+//! the overall transaction-set hash and the hash-sorted position number of each
+//! transaction within it. But this seed is **not secret** and **not
+//! cryptographically hard to bias** if a corrupt **validator** were to choose
+//! to do so (similar to the way a corrupt validator can bias overall
+//! transaction admission in the network).
+//!
+//! In other words the network will provide a stronger seed than a contract
+//! could likely derive on-chain using any other public data visible to it (eg.
+//! better than using a timestamp, ledger number, counter, or a similarly weak
+//! seed) but weaker than a contract could acquire using a commit/reveal scheme
+//! with an off-chain source of trusted random entropy.
+//!
+//! You should carefully consider whether these limitations are acceptable for
+//! your application before using this module.
+//!
+//! ## Operation
+//!
+//! The host has a single hidden "base" PRNG that is seeded by the network. The
+//! base PRNG is then used to seed separate, independent "local" PRNGs for each
+//! contract invocation. Contracts can only access (use or reseed) their local
+//! PRNG, and cannot influence any other contract instance's local PRNGs nor the
+//! base PRNG. If a contract invocation fails, the seed from the failed
+//! invocation is also not reused for the next invocation of the contract.
+//!
+//! ## Testing
+//!
+//! In local tests, the base PRNG of each host is seeded to zero when the host
+//! is constructed, so each contract invocation's local PRNG seed (and all its
+//! PRNG-derived calls) will be determined strictly by its order of invocation
+//! in the test. Assuming this order is stable, each test run should see stable
+//! output from the local PRNG.
 use core::ops::{Bound, RangeBounds};
 
 use crate::{env::internal, unwrap::UnwrapInfallible, Bytes, Env, IntoVal, TryIntoVal, Val, Vec};
@@ -26,8 +60,8 @@ use crate::{env::internal, unwrap::UnwrapInfallible, Bytes, Env, IntoVal, TryInt
 ///
 /// # Warning
 ///
-/// **The pseudo-random generator contained in this module is not suitable for
-/// security-sensitive work.**
+/// **The PRNG is unsuitable for generating secrets or use in applications with
+/// low risk tolerance, see the module-level comment.**
 pub struct Prng {
     env: Env,
 }
@@ -47,8 +81,8 @@ impl Prng {
     ///
     /// # Warning
     ///
-    /// **The pseudo-random generator contained in this module is not suitable for
-    /// security-sensitive work.**
+    /// **The PRNG is unsuitable for generating secrets or use in applications with
+    /// low risk tolerance, see the module-level comment.**
     pub fn seed(&self, seed: Bytes) {
         let env = self.env();
         internal::Env::prng_reseed(env, seed.into()).unwrap_infallible();
@@ -62,8 +96,8 @@ impl Prng {
     ///
     /// # Warning
     ///
-    /// **The pseudo-random generator contained in this module is not suitable for
-    /// security-sensitive work.**
+    /// **The PRNG is unsuitable for generating secrets or use in applications with
+    /// low risk tolerance, see the module-level comment.**
     ///
     /// # Examples
     ///
@@ -114,8 +148,8 @@ impl Prng {
     ///
     /// # Warning
     ///
-    /// **The pseudo-random generator contained in this module is not suitable for
-    /// security-sensitive work.**
+    /// **The PRNG is unsuitable for generating secrets or use in applications with
+    /// low risk tolerance, see the module-level comment.**
     pub fn shuffle<V>(&self, v: V) -> Vec<Val>
     where
         V: IntoVal<Env, Vec<Val>>,
