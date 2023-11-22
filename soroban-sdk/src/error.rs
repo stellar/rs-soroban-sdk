85b52f74432588e6b641b17023aa9705f206a05a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-11-22 11:16:45 -0800

    docs

--- soroban-sdk/src/error.rs
+++ soroban-sdk/src/error.rs
@@ -6,13 +6,19 @@ use crate::xdr;
 /// contract.
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum InvokeError {
-    /// Abort occurs if the invoked contract function exited due to some
-    /// irrecoverable error, such as exceeding budget, or out-of-bounds access.
+    /// Abort occurs if the invoke contract panicks with a [`panic!`], or a host
+    /// function of the environment has a failure, or a runtime error occurs.
+    /// either induced by an explicit panic!, or a host function failure, or a
+    /// runtime error (like out-of-bounds access mentioned here).
     Abort,
-    /// Contract error occurs if the invoked contract function exited due to
-    /// some error that the contract has indicated with a code. May be mappable
-    /// to an enum type if the contract has defined a
-    /// [`contracterror`][crate::contracterror].
+    /// Contract error occurs if the invoked contract function exited returning
+    /// an error or called [`panic_with_error!`][crate::panic_with_error!] with
+    /// a [`contracterror`][crate::contracterror].
+    ///
+    /// If the contract defines a [`contracterror`][crate::contracterror] type
+    /// as part of its interface, this variant of the error will be convertible
+    /// to that type, but if that conversion failed then this variant of the
+    /// error would be used to represent the error.
     Contract(u32),
 }
 
