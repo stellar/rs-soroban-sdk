f109deeb05af25230079e36038b0a4b7ff8dfce8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-01-08 11:50:46 +1000

    Add test exampling failure

--- soroban-sdk/src/env.rs
+++ soroban-sdk/src/env.rs
@@ -76,7 +76,7 @@ pub mod internal {
 }
 
 pub use internal::xdr;
-pub use internal::Compare;
+// pub use internal::Compare;
 pub use internal::ConversionError;
 pub use internal::EnvBase;
 pub use internal::Error;
