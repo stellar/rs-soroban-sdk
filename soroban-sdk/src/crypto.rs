22591b0579b80a6b0345a61c36773f7279a98200
Jay Geng <jay@stellar.org>
2024-04-15 18:47:52 -0400

    Add secp256r1 sig verify, split crypto interface into "safe" (using `Hash` type) and "hazmat"
    remove mistakenly checked-in test observation file

--- soroban-sdk/src/crypto.rs
+++ soroban-sdk/src/crypto.rs
@@ -103,6 +103,59 @@ impl Crypto {
     /// for a given recovery_id byte.
     pub fn secp256k1_recover(
         &self,
+        message_digest: &Hash<32>,
+        signature: &BytesN<64>,
+        recorvery_id: u32,
+    ) -> BytesN<65> {
+        let env = self.env();
+        CryptoHazmat::new(env).secp256k1_recover(&message_digest.0, signature, recorvery_id)
+    }
+
+    /// Verifies the ECDSA secp256r1 signature.
+    ///
+    /// The SEC-1-encoded public key is provided along with the message,
+    /// verifies the 64-byte signature.
+    pub fn secp256r1_verify(
+        &self,
+        public_key: &BytesN<65>,
+        message_digest: &Hash<32>,
+        signature: &BytesN<64>,
+    ) {
+        let env = self.env();
+        CryptoHazmat::new(env).secp256r1_verify(public_key, &message_digest.0, signature)
+    }
+}
+
+/// Hazardous Materials
+///
+/// Cryptographic functions under [CryptoHazmat] are low-leveled which can be
+/// insecure if misused. They are not generally recommended. Using them
+/// incorrectly can introduce security vulnerabilities. Please use [Crypto] if
+/// possible.
+pub struct CryptoHazmat {
+    env: Env,
+}
+
+impl CryptoHazmat {
+    pub(crate) fn new(env: &Env) -> CryptoHazmat {
+        CryptoHazmat { env: env.clone() }
+    }
+
+    pub fn env(&self) -> &Env {
+        &self.env
+    }
+
+    /// Recovers the ECDSA secp256k1 public key.
+    ///
+    /// The public key returned is the SEC-1-encoded ECDSA secp256k1 public key
+    /// that produced the 64-byte signature over a given 32-byte message digest,
+    /// for a given recovery_id byte.
+    ///
+    /// WARNING: The `message_digest` must be produced by a secure cryptographic
+    /// hash function on the message, otherwise the attacker can potentially
+    /// forge signatures.
+    pub fn secp256k1_recover(
+        &self,
         message_digest: &BytesN<32>,
         signature: &BytesN<64>,
         recorvery_id: u32,
@@ -116,5 +169,29 @@ impl Crypto {
         )
         .unwrap_infallible();
         unsafe { BytesN::unchecked_new(env.clone(), bytes) }
+    }
+
+    /// Verifies the ECDSA secp256r1 signature.
+    ///
+    /// The SEC-1-encoded public key is provided along with a 32-byte message
+    /// digest, verifies the 64-byte signature.
+    ///
+    /// WARNING: The `message_digest` must be produced by a secure cryptographic
+    /// hash function on the message, otherwise the attacker can potentially
+    /// forge signatures.
+    pub fn secp256r1_verify(
+        &self,
+        public_key: &BytesN<65>,
+        message_digest: &BytesN<32>,
+        signature: &BytesN<64>,
+    ) {
+        let env = self.env();
+        let _ = internal::Env::verify_sig_ecdsa_secp256r1(
+            env,
+            public_key.to_object(),
+            message_digest.to_object(),
+            signature.to_object(),
+        )
+        .unwrap_infallible();
     }
 }
