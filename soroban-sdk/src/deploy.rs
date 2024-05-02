a2991e9424acd83f4fc600ffd98b22ff2c1fdc07
Jay Geng <jay@stellar.org>
2024-05-02 15:01:25 -0400

    Add docs for extend_ttl_for_{code,contract_instance} (#1268)
    ### What
    
    [TODO: Short statement about what is changing.]
    
    ### Why
    
    [TODO: Why this change is being made. Include any context required to
    understand the why.]
    
    ### Known limitations
    
    [TODO or N/A]

--- soroban-sdk/src/deploy.rs
+++ soroban-sdk/src/deploy.rs
@@ -160,6 +160,9 @@ impl Deployer {
             .unwrap_infallible();
     }
 
+    /// Extend the TTL of the contract instance.
+    ///
+    /// Same as [`extend_ttl`](Self::extend_ttl) but only for contract instance.
     pub fn extend_ttl_for_contract_instance(
         &self,
         contract_address: Address,
@@ -175,6 +178,9 @@ impl Deployer {
             .unwrap_infallible();
     }
 
+    /// Extend the TTL of the contract code.
+    ///
+    /// Same as [`extend_ttl`](Self::extend_ttl) but only for contract code.
     pub fn extend_ttl_for_code(&self, contract_address: Address, threshold: u32, extend_to: u32) {
         self.env
             .extend_contract_code_ttl(
