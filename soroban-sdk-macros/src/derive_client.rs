cd541d059e14ca7e12b1309aa2e4f705f69c8ce5
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-07-18 16:31:24 +1000

    Keep testutils on/off state internal to the sdk (#1301)
    ### What
    Change how testutils code is generated to make it rely only on the state
    of the SDK testutils feature, and not influenced at all by the contract
    crate's feature set or test cfg.
    
    Today the SDK macros generate both non-testutils and testutils code and
    it is up to the contract crate to enable or disable it. With this change
    the SDK macros only generate testutils code if the SDK is compiled with
    testutils.
    
    ### Why
    Today both the SDK and the contract crate must be configured with the
    testutils feature (or the test cfg) to enable the testutils that are
    generated by the SDK macros.
    
    The SDK has a testutils feature that enables the testutils functions and
    types in the SDK.
    
    The contract crate generated code also has code behind a testutils
    feature, and the contract crate testutils feature (or test cfg) must be
    enabled for them to function as well.
    
    This largely works, but it results in an oddity in more complex
    workspace setups that you can be using a testutils SDK with a
    non-testutils compiled contract crate meaning that none of the types or
    functions in that crate can be used with testutils functions.
    
    This largely goes unnoticed because it is a bit of an edge case and
    definitely not apparent in simpler setups. Even larger setups may never
    notice this as an oddity if they always happen to configure their crates
    appropriately.
    
    This also goes unnoticed because when the test cfg is enabled (when
    building for tests) we also enable all that testutils generated code.
    
    This issue has become more apparent to me lately because I'm
    experimenting with Soroban contracts inside Jupyter notebooks, as one
    way that folks can experiment and learn Soroban.
    
    The Rust runtime kernel for Jupyter notebooks doesn't currently support
    enable features for code in the notebook so I end up with an odd
    situation where testutils are enabled in the SDK, but not in the
    contract.
    
    After looking at how to solve this problem it occurred to me that the
    way we implemented the generated testutils code is not ideal, even
    independent of this problem.
    
    Instead of implementing the generated code so it contains a new
    testutils feature for the contract crate, we should have made all the
    generated testutils code enabled/disabled based on the SDKs feature
    flag.
    
    The advantage of this is that in any workspace whenever you have
    testutils enabled on the SDK, all contracts will also have testutils
    enabled. Instead of having many levers to pull to get testutils enabled,
    there is just one.
    
    ### Reviewing
    
    I recommend reviewing with whitespace changes disabled, because a large
    portion of the diff is the change to indents when some code was moved
    inside if..else statements.
    
    ### Merging
    
    This change _should_ be practically non-breaking. While technically it
    is removing code from being generated when compiled with an SDK without
    testutils enabled, most of the testutils code in generated code would be
    unusable without it anyway.
    
    It might be warranted to hold this change until v22, just for the
    purpose of shipping it in a preview / rc and getting feedback before
    committing to it. But I think the change is also pretty straightforward
    and low risk.
    
    I'd like to merge this in a minor release, but it warrants some
    discussion.

--- soroban-sdk-macros/src/derive_client.rs
+++ soroban-sdk-macros/src/derive_client.rs
@@ -9,119 +9,124 @@ pub fn derive_client_type(crate_path: &Path, ty: &str, name: &str) -> TokenStrea
     // Render the Client.
     let client_doc = format!("{name} is a client for calling the contract defined in {ty_str}.");
     let client_ident = format_ident!("{}", name);
-    quote! {
-        #[doc = #client_doc]
-        pub struct #client_ident<'a> {
-            pub env: #crate_path::Env,
-            pub address: #crate_path::Address,
-            #[doc(hidden)]
-            #[cfg(not(any(test, feature = "testutils")))]
-            _phantom: core::marker::PhantomData<&'a ()>,
-            #[doc(hidden)]
-            #[cfg(any(test, feature = "testutils"))]
-            set_auths: Option<&'a [#crate_path::xdr::SorobanAuthorizationEntry]>,
-            #[doc(hidden)]
-            #[cfg(any(test, feature = "testutils"))]
-            mock_auths: Option<&'a [#crate_path::testutils::MockAuth<'a>]>,
-            #[doc(hidden)]
-            #[cfg(any(test, feature = "testutils"))]
-            mock_all_auths: bool,
-            #[doc(hidden)]
-            #[cfg(any(test, feature = "testutils"))]
-            allow_non_root_auth: bool,
+    if cfg!(not(feature = "testutils")) {
+        quote! {
+            #[doc = #client_doc]
+            pub struct #client_ident<'a> {
+                pub env: #crate_path::Env,
+                pub address: #crate_path::Address,
+                #[doc(hidden)]
+                _phantom: core::marker::PhantomData<&'a ()>,
+            }
+
+            impl<'a> #client_ident<'a> {
+                pub fn new(env: &#crate_path::Env, address: &#crate_path::Address) -> Self {
+                    Self {
+                        env: env.clone(),
+                        address: address.clone(),
+                        _phantom: core::marker::PhantomData,
+                    }
+                }
+            }
         }
-
-        impl<'a> #client_ident<'a> {
-            pub fn new(env: &#crate_path::Env, address: &#crate_path::Address) -> Self {
-                Self {
-                    env: env.clone(),
-                    address: address.clone(),
-                    #[cfg(not(any(test, feature = "testutils")))]
-                    _phantom: core::marker::PhantomData,
-                    #[cfg(any(test, feature = "testutils"))]
-                    set_auths: None,
-                    #[cfg(any(test, feature = "testutils"))]
-                    mock_auths: None,
-                    #[cfg(any(test, feature = "testutils"))]
-                    mock_all_auths: false,
-                    #[cfg(any(test, feature = "testutils"))]
-                    allow_non_root_auth: false,
-                }
+    } else {
+        quote! {
+            #[doc = #client_doc]
+            pub struct #client_ident<'a> {
+                pub env: #crate_path::Env,
+                pub address: #crate_path::Address,
+                #[doc(hidden)]
+                set_auths: Option<&'a [#crate_path::xdr::SorobanAuthorizationEntry]>,
+                #[doc(hidden)]
+                mock_auths: Option<&'a [#crate_path::testutils::MockAuth<'a>]>,
+                #[doc(hidden)]
+                mock_all_auths: bool,
+                #[doc(hidden)]
+                allow_non_root_auth: bool,
             }
 
-            /// Set authorizations in the environment which will be consumed by
-            /// contracts when they invoke `Address::require_auth` or
-            /// `Address::require_auth_for_args` functions.
-            ///
-            /// Requires valid signatures for the authorization to be successful.
-            /// To mock auth without requiring valid signatures, use `mock_auths`.
-            ///
-            /// See `soroban_sdk::Env::set_auths` for more details and examples.
-            #[cfg(any(test, feature = "testutils"))]
-            pub fn set_auths(&self, auths: &'a [#crate_path::xdr::SorobanAuthorizationEntry]) -> Self {
-                Self {
-                    env: self.env.clone(),
-                    address: self.address.clone(),
-                    set_auths: Some(auths),
-                    mock_auths: self.mock_auths.clone(),
-                    mock_all_auths: false,
-                    allow_non_root_auth: false,
+            impl<'a> #client_ident<'a> {
+                pub fn new(env: &#crate_path::Env, address: &#crate_path::Address) -> Self {
+                    Self {
+                        env: env.clone(),
+                        address: address.clone(),
+                        set_auths: None,
+                        mock_auths: None,
+                        mock_all_auths: false,
+                        allow_non_root_auth: false,
+                    }
                 }
-            }
 
-            /// Mock authorizations in the environment which will cause matching invokes
-            /// of `Address::require_auth` and `Address::require_auth_for_args` to
-            /// pass.
-            ///
-            /// See `soroban_sdk::Env::set_auths` for more details and examples.
-            #[cfg(any(test, feature = "testutils"))]
-            pub fn mock_auths(&self, mock_auths: &'a [#crate_path::testutils::MockAuth<'a>]) -> Self {
-                Self {
-                    env: self.env.clone(),
-                    address: self.address.clone(),
-                    set_auths: self.set_auths.clone(),
-                    mock_auths: Some(mock_auths),
-                    mock_all_auths: false,
-                    allow_non_root_auth: false,
+                /// Set authorizations in the environment which will be consumed by
+                /// contracts when they invoke `Address::require_auth` or
+                /// `Address::require_auth_for_args` functions.
+                ///
+                /// Requires valid signatures for the authorization to be successful.
+                /// To mock auth without requiring valid signatures, use `mock_auths`.
+                ///
+                /// See `soroban_sdk::Env::set_auths` for more details and examples.
+                pub fn set_auths(&self, auths: &'a [#crate_path::xdr::SorobanAuthorizationEntry]) -> Self {
+                    Self {
+                        env: self.env.clone(),
+                        address: self.address.clone(),
+                        set_auths: Some(auths),
+                        mock_auths: self.mock_auths.clone(),
+                        mock_all_auths: false,
+                        allow_non_root_auth: false,
+                    }
                 }
-            }
 
-            /// Mock all calls to the `Address::require_auth` and
-            /// `Address::require_auth_for_args` functions in invoked contracts,
-            /// having them succeed as if authorization was provided.
-            ///
-            /// See `soroban_sdk::Env::mock_all_auths` for more details and
-            /// examples.
-            #[cfg(any(test, feature = "testutils"))]
-            pub fn mock_all_auths(&self) -> Self {
-                Self {
-                    env: self.env.clone(),
-                    address: self.address.clone(),
-                    set_auths: None,
-                    mock_auths: None,
-                    mock_all_auths: true,
-                    allow_non_root_auth: false,
+                /// Mock authorizations in the environment which will cause matching invokes
+                /// of `Address::require_auth` and `Address::require_auth_for_args` to
+                /// pass.
+                ///
+                /// See `soroban_sdk::Env::set_auths` for more details and examples.
+                pub fn mock_auths(&self, mock_auths: &'a [#crate_path::testutils::MockAuth<'a>]) -> Self {
+                    Self {
+                        env: self.env.clone(),
+                        address: self.address.clone(),
+                        set_auths: self.set_auths.clone(),
+                        mock_auths: Some(mock_auths),
+                        mock_all_auths: false,
+                        allow_non_root_auth: false,
+                    }
                 }
-            }
 
-            /// A version of `mock_all_auths` that allows authorizations that
-            /// are not present in the root invocation.
-            ///
-            /// Refer to `mock_all_auths` documentation for details and
-            /// prefer using `mock_all_auths` unless non-root authorization is
-            /// required.
-            ///
-            /// See `soroban_sdk::Env::mock_all_auths_allowing_non_root_auth`
-            /// for more details and examples.
-            #[cfg(any(test, feature = "testutils"))]
-            pub fn mock_all_auths_allowing_non_root_auth(&self) -> Self {
-                Self {
-                    env: self.env.clone(),
-                    address: self.address.clone(),
-                    set_auths: None,
-                    mock_auths: None,
-                    mock_all_auths: true,
-                    allow_non_root_auth: true,
+                /// Mock all calls to the `Address::require_auth` and
+                /// `Address::require_auth_for_args` functions in invoked contracts,
+                /// having them succeed as if authorization was provided.
+                ///
+                /// See `soroban_sdk::Env::mock_all_auths` for more details and
+                /// examples.
+                pub fn mock_all_auths(&self) -> Self {
+                    Self {
+                        env: self.env.clone(),
+                        address: self.address.clone(),
+                        set_auths: None,
+                        mock_auths: None,
+                        mock_all_auths: true,
+                        allow_non_root_auth: false,
+                    }
+                }
+
+                /// A version of `mock_all_auths` that allows authorizations that
+                /// are not present in the root invocation.
+                ///
+                /// Refer to `mock_all_auths` documentation for details and
+                /// prefer using `mock_all_auths` unless non-root authorization is
+                /// required.
+                ///
+                /// See `soroban_sdk::Env::mock_all_auths_allowing_non_root_auth`
+                /// for more details and examples.
+                pub fn mock_all_auths_allowing_non_root_auth(&self) -> Self {
+                    Self {
+                        env: self.env.clone(),
+                        address: self.address.clone(),
+                        set_auths: None,
+                        mock_auths: None,
+                        mock_all_auths: true,
+                        allow_non_root_auth: true,
+                    }
                 }
             }
         }
@@ -193,74 +198,94 @@ pub fn derive_client_impl(crate_path: &Path, name: &str, fns: &[syn_ext::Fn]) ->
             let fn_output = f.output();
             let fn_try_output = f.try_output(crate_path);
             let fn_attrs = f.attrs;
-            quote! {
-                #(#fn_attrs)*
-                pub fn #fn_ident(&self, #(#fn_input_types),*) -> #fn_output {
-                    use core::ops::Not;
-                    #[cfg(any(test, feature = "testutils"))]
-                    let old_auth_manager = self.env.in_contract().not().then(||
-                        self.env.host().snapshot_auth_manager().unwrap()
-                    );
-                    #[cfg(any(test, feature = "testutils"))]
-                    {
-                        if let Some(set_auths) = self.set_auths {
-                            self.env.set_auths(set_auths);
+            if cfg!(not(feature = "testutils")) {
+                quote! {
+                    #(#fn_attrs)*
+                    pub fn #fn_ident(&self, #(#fn_input_types),*) -> #fn_output {
+                        use core::ops::Not;
+                        use #crate_path::{IntoVal,FromVal};
+                        let res = self.env.invoke_contract(
+                            &self.address,
+                            &#fn_name_symbol,
+                            #crate_path::vec![&self.env, #(#fn_input_names.into_val(&self.env)),*],
+                        );
+                        res
+                    }
+
+                    #(#fn_attrs)*
+                    pub fn #fn_try_ident(&self, #(#fn_input_types),*) -> #fn_try_output {
+                        use #crate_path::{IntoVal,FromVal};
+                        let res = self.env.try_invoke_contract(
+                            &self.address,
+                            &#fn_name_symbol,
+                            #crate_path::vec![&self.env, #(#fn_input_names.into_val(&self.env)),*],
+                        );
+                        res
+                    }
+                }
+            } else {
+                quote! {
+                    #(#fn_attrs)*
+                    pub fn #fn_ident(&self, #(#fn_input_types),*) -> #fn_output {
+                        use core::ops::Not;
+                        let old_auth_manager = self.env.in_contract().not().then(||
+                            self.env.host().snapshot_auth_manager().unwrap()
+                        );
+                        {
+                            if let Some(set_auths) = self.set_auths {
+                                self.env.set_auths(set_auths);
+                            }
+                            if let Some(mock_auths) = self.mock_auths {
+                                self.env.mock_auths(mock_auths);
+                            }
+                            if self.mock_all_auths {
+                                if self.allow_non_root_auth {
+                                    self.env.mock_all_auths_allowing_non_root_auth();
+                                } else {
+                                    self.env.mock_all_auths();
+                                }
+                            }
                         }
-                        if let Some(mock_auths) = self.mock_auths {
-                            self.env.mock_auths(mock_auths);
+                        use #crate_path::{IntoVal,FromVal};
+                        let res = self.env.invoke_contract(
+                            &self.address,
+                            &#fn_name_symbol,
+                            #crate_path::vec![&self.env, #(#fn_input_names.into_val(&self.env)),*],
+                        );
+                        if let Some(old_auth_manager) = old_auth_manager {
+                            self.env.host().set_auth_manager(old_auth_manager).unwrap();
                         }
-                        if self.mock_all_auths {
-                            if self.allow_non_root_auth {
-                                self.env.mock_all_auths_allowing_non_root_auth();
-                            } else {
+                        res
+                    }
+
+                    #(#fn_attrs)*
+                    pub fn #fn_try_ident(&self, #(#fn_input_types),*) -> #fn_try_output {
+                        use core::ops::Not;
+                        let old_auth_manager = self.env.in_contract().not().then(||
+                            self.env.host().snapshot_auth_manager().unwrap()
+                        );
+                        {
+                            if let Some(set_auths) = self.set_auths {
+                                self.env.set_auths(set_auths);
+                            }
+                            if let Some(mock_auths) = self.mock_auths {
+                                self.env.mock_auths(mock_auths);
+                            }
+                            if self.mock_all_auths {
                                 self.env.mock_all_auths();
                             }
                         }
-                    }
-                    use #crate_path::{IntoVal,FromVal};
-                    let res = self.env.invoke_contract(
-                        &self.address,
-                        &#fn_name_symbol,
-                        #crate_path::vec![&self.env, #(#fn_input_names.into_val(&self.env)),*],
-                    );
-                    #[cfg(any(test, feature = "testutils"))]
-                    if let Some(old_auth_manager) = old_auth_manager {
-                        self.env.host().set_auth_manager(old_auth_manager).unwrap();
-                    }
-                    res
-                }
-
-                #(#fn_attrs)*
-                pub fn #fn_try_ident(&self, #(#fn_input_types),*) -> #fn_try_output {
-                    #[cfg(any(test, feature = "testutils"))]
-                    use core::ops::Not;
-                    #[cfg(any(test, feature = "testutils"))]
-                    let old_auth_manager = self.env.in_contract().not().then(||
-                        self.env.host().snapshot_auth_manager().unwrap()
-                    );
-                    #[cfg(any(test, feature = "testutils"))]
-                    {
-                        if let Some(set_auths) = self.set_auths {
-                            self.env.set_auths(set_auths);
+                        use #crate_path::{IntoVal,FromVal};
+                        let res = self.env.try_invoke_contract(
+                            &self.address,
+                            &#fn_name_symbol,
+                            #crate_path::vec![&self.env, #(#fn_input_names.into_val(&self.env)),*],
+                        );
+                        if let Some(old_auth_manager) = old_auth_manager {
+                            self.env.host().set_auth_manager(old_auth_manager).unwrap();
                         }
-                        if let Some(mock_auths) = self.mock_auths {
-                            self.env.mock_auths(mock_auths);
-                        }
-                        if self.mock_all_auths {
-                            self.env.mock_all_auths();
-                        }
+                        res
                     }
-                    use #crate_path::{IntoVal,FromVal};
-                    let res = self.env.try_invoke_contract(
-                        &self.address,
-                        &#fn_name_symbol,
-                        #crate_path::vec![&self.env, #(#fn_input_names.into_val(&self.env)),*],
-                    );
-                    #[cfg(any(test, feature = "testutils"))]
-                    if let Some(old_auth_manager) = old_auth_manager {
-                        self.env.host().set_auth_manager(old_auth_manager).unwrap();
-                    }
-                    res
                 }
             }
         })
