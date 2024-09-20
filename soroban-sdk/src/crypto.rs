34437add345af8ba188805cd049660dd230ffd47
Siddharth Suresh <siddharth@stellar.org>
2024-09-20 16:19:30 -0700

    Fix doc comment

--- soroban-sdk/src/crypto.rs
+++ soroban-sdk/src/crypto.rs
@@ -286,7 +286,7 @@ pub struct Bls12_381 {
 /// ```rust
 /// use soroban_sdk::{Env, bytesn, crypto::{Bls12_381, G1Affine}};
 /// let env = Env::default();
-/// let bls12_381 = env.bls12_381();
+/// let bls12_381 = env.crypto().bls12_381();
 /// let zero = G1Affine::from_bytes(bytesn!(&env, 0x400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000));
 /// let one = G1Affine::from_bytes(bytesn!(&env, 0x17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb08b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e1));
 /// let res = bls12_381.g1_add(&zero, &one);
