d463e0f28919237a5f46f6cca8d0d4ccf2d3c332
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-09-30 08:42:50 +1000

    tweak

--- soroban-sdk/src/lib.rs
+++ soroban-sdk/src/lib.rs
@@ -48,10 +48,10 @@
 compile_error!("'testutils' feature is not supported on 'wasm' target");
 
 #[cfg(all(target_family = "wasm", target_feature = "reference-types",))]
-compile_error!("Compiler configuration is unsupported by Soroban Environment, because a Wasm target-feature is enabled that is not yet supported: reference-types. Use Rust 1.81 or older.");
+compile_error!("Compiler configuration is unsupported by Soroban Environment, because a Wasm target-feature is enabled that is not yet supported: reference-types. Use Rust 1.81 or older to get a compatible default configuration.");
 
 #[cfg(all(target_family = "wasm", target_feature = "multivalue",))]
-compile_error!("Compiler configuration is unsupported by Soroban Environment, because a Wasm target-feature is enabled that is not yet supported: multivalue. Use Rust 1.81 or older.");
+compile_error!("Compiler configuration is unsupported by Soroban Environment, because a Wasm target-feature is enabled that is not yet supported: multivalue. Use Rust 1.81 or older to get a compatible default configuration.");
 
 // When used in a no_std contract, provide a panic handler as one is required.
 #[cfg(all(not(feature = "alloc"), target_family = "wasm"))]
