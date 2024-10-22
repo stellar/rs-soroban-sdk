7994498da6caca403167917b83043cb36c7f6de8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-10-22 13:17:50 +1000

    tweak error message

--- soroban-sdk/build.rs
+++ soroban-sdk/build.rs
@@ -1,7 +1,7 @@
 pub fn main() {
     if let Ok(version) = rustc_version::version() {
         if version.major == 1 && version.minor >= 82 {
-            panic!("Rust compiler 1.82+ is unsupported by the Soroban Environment, because a Wasm target-feature is enabled that is not yet supported and not easily disabled: reference-types, multi-value. Use Rust 1.81.");
+            panic!("Rust compiler 1.82+ is unsupported by the Soroban Environment, because Wasm target features are enabled that are not yet supported and not easily disabled: reference-types, multi-value. Use Rust 1.81.");
         }
     }
 }
