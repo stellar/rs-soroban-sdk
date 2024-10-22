8a3090af9c1e272e4d71997cd3019bf84ea7ce8f
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-10-23 06:54:07 +1000

    constrain to wasm releases

--- soroban-sdk/build.rs
+++ soroban-sdk/build.rs
@@ -1,4 +1,5 @@
 pub fn main() {
+    #[cfg(all(target_family = "wasm", target_os = "unknown"))]
     if let Ok(version) = rustc_version::version() {
         if version.major == 1 && version.minor >= 82 {
             panic!("Rust compiler 1.82+ is unsupported by the Soroban Environment, because Wasm target features are enabled that are not yet supported and not easily disabled: reference-types, multi-value. Use Rust 1.81.");
