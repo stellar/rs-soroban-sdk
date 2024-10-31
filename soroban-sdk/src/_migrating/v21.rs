df6f81553f95d890bb97e0bd3fafc30cbad78c6e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-11-01 06:35:26 +1000

    wip

--- soroban-sdk/src/_migrating/v21.rs
+++ soroban-sdk/src/_migrating/v21.rs
@@ -1,3 +1,51 @@
 //! # Migrating from v20 to v21
 //!
-//! - `CustomAccountInterface`'s `__check_auth` function `signature_payload` parameter changes from type `BytesN<32>` to `Hash<32>`.
+//! ## `CustomAccountInterface`'s `__check_auth` function `signature_payload` parameter changes from type `BytesN<32>` to `Hash<32>`.
+//!
+//! ### Before
+//!
+//! ```
+//! # use soroban_sdk_21 as soroban_sdk;
+//! use soroban_sdk::auth::CustomAccountInterface;
+//!
+//! #[contract]
+//! pub struct Contract;
+//!
+//! impl CustomAccountInterface for Contract {
+//!     type Signature = ();
+//!     type Error: Into<Error> = u32;
+//!
+//!     fn __check_auth(
+//!         env: Env,
+//!         signature_payload: BytesN<32>,
+//!         signatures: Self::Signature,
+//!         auth_contexts: Vec<Context>,
+//!     ) -> Result<(), Self::Error> {
+//!         // ...
+//!     }
+//! }
+//! ```
+//!
+//! ### After
+//!
+//! ```
+//! use soroban_sdk::auth::CustomAccountInterface;
+//!
+//! #[contract]
+//! pub struct Contract;
+//!
+//! impl CustomAccountInterface for Contract {
+//!     type Signature = ();
+//!     type Error: Into<Error> = u32;
+//!
+//!     fn __check_auth(
+//!         env: Env,
+//!         signature_payload: Hash<32>,
+//!         signatures: Self::Signature,
+//!         auth_contexts: Vec<Context>,
+//!     ) -> Result<(), Self::Error> {
+//!         // ...
+//!     }
+//! }
+//! ```
+//!
