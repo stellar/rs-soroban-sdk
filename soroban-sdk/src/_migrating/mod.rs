514516df0c75b2453130445d9df17999460003c5
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-11-04 15:28:33 +1000

    make it one file

--- soroban-sdk/src/_migrating/mod.rs
+++
@@ -1,4 +0,0 @@
-//! How to migrate major releases of the soroban-sdk.
-
-pub mod v21;
-pub mod v22;
--- soroban-sdk/src/_migrating/v22.rs
+++
@@ -1,7 +0,0 @@
-//! # Migrating from v21 to v22
-//!
-//! - Add `Env` functions `register`` and `register_at` to replace `register_contract`, `register_contract_wasm`.
-//!
-//! - Deprecate `fuzz_catch_panic`. Use `Env` `try_invoke_contract` and the `try_` client invoke functions instead.
-//!
-//! - Events in test snapshots are now reduced to only contract events and system events. Diagnostic events will no longer appear in test snapshots.
