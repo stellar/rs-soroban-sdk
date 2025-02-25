5ddc8b0088bd3f14820a438a506780b391b4f315
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-02-26 07:34:15 +1000

    remove token muxed extension import

--- soroban-token-sdk/src/event.rs
+++ soroban-token-sdk/src/event.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{contracttype, symbol_short, token::muxed_ext::Mux, Address, Env, Symbol};
+use soroban_sdk::{contracttype, symbol_short, Address, Env, Symbol};
 
 pub struct Events {
     env: Env,
