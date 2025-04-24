bb5d643bbae697135a79c44dbcb6d7a194a23f10
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-04-24 16:39:08 +1000

    feat: Implement remove_attributes_from_item function

--- soroban-sdk-macros/src/attribute.rs
+++ soroban-sdk-macros/src/attribute.rs
@@ -1,4 +1,4 @@
-use syn::Attribute;
+use syn::{Attribute, Data, DeriveInput, Fields};
 
 /// Returns true if the attribute is an attribute that should be preserved and
 /// passed through to code generated for the item the attribute is on.
@@ -10,5 +10,29 @@ pub fn pass_through_attr_to_gen_code(attr: &Attribute) -> bool {
 }
 
 pub fn remove_attributes_from_item(input: &mut DeriveInput) {
-
+    if let Data::Struct(ref mut data_struct) = input.data {
+        match &mut data_struct.fields {
+            Fields::Named(ref mut fields_named) => {
+                for field in fields_named.named.iter_mut() {
+                    // Retain only attributes that are NOT 'topic' or 'data'.
+                    field.attrs.retain(|attr| {
+                        !attr.path().is_ident("topic") && !attr.path().is_ident("data")
+                    });
+                }
+            }
+            Fields::Unnamed(ref mut fields_unnamed) => {
+                for field in fields_unnamed.unnamed.iter_mut() {
+                    // Retain only attributes that are NOT 'topic' or 'data'.
+                    field.attrs.retain(|attr| {
+                        !attr.path().is_ident("topic") && !attr.path().is_ident("data")
+                    });
+                }
+            }
+            Fields::Unit => {
+                // Unit structs have no fields, nothing to do.
+            }
+        }
+    }
+    // If the DeriveInput is for an Enum or Union, we don't need to do anything
+    // as the request only specified removing attributes from struct fields.
 }
