a778eb738b2a6d22b83087538c9d406783a13199
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-08-08 13:59:23 +1000

    add set_authorized

--- soroban-sdk/src/tests/token_events_stellar_asset.rs
+++ soroban-sdk/src/tests/token_events_stellar_asset.rs
@@ -60,21 +60,46 @@ fn test_set_admin() {
 #[test]
 fn test_set_authorized() {
     let env = Env::default();
-    let id = env.register(Contract, ());
+    env.mock_all_auths();
+
+    let authorizee = Address::generate(&env);
+    let authorize = true;
+
     let event = SetAuthorized {
-        id: Address::generate(&env),
+        id: authorizee.clone(),
         authorize: true,
     };
+
+    // Verify the event publishes the expected topics and data.
+    let topics = (Symbol::new(&env, "set_authorized"), authorizee.clone());
+    let data = authorize;
+
+    let id = env.register(Contract, ());
     env.as_contract(&id, || event.publish(&env));
+    let token_events = env.events().all();
     assert_eq!(
-        env.events().all(),
+        token_events,
         vec![
             &env,
-            (
-                id.clone(),
-                (Symbol::new(&env, "set_authorized"), event.id.clone(),).into_val(&env),
-                true.into_val(&env),
-            ),
+            (id.clone(), topics.into_val(&env), data.into_val(&env)),
+        ]
+    );
+
+    // Verify the event published is consistent with the asset contract.
+    let admin = Address::generate(&env);
+    let asset = env.register_stellar_asset_contract_v2(admin);
+    let client = StellarAssetClient::new(&env, &asset.address());
+
+    let (t0, t1) = topics;
+    let topics = (t0, t1, client.name());
+
+    client.set_authorized(&authorizee, &authorize);
+    let asset_events = env.events().all();
+    assert_eq!(
+        asset_events,
+        vec![
+            &env,
+            (asset.address(), topics.into_val(&env), data.into_val(&env)),
         ]
     );
 }
