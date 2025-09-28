7d20612ee89d692664bbd618367ee36dc90a405d
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-09-29 08:06:44 +1000

    remove less valuable test cases

--- soroban-meta/src/tests.rs
+++ soroban-meta/src/tests.rs
@@ -17,37 +17,9 @@ fn test_from_wasm() {
 }
 
 #[test]
-fn test_raw_from_wasm() {
-    let wasm = fs::read("../target/wasm32v1-none/release/test_zero.wasm").unwrap();
-    let raw_meta = read::raw_from_wasm(&wasm).unwrap();
-    assert!(!raw_meta.is_empty());
-}
-
-#[test]
-fn test_parse_raw() {
-    let wasm = fs::read("../target/wasm32v1-none/release/test_zero.wasm").unwrap();
-    let raw_meta = read::raw_from_wasm(&wasm).unwrap();
-    let meta = read::parse_raw(&raw_meta).unwrap();
-    assert!(!meta.is_empty());
-}
-
-#[test]
 fn test_from_wasm_no_metadata() {
     // Create a simple Wasm file without metadata
     let wasm = vec![0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]; // minimal Wasm header
     let result = read::from_wasm(&wasm);
     assert!(matches!(result, Err(read::FromWasmError::NotFound)));
-}
-
-#[test]
-fn test_raw_from_wasm_no_metadata() {
-    let wasm = vec![0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00];
-    let result = read::raw_from_wasm(&wasm);
-    assert!(matches!(result, Err(read::FromWasmError::NotFound)));
-}
-
-#[test]
-fn test_parse_raw_invalid() {
-    let result = read::parse_raw(b"invalid xdr data");
-    assert!(matches!(result, Err(_)));
 }
