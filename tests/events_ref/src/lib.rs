434b9a8103a25fba879654be4361771abdf8c2d6
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-10-21 03:47:25 +1000

    Add tests for capturing some ways that generics are permitted (#1601)
    ### What
    Add a new test vectors that uses generic lifetimes with the contract
    macros.
    
      ### Why
    The soroban-sdk macros have limited support for generics. We should
    exercise them in the test vectors to ensure that support doesn't change
    over time.
    
    For #1544

---
+++ tests/events_ref/src/lib.rs
@@ -0,0 +1,135 @@
+#![no_std]
+use soroban_sdk::{contract, contractevent, contractimpl, Address, Env, MuxedAddress};
+
+#[contract]
+pub struct Contract;
+
+#[contractevent]
+pub struct Transfer<'a> {
+    #[topic]
+    from: &'a Address,
+    #[topic]
+    to: &'a Address,
+    amount: &'a i128,
+    to_muxed_id: Option<&'a u64>,
+}
+
+#[contractimpl]
+impl Contract {
+    pub fn transfer(env: Env, from: Address, to: MuxedAddress, amount: i128) {
+        Transfer {
+            from: &from,
+            to: &to.address(),
+            amount: &amount,
+            to_muxed_id: to.id().as_ref(),
+        }
+        .publish(&env);
+    }
+
+    pub fn failed_transfer(env: Env, from: Address, to: Address, amount: i128) {
+        Transfer {
+            from: &from,
+            to: &to,
+            amount: &amount,
+            to_muxed_id: None,
+        }
+        .publish(&env);
+        panic!("fail");
+    }
+}
+
+#[cfg(test)]
+mod test {
+    extern crate alloc;
+    use soroban_sdk::{
+        map, symbol_short,
+        testutils::{Address as _, Events, MuxedAddress as _},
+        vec, Address, Env, IntoVal, MuxedAddress, Symbol, Val,
+    };
+
+    use crate::{Contract, ContractClient};
+
+    #[test]
+    fn test_event() {
+        let env = Env::default();
+        let contract_id = env.register(Contract, ());
+        let client = ContractClient::new(&env, &contract_id);
+
+        let from = Address::generate(&env);
+        let to = MuxedAddress::generate(&env);
+        let amount = 1i128;
+
+        client.transfer(&from, &to, &amount);
+
+        assert_eq!(
+            env.events().all(),
+            vec![
+                &env,
+                (
+                    contract_id.clone(),
+                    // Expect these event topics.
+                    (Symbol::new(&env, "transfer"), &from, to.address()).into_val(&env),
+                    // Expect this event body.
+                    map![
+                        &env,
+                        (
+                            symbol_short!("amount"),
+                            <_ as IntoVal<Env, Val>>::into_val(&1i128, &env)
+                        ),
+                        (
+                            Symbol::new(&env, "to_muxed_id"),
+                            <_ as IntoVal<Env, Val>>::into_val(&to.id().unwrap(), &env)
+                        ),
+                    ]
+                    .to_val()
+                ),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_event_with_option_none() {
+        let env = Env::default();
+        let contract_id = env.register(Contract, ());
+        let client = ContractClient::new(&env, &contract_id);
+
+        let from = Address::generate(&env);
+        let to = Address::generate(&env);
+        let amount = 1i128;
+
+        client.transfer(&from, &to, &amount);
+
+        assert_eq!(
+            env.events().all(),
+            vec![
+                &env,
+                (
+                    contract_id.clone(),
+                    // Expect these event topics.
+                    (Symbol::new(&env, "transfer"), &from, &to).into_val(&env),
+                    // Expect this event body.
+                    map![
+                        &env,
+                        (
+                            symbol_short!("amount"),
+                            <_ as IntoVal<Env, Val>>::into_val(&1i128, &env)
+                        ),
+                        (Symbol::new(&env, "to_muxed_id"), ().into_val(&env),),
+                    ]
+                    .to_val()
+                ),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_no_events_recorded_for_failed_call() {
+        let env = Env::default();
+        let contract_id = env.register(Contract, ());
+        let client = ContractClient::new(&env, &contract_id);
+        let from = Address::generate(&env);
+        let to = Address::generate(&env);
+        let _ = client.try_failed_transfer(&from, &to, &1);
+        assert_eq!(env.events().all(), vec![&env]);
+    }
+}
