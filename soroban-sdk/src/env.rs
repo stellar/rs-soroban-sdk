3db0574f9958f9b52f8fb79c42b1d65501d84182
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-11-14 15:50:48 +1000

    make ledger_info optional in new_for_testutils

--- soroban-sdk/src/env.rs
+++ soroban-sdk/src/env.rs
@@ -513,9 +513,8 @@ impl Env {
         }
 
         let rf = Rc::new(EmptySnapshotSource());
-        let info = default_ledger_info();
 
-        Env::new_for_testutils(config, rf, None, info, None)
+        Env::new_for_testutils(config, rf, None, None, None)
     }
 
     /// Change the test config of an Env.
@@ -528,7 +527,7 @@ impl Env {
         config: EnvTestConfig,
         recording_footprint: Rc<dyn internal::storage::SnapshotSource>,
         generators: Option<Rc<RefCell<Generators>>>,
-        ledger_info: internal::LedgerInfo,
+        ledger_info: Option<internal::LedgerInfo>,
         snapshot: Option<Rc<LedgerSnapshot>>,
     ) -> Env {
         let storage = internal::storage::Storage::with_recording_footprint(recording_footprint);
@@ -573,6 +572,7 @@ impl Env {
             },
         };
 
+        let ledger_info = ledger_info.unwrap_or_else(default_ledger_info);
         env.ledger().set(ledger_info);
 
         env
@@ -1511,7 +1511,7 @@ impl Env {
             EnvTestConfig::default(),
             Rc::new(s.ledger.clone()),
             Some(Rc::new(RefCell::new(s.generators))),
-            s.ledger.ledger_info(),
+            Some(s.ledger.ledger_info()),
             Some(Rc::new(s.ledger.clone())),
         )
     }
@@ -1556,7 +1556,7 @@ impl Env {
             EnvTestConfig::default(), // TODO: Allow setting the config.
             Rc::new(s.clone()),
             None,
-            s.ledger_info(),
+            Some(s.ledger_info()),
             Some(Rc::new(s.clone())),
         )
     }
