af8722655ea182b49d3836af0d224d9580318c09
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-11-15 23:54:09 +1000

    have fork test access multiple entries

--- tests/fork/src/lib.rs
+++ tests/fork/src/lib.rs
@@ -1,24 +1,32 @@
-#![no_std]
-
 #[cfg(test)]
-mod test {
-    use soroban_sdk::{token::TokenClient, Address, Env};
+use soroban_sdk::{token::TokenClient, Address, Env};
 
-    #[test]
-    fn test_hello() {
-        let rpc = soroban_snapshot_source_rpc::RpcSnapshotSource::new("http://localhost:8000/rpc");
-        let e = Env::from_ledger_snapshot(rpc);
-        let a = Address::from_str(
-            &e,
-            "CDMLFMKMMD7MWZP3FKUBZPVHTUEDLSX4BYGYKH4GCESXYHS3IHQ4EIG4",
-        );
-        let t = TokenClient::new(&e, &a);
+#[test]
+fn test_hello() {
+    let rpc = soroban_snapshot_source_rpc::RpcSnapshotSource::new("http://localhost:8000/rpc");
+    let e = Env::from_ledger_snapshot(rpc);
+    let a = Address::from_str(
+        &e,
+        "CDMLFMKMMD7MWZP3FKUBZPVHTUEDLSX4BYGYKH4GCESXYHS3IHQ4EIG4",
+    );
+    let t = TokenClient::new(&e, &a);
 
-        let a2 = Address::from_str(
-            &e,
-            "GCDRCSMJAYHQ5VL4K56STZLJYNHKFXZD4VKA43XCT5UUBFFZCLRR32CP",
-        );
-        let res = t.balance(&a2);
-        assert_eq!(res, 199999944857);
-    }
+    println!("test one");
+    let a2 = Address::from_str(
+        &e,
+        "GCDRCSMJAYHQ5VL4K56STZLJYNHKFXZD4VKA43XCT5UUBFFZCLRR32CP",
+    );
+    let res = t.balance(&a2);
+    println!("result {res}");
+
+    println!("waiting");
+    std::thread::sleep(std::time::Duration::from_secs(20));
+
+    println!("test two");
+    let a3 = Address::from_str(
+        &e,
+        "GAFUHA24GY66NPICT7KIP4QWZVCWQEL6OMR5QSY6JNDTCBMX72LHPKKQ",
+    );
+    let res2 = t.balance(&a3);
+    println!("result {res2}");
 }
