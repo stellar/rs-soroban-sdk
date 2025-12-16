d250e0e085e17217bf1a48615cbe5c6da580957e
Siddharth Suresh <siddharth@stellar.org>
2025-12-15 17:57:14 -0800

    Remove limit of 10 and just call unwrap

--- soroban-sdk-macros/src/derive_spec_fn.rs
+++ soroban-sdk-macros/src/derive_spec_fn.rs
@@ -143,17 +143,7 @@ pub fn derive_fn_spec(
             ));
             ScSymbol::default()
         }),
-        inputs: spec_args.try_into().unwrap_or_else(|_| {
-            const MAX: u32 = 10;
-            errors.push(Error::new(
-                inputs.iter().nth(MAX as usize).span(),
-                format!(
-                    "contract function has too many parameters, max count {} parameters",
-                    MAX,
-                ),
-            ));
-            VecM::<_, { u32::MAX }>::default()
-        }),
+        inputs: spec_args.try_into().unwrap(),
         outputs: spec_result.try_into().unwrap(),
     });
     let spec_xdr = spec_entry.to_xdr(DEFAULT_XDR_RW_LIMITS).unwrap();
