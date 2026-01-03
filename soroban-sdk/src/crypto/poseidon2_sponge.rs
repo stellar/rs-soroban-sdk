672d1a4428907ffa4b335431d5e17b6599886bbf
Jay Geng <jay@stellar.org>
2026-01-02 20:45:36 -0500

    Add comprehensive Poseidon/Poseidon2 support for BN254 and BLS12-381
      - Add poseidon_hash<N>() matching circom implementation
      - Add poseidon2_hash<N>() matching noir/barretenberg implementation
      - Add Poseidon parameters (MDS, RC) for BN254 t=3, BLS12-381 t=2,3,4
      - Add Poseidon2 parameters (MAT_DIAG, RC) for BN254/BLS12-381 t=2,3,4
      - Add PoseidonConfig/Poseidon2Config for flexible parameter selection
      - Add comprehensive test coverage validated against:
        - circomlib (BN254 Poseidon)
        - poseidon-bls12381-circom (BLS12-381 Poseidon)
        - barretenberg (BN254 Poseidon2)
        - Reference Sage scripts for parameter generation

--- soroban-sdk/src/crypto/poseidon2_sponge.rs
+++ soroban-sdk/src/crypto/poseidon2_sponge.rs
@@ -1,52 +1,87 @@
-use super::poseidon2_params::{get_mat_diag4_m_1, get_rc4, ROUNDS_F, ROUNDS_P, SBOX_D, T};
-use crate::{env::internal, unwrap::UnwrapInfallible, vec, Env, Symbol, TryIntoVal, Vec, U256};
+use super::poseidon2_params::{
+    get_mat_diag_m_1_bn254, get_rc_bn254, get_rounds_f, get_rounds_p, SBOX_D,
+};
+use crate::{
+    crypto::poseidon2_params::{get_mat_diag_m_1_bls12_381, get_rc_bls12_381},
+    env::internal,
+    unwrap::UnwrapInfallible,
+    vec, Env, Symbol, TryIntoVal, Vec, U256,
+};
 
-const RATE: u32 = 3;
+const CAPACITY: u32 = 1;
 
-pub struct Poseidon2Sponge {
-    env: Env,
-    field: Symbol,
+pub struct Poseidon2Config {
+    field_type: Symbol,
     rate: u32,
-    squeezed: bool,
-    cache: Vec<U256>,
-    state: Vec<U256>,
+    capacity: u32,
+    rounds_f: u32,
+    rounds_p: u32,
     m_diag: Vec<U256>,
     rc: Vec<Vec<U256>>,
 }
 
+impl Poseidon2Config {
+    pub fn new(env: &Env, field_type: Symbol, rate: u32) -> Self {
+        let t = rate + CAPACITY;
+        if field_type == soroban_sdk_macros::internal_symbol_short!("BN254") {
+            Poseidon2Config {
+                field_type,
+                rate,
+                capacity: CAPACITY,
+                rounds_f: get_rounds_f(t),
+                rounds_p: get_rounds_p(t),
+                m_diag: get_mat_diag_m_1_bn254(env, t),
+                rc: get_rc_bn254(env, t),
+            }
+        } else if field_type == soroban_sdk_macros::internal_symbol_short!("BLS12_381") {
+            Poseidon2Config {
+                field_type,
+                rate,
+                capacity: CAPACITY,
+                rounds_f: get_rounds_f(t),
+                rounds_p: get_rounds_p(t),
+                m_diag: get_mat_diag_m_1_bls12_381(env, t),
+                rc: get_rc_bls12_381(env, t),
+            }
+        } else {
+            panic!()
+        }
+    }
+}
+
+pub struct Poseidon2Sponge {
+    env: Env,
+    squeezed: bool,
+    cache: Vec<U256>,
+    state: Vec<U256>,
+    config: Poseidon2Config,
+}
+
 impl Poseidon2Sponge {
-    pub fn new(env: &Env, iv: U256, field: Symbol) -> Poseidon2Sponge {
-        // TODO: we need to set up parameters for BLS12-381
-        assert_eq!(field, soroban_sdk_macros::internal_symbol_short!("BN254"));
+    pub fn new(env: &Env, iv: U256, config: Poseidon2Config) -> Self {
+        // the last element is reserved for capacity
         let mut state = vec![env];
-        for _ in 0..RATE {
+        for _ in 0..config.rate {
             state.push_back(U256::from_u32(env, 0));
         }
         state.push_back(iv);
 
-        // Get parameters from poseidon2_params
-        let m_diag = get_mat_diag4_m_1(env);
-        let rc = get_rc4(env);
-
-        Poseidon2Sponge {
+        Self {
             env: env.clone(),
-            field,
-            rate: RATE,
             cache: Vec::new(env),
             state,
             squeezed: false,
-            m_diag,
-            rc,
+            config,
         }
     }
 
     fn perform_duplex(&mut self) {
         // zero-pad the cache
-        for _ in self.cache.len()..RATE {
+        for _ in self.cache.len()..self.config.rate {
             self.cache.push_back(U256::from_u32(&self.env, 0));
         }
         // add the cache into sponge state
-        for i in 0..RATE {
+        for i in 0..self.config.rate {
             let elem = self
                 .state
                 .get_unchecked(i)
@@ -54,47 +89,57 @@ impl Poseidon2Sponge {
             self.state.set(i, elem);
         }
 
-        // t = rate + 1 = 4
-        let t = T as u32;
-        let d = SBOX_D;
-        let rounds_f = ROUNDS_F;
-        let rounds_p = ROUNDS_P;
-
         self.state = internal::Env::poseidon2_permutation(
             &self.env,
             self.state.to_object(),
-            self.field.to_symbol_val(),
-            t.into(),
-            d.into(),
-            rounds_f.into(),
-            rounds_p.into(),
-            self.m_diag.to_object(),
-            self.rc.to_object(),
+            self.config.field_type.to_symbol_val(),
+            self.state.len().into(), // t = rate + capacity
+            SBOX_D.into(),
+            self.config.rounds_f.into(),
+            self.config.rounds_p.into(),
+            self.config.m_diag.to_object(),
+            self.config.rc.to_object(),
         )
         .unwrap_infallible()
         .try_into_val(&self.env)
         .unwrap_infallible();
     }
 
-    pub fn absorb(&mut self, input: U256) {
+    pub fn absorb(&mut self, inputs: &Vec<U256>) {
         assert!(!self.squeezed);
-        if self.cache.len() == RATE {
-            // If we're absorbing, and the cache is full, apply the sponge permutation to compress the cache
+        let cache_len = self.cache.len();
+        let inputs_len = inputs.len();
+
+        if cache_len + inputs_len > self.config.rate {
+            // if cache does not have enough room, absorb the remaining room.
+            // Remain must be positive, since cache size starts at 0 (<=rate),
+            // and after each iteration cache size <= rate.
+            let remain = self.config.rate - cache_len;
+            self.cache.append(&inputs.slice(0..remain));
+            // apply the sponge permutation to compress the cache
             self.perform_duplex();
-            self.cache = vec![&self.env, input];
+            self.cache = vec![&self.env];
+            // call absorb with the leftover inputs
+            self.absorb(&inputs.slice(remain..));
         } else {
-            // If we're absorbing, and the cache is not full, add the input into the cache
-            self.cache.push_back(input);
+            // If the cache is not full, add the input into the cache
+            self.cache.append(inputs);
         }
     }
 
     pub fn squeeze(&mut self) -> U256 {
         assert!(!self.squeezed);
-        // If we're in absorb mode, apply sponge permutation to compress the cache.
         self.perform_duplex();
         self.squeezed = true;
-
-        // Pop one item off the top of the permutation and return it.
         self.state.get_unchecked(0)
     }
+}
+
+pub fn hash<const N: usize>(env: &Env, inputs: &[U256; N], config: Poseidon2Config) -> U256 {
+    // The initial value for the capacity element initialized with `input.len() * 2^64` for Poseidon2
+    let iv = U256::from_u128(env, (N as u128) << 64);
+    let mut sponge = Poseidon2Sponge::new(env, iv, config);
+    let input_vec = Vec::from_array(env, inputs.clone());
+    sponge.absorb(&input_vec);
+    sponge.squeeze()
 }
--- soroban-sdk/src/crypto/poseidon_sponge.rs
+++ soroban-sdk/src/crypto/poseidon_sponge.rs
@@ -1,99 +1,154 @@
-use super::poseidon_params::{get_mds3, get_rc3, ROUNDS_F, ROUNDS_P, SBOX_D, T};
-use crate::{env::internal, unwrap::UnwrapInfallible, vec, Env, Symbol, TryIntoVal, Vec, U256};
+use super::poseidon_params::{
+    get_mds_bls12_381, get_mds_bn254, get_rc_bls12_381, get_rc_bn254, SBOX_D,
+};
+use crate::{
+    crypto::poseidon_params::{
+        get_rounds_f_bls12_381, get_rounds_f_bn254, get_rounds_p_bls12_381, get_rounds_p_bn254,
+    },
+    env::internal,
+    unwrap::UnwrapInfallible,
+    vec, Env, Symbol, TryIntoVal, Vec, U256,
+};
 
-const RATE: u32 = 2;
+const CAPACITY: u32 = 1;
 
-pub struct PoseidonSponge {
-    env: Env,
-    field: Symbol,
+pub struct PoseidonConfig {
+    field_type: Symbol,
     rate: u32,
-    squeezed: bool,
-    cache: Vec<U256>,
-    state: Vec<U256>,
+    capacity: u32,
+    rounds_f: u32,
+    rounds_p: u32,
     mds: Vec<Vec<U256>>,
     rc: Vec<Vec<U256>>,
 }
 
+impl PoseidonConfig {
+    pub fn new(env: &Env, field_type: Symbol, rate: u32) -> Self {
+        let t = rate + CAPACITY;
+        if field_type == soroban_sdk_macros::internal_symbol_short!("BN254") {
+            PoseidonConfig {
+                field_type,
+                rate,
+                capacity: CAPACITY,
+                rounds_f: get_rounds_f_bn254(t),
+                rounds_p: get_rounds_p_bn254(t),
+                mds: get_mds_bn254(env, t),
+                rc: get_rc_bn254(env, t),
+            }
+        } else if field_type == soroban_sdk_macros::internal_symbol_short!("BLS12_381") {
+            PoseidonConfig {
+                field_type,
+                rate,
+                capacity: CAPACITY,
+                rounds_f: get_rounds_f_bls12_381(t),
+                rounds_p: get_rounds_p_bls12_381(t),
+                mds: get_mds_bls12_381(env, t),
+                rc: get_rc_bls12_381(env, t),
+            }
+        } else {
+            panic!()
+        }
+    }
+}
+
+pub struct PoseidonSponge {
+    env: Env,
+    squeezed: bool,
+    cache: Vec<U256>,
+    state: Vec<U256>, // state size = rate + capacity
+    config: PoseidonConfig,
+}
+
 impl PoseidonSponge {
-    pub fn new(env: &Env, iv: U256, field: Symbol) -> PoseidonSponge {
-        // TODO: we need to set up parameters for BLS12-381
-        assert_eq!(field, soroban_sdk_macros::internal_symbol_short!("BN254"));
+    // field_type - 'BN254' or 'BLS12_381'
+    // rate - number of elements the sponge can absorb/squeeze continously at once, after which a permutation is
+    // required.
+    // capacity - number of extra elements the state contains, capacity provides more safety. capacity = 1 in our simplified sponge (matching circom's).
+    // state.len() == rate + capacity
+    // returns a Poseidon object initialize with the prepulated parameters for use, you can call hash on it.
+    // limitation - single sqeeze, no duplex switching between absorb and sqeeze
+    pub fn new(env: &Env, iv: U256, config: PoseidonConfig) -> Self {
         // the 0-th element is reserved for capacity
         let mut state = vec![env, iv];
-        for _ in 0..RATE {
+        for _ in 0..config.rate {
             state.push_back(U256::from_u32(env, 0));
         }
-
-        // Get parameters from poseidon2_params
-        let mds = get_mds3(env);
-        let rc = get_rc3(env);
-
-        PoseidonSponge {
+        Self {
             env: env.clone(),
-            field,
-            rate: RATE,
+            squeezed: false,
             cache: Vec::new(env),
             state,
-            squeezed: false,
-            mds,
-            rc,
+            config,
         }
     }
 
     fn perform_duplex(&mut self) {
         // zero-pad the cache
-        for _ in self.cache.len()..RATE {
+        for _ in self.cache.len()..self.config.rate {
             self.cache.push_back(U256::from_u32(&self.env, 0));
         }
         // add the cache into sponge state, leaving the 0-th element
-        for i in 1..RATE + 1 {
+        for i in 0..self.config.rate {
             let elem = self
                 .state
-                .get_unchecked(i)
-                .add(&self.cache.get_unchecked(i - 1));
-            self.state.set(i, elem);
+                .get_unchecked(i + CAPACITY)
+                .add(&self.cache.get_unchecked(i));
+            self.state.set(i + CAPACITY, elem);
         }
 
-        let t = T as u32;
         let d = SBOX_D;
-        let rounds_f = ROUNDS_F;
-        let rounds_p = ROUNDS_P;
 
         self.state = internal::Env::poseidon_permutation(
             &self.env,
             self.state.to_object(),
-            self.field.to_symbol_val(),
-            t.into(),
+            self.config.field_type.to_symbol_val(),
+            self.state.len().into(), // t = rate + capacity
             d.into(),
-            rounds_f.into(),
-            rounds_p.into(),
-            self.mds.to_object(),
-            self.rc.to_object(),
+            self.config.rounds_f.into(),
+            self.config.rounds_p.into(),
+            self.config.mds.to_object(),
+            self.config.rc.to_object(),
         )
         .unwrap_infallible()
         .try_into_val(&self.env)
         .unwrap_infallible();
     }
 
-    pub fn absorb(&mut self, input: U256) {
+    pub fn absorb(&mut self, inputs: &Vec<U256>) {
         assert!(!self.squeezed);
-        if self.cache.len() == RATE {
-            // If we're absorbing, and the cache is full, apply the sponge permutation to compress the cache
+        let cache_len = self.cache.len();
+        let inputs_len = inputs.len();
+
+        if cache_len + inputs_len > self.config.rate {
+            // if cache does not have enough room, absorb the remaining room.
+            // Remain must be positive, since cache size starts at 0 (<=rate),
+            // and after each iteration cache size <= rate.
+            let remain = self.config.rate - cache_len;
+            self.cache.append(&inputs.slice(0..remain));
+            // apply the sponge permutation to compress the cache
             self.perform_duplex();
-            self.cache = vec![&self.env, input];
+            self.cache = vec![&self.env];
+            // call absorb with the leftover inputs
+            self.absorb(&inputs.slice(remain..));
         } else {
-            // If we're absorbing, and the cache is not full, add the input into the cache
-            self.cache.push_back(input);
+            // If the cache is not full, add the input into the cache
+            self.cache.append(inputs);
         }
     }
 
     pub fn squeeze(&mut self) -> U256 {
         assert!(!self.squeezed);
-        // If we're in absorb mode, apply sponge permutation to compress the cache.
         self.perform_duplex();
         self.squeezed = true;
-
-        // Pop one item off the top of the permutation and return it.
         self.state.get_unchecked(0)
     }
+}
+
+pub fn hash<const N: usize>(env: &Env, inputs: &[U256; N], config: PoseidonConfig) -> U256 {
+    // The initial value for the capacity element initialized with 0 for standard Poseidon
+    let iv = U256::from_u32(env, 0);
+    let mut sponge = PoseidonSponge::new(env, iv, config);
+    let input_vec = Vec::from_array(env, inputs.clone());
+    sponge.absorb(&input_vec);
+    sponge.squeeze()
 }
