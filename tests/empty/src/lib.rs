1a15573ab30afe6cb1a178582888173fa53b90fc
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-01-09 23:37:35 +1000

    remove soroban-token-sdk dependency from empty test

--- tests/empty/src/lib.rs
+++ tests/empty/src/lib.rs
@@ -1,25 +1,17 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Address, Env};
-use soroban_token_sdk::TokenUtils as _;
+use soroban_sdk::{contract, contractimpl};
 
 #[contract]
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn empty(env: &Env, a: Address) {
-        soroban_token_sdk::events::TransferWithAmountOnly {
-            to: a.clone(),
-            from: a,
-            amount: 1,
-        }
-        .publish(env);
-    }
+    pub fn empty() {}
 }
 
 #[cfg(test)]
 mod test {
-    use soroban_sdk::{testutils::Address as _, Address, Env};
+    use soroban_sdk::Env;
 
     use crate::{Contract, ContractClient};
 
@@ -28,7 +20,7 @@ mod test {
         let e = Env::default();
         let contract_id = e.register(Contract, ());
         let client = ContractClient::new(&e, &contract_id);
-        let a = Address::generate(&e);
-        client.empty(&a);
+
+        client.empty();
     }
 }
