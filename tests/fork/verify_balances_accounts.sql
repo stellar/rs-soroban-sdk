75b823e42da6aebbea3a7f361a77e0c097177220
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-02-27 13:05:27 +1000

    simplify balance query by removing CTEs

--- tests/fork/verify_balances_accounts.sql
+++ tests/fork/verify_balances_accounts.sql
@@ -13,26 +13,10 @@ Usage:
     "$(cat verify_balances_accounts.sql)" > verify_balances_accounts.csv
 */
 
-WITH
-account_balances AS (
-  SELECT
-    ledger_sequence,
-    CAST(ROUND(balance * 10000000) AS INT64) as balance_stroops
-  FROM `crypto-stellar.crypto_stellar.accounts`
-  WHERE account_id = "GBLKX4UPDM7CC4UUG2FXBLOCXOTQ6ARHOQYVL4RD6A4AQVB6TPTLIUYN"
-    AND ledger_sequence BETWEEN 61292151 AND 61292232
-  ORDER BY ledger_sequence
-),
-with_next AS (
-  SELECT
-    ledger_sequence,
-    balance_stroops,
-    LEAD(ledger_sequence) OVER (ORDER BY ledger_sequence) as next_active_ledger,
-    LEAD(balance_stroops) OVER (ORDER BY ledger_sequence) as next_balance_stroops
-  FROM account_balances
-)
 SELECT
   ledger_sequence,
-  balance_stroops as end_of_ledger_balance
-FROM with_next
+  CAST(ROUND(balance * 10000000) AS INT64) as end_of_ledger_balance
+FROM `crypto-stellar.crypto_stellar.accounts`
+WHERE account_id = "GBLKX4UPDM7CC4UUG2FXBLOCXOTQ6ARHOQYVL4RD6A4AQVB6TPTLIUYN"
+  AND ledger_sequence BETWEEN 61292151 AND 61292232
 ORDER BY ledger_sequence
