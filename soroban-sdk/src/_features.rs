88bf5c3d92531ad6aafa9b7b1bd90e170c48826d
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-03-03 18:34:43 +1000

    rename env var to remove experimental prefix

--- soroban-sdk/src/_features.rs
+++ soroban-sdk/src/_features.rs
@@ -117,7 +117,7 @@
 //! This feature requires building with `stellar contract build` from
 //! `stellar-cli` v25.2.0 or newer. Building directly with `cargo build` will
 //! produce a build error unless the
-//! `SOROBAN_SDK_BUILD_SYSTEM_SUPPORTS_EXPERIMENTAL_SPEC_SHAKING_V2` environment variable is
+//! `SOROBAN_SDK_BUILD_SYSTEM_SUPPORTS_SPEC_SHAKING_V2` environment variable is
 //! set.
 //!
 //! [`contracttype`]: crate::contracttype
