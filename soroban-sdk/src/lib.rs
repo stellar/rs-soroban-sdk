ff1225fcd5bc8f9b75d04d78194dde3bd30231c3
mootz12 <mootz12@protonmail.com>
2026-04-01 12:05:33 -0400

    chore: deprecate instead of remove assert_in_contract

--- soroban-sdk/src/lib.rs
+++ soroban-sdk/src/lib.rs
@@ -137,9 +137,27 @@ pub mod reexports_for_macros {
 /// `debug_assert_in_contract!` asserts that the contract is currently executing within a
 /// contract. The macro expands to an assertion when testutils are enabled or in tests,
 /// otherwise it expands to nothing.
-#[doc(hidden)]
-#[macro_export]
 macro_rules! debug_assert_in_contract {
+    ($env:expr $(,)?) => {{
+        {
+            #[cfg(any(test, feature = "testutils"))]
+            assert!(
+                ($env).in_contract(),
+                "this function is not accessible outside of a contract, wrap \
+                the call with `env.as_contract()` to access it from a \
+                particular contract"
+            );
+        }
+    }};
+}
+
+// For internal use, use `debug_assert_in_contract!` instead.
+/// Assert in contract asserts that the contract is currently executing within a
+/// contract. The macro maps to code when testutils are enabled or in tests,
+/// otherwise maps to nothing.
+#[deprecated(note = "this macro is deprecated and will be removed in a future release")]
+#[macro_export]
+macro_rules! assert_in_contract {
     ($env:expr $(,)?) => {{
         {
             #[cfg(any(test, feature = "testutils"))]
