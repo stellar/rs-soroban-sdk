23413da8e8c4903a0cbd1de19c668d6a71c69836
mootz12 <mootz12@protonmail.com>
2026-04-07 20:26:45 -0400

    chore: add migration docs

--- soroban-token-sdk/src/_migrating.rs
+++ soroban-token-sdk/src/_migrating.rs
@@ -1,3 +1,10 @@
+//! # Migrating from v23 to v26
+//! 
+//! 1. Remove the deprecated event format. For details, see the [migration guide for v23 contract events].
+//!    And for an example using the `soroban_token_sdk` directly, see the [migration guide for v23 token transfer][v23_token_transfer].
+//! 
+//! [migration guide for v23 contract events]: soroban_sdk::_migrating::v23_contractevent
+//! 
 //! # Migrating from v22 to v23
 //!
 //! 1. [`MuxedAddress` replaces `Address` as the `to` of the `TokenInterface::transfer`][v23_token_transfer]
