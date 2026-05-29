0631bd8a3e9bdf8f3288d0130d74a3839c9b5d05
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-05-29 06:31:09 +0000

    require stellar-cli for wasm contract builds

--- soroban-sdk/build.rs
+++ soroban-sdk/build.rs
@@ -20,15 +20,18 @@ pub fn main() {
         println!("cargo:rustc-env=RUSTC_VERSION={rustc_version}");
     }
 
-    // When the experimental_spec_shaking_v2 feature is enabled on a wasm target, check for an
-    // env var from the build system (Stellar CLI) that indicates it supports spec optimization
-    // using markers.
-    if std::env::var("CARGO_FEATURE_EXPERIMENTAL_SPEC_SHAKING_V2").is_ok() {
-        let env_name = "SOROBAN_SDK_BUILD_SYSTEM_SUPPORTS_SPEC_SHAKING_V2";
-        println!("cargo::rerun-if-env-changed={env_name}");
-        if std::env::var(env_name).is_err()
-            && std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default() == "wasm"
-        {
+    println!("cargo::rerun-if-env-changed=SOROBAN_SDK_BUILD_SYSTEM_SUPPORTS_SPEC_SHAKING_V2");
+    println!("cargo::rerun-if-env-changed=SOROBAN_SDK_ALLOW_BUILD_WITHOUT_STELLAR_CLI");
+
+    // When building for wasm and the build system (e.g. stellar-cli) does not support spec shaking
+    // v2, we check some situations which will compile error to prevent developers from building
+    // contracts that are broken.
+    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap_or_default() == "wasm"
+        && std::env::var("SOROBAN_SDK_BUILD_SYSTEM_SUPPORTS_SPEC_SHAKING_V2").is_err()
+    {
+        // Check if spec shaking v2 is enabled, if it is, the build needs to be run with a version
+        // of stellar-cli that supports it.
+        if std::env::var("CARGO_FEATURE_EXPERIMENTAL_SPEC_SHAKING_V2").is_ok() {
             eprintln!(
                 "\
 \nerror: soroban-sdk feature 'experimental_spec_shaking_v2' requires stellar-cli v25.2.0+\
@@ -40,6 +43,25 @@ pub fn main() {
 \n  - Build with `stellar contract build` using stellar-cli v25.2.0+\
 \n  - Disable the feature by removing 'experimental_spec_shaking_v2' from\
 \n    the soroban-sdk import features list in Cargo.toml.\
+"
+            );
+            std::process::exit(1);
+        }
+
+        // Building contracts that use the soroban-sdk will soon require the stellar-cli and a
+        // version that supports spec shaking v2. An env var
+        // escape hatch is provided to temporarily disable the error, and will be removed in a future
+        // version of soroban-sdk.
+        if std::env::var("SOROBAN_SDK_ALLOW_BUILD_WITHOUT_STELLAR_CLI").is_err() {
+            eprintln!(
+                "\
+\nerror: building contracts that use the soroban-sdk will soon require a recent version of the stellar-cli\
+\n\
+\nBuild with `stellar contract build` from stellar-cli v25.2.0 or newer.\
+\n\
+\nTo temporarily allow building without stellar-cli, set the env var:\
+\n  {allow_env}=1\
+\nThis escape hatch will be removed in a near future version of soroban-sdk.\
 "
             );
             std::process::exit(1);
